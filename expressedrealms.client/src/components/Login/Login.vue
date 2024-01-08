5be3f9ad78ba7f7469b950a2c09fe834d95c809a
Noremac Skich <noremacskich@gmail.com>
2024-01-08 11:45:01 -0600

    Initial swing at validation on the login form

--- expressedrealms.client/src/components/Login/Login.vue
+++ expressedrealms.client/src/components/Login/Login.vue
@@ -2,12 +2,30 @@
 
 import InputText from 'primevue/inputtext';
 import Button from 'primevue/button';
-
-import { ref } from 'vue'
+import axios from "axios";
+import Router from "@/router";
 import LoginBasePlate from "@/components/Login/LoginBasePlate.vue";
+import { useForm } from 'vee-validate';
+import * as yup from 'yup';
 
-const email = ref("");
-const password = ref("");
+const schema = yup.object({
+  email: yup.string().required().email().label('Email address'),
+  password: yup.string().required().label('Password'),
+});
+
+const { defineField, handleSubmit, errors } = useForm({
+  validationSchema: schema,
+});
+
+const [email] = defineField('email');
+const [password] = defineField('password');
+
+const onSubmit = handleSubmit((values) => {
+  axios.post('/api/auth/login', values)
+  .then((response) => {
+    Router.push('characters');
+  });
+});
 
 </script>
 
@@ -15,19 +33,19 @@ const password = ref("");
   <login-base-plate>
     <div class="row">
       <div class="col">
-        <div>
-          <label for="email1" class="block text-900 font-medium mb-2">Email</label>
-          <InputText id="email1" type="text" v-model="email" class="w-100 mb-3" />
-        </div>
-        <div>
-          <label for="password1" class="block text-900 font-medium mb-2">Password</label>
-          <InputText id="password1" type="password" v-model="password" class="w-100 mb-3" />
-        </div>
-      </div>
-    </div>
-    <div class="row">
-      <div class="col">
-        <Button label="Sign In" class="w-100 mb-2"></Button>
+        <form @submit="onSubmit">
+          <div class="mb-3">
+            <label for="email" class="block text-900 font-medium">Email</label>
+            <InputText id="email" v-model="email" class="w-100" :class="{ 'p-invalid': errors.email }"/>
+            <small id="email-help" class="text-danger">{{ errors.email }}</small>
+          </div>
+          <div class="mb-3">
+            <label for="password" class="block text-900 font-medium">Password</label>
+            <InputText id="password" type="password" v-model="password" class="w-100" :class="{ 'p-invalid': errors.password }"/>
+            <small id="password-help" class="text-danger">{{ errors.password }}</small>
+          </div>
+          <Button label="Sign In" class="w-100 mb-2" type="submit"></Button>
+        </form>
         <router-link to="CreateAccount">
           <Button label="Create Account" class="w-100 mb-2"></Button>
         </router-link>
