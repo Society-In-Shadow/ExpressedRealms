08d5a64d7adc5b1a623a33b05a9043cc0e0c340c
Noremac Skich <noremacskich@gmail.com>
2024-01-09 12:57:53 -0600

    Fixed missing dependencies in the console

--- expressedrealms.client/src/main.ts
+++ expressedrealms.client/src/main.ts
@@ -4,7 +4,12 @@ import { createApp } from 'vue'
 import PrimeVue from 'primevue/config';
 import Router from "@/router";
 import 'primevue/resources/themes/lara-dark-green/theme.css'
+import 'primevue/resources/themes/lara-dark-green/fonts/Inter-italic.var.woff2'
+import 'primevue/resources/themes/lara-dark-green/fonts/Inter-roman.var.woff2'
 import "primeicons/primeicons.css";
+import "primeicons/fonts/primeicons.ttf"
+import "primeicons/fonts/primeicons.woff"
+import "primeicons/fonts/primeicons.woff2"
 import "primeflex/primeflex.css"
 
 import 'bootstrap/scss/bootstrap-utilities.scss'
