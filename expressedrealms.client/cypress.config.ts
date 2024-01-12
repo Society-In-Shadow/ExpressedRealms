930ed1096246678debb9dca6832851a8b5939d60
Noremac Skich <noremacskich@gmail.com>
2024-01-12 15:55:34 -0600

    Got the component level testing figured out finally, took using the chat gpt to figure out though : https://chat.openai.com/share/f09deddb-821e-450d-b78d-444977b4aeab

--- expressedrealms.client/cypress.config.ts
+++ expressedrealms.client/cypress.config.ts
@@ -1,4 +1,15 @@
 import { defineConfig } from 'cypress';
+import { UserConfig } from 'vite';
+
+import ViteConfig from "./vite.config";
+
+const viteConfig = ViteConfig as UserConfig;
+
+if (viteConfig.server) {
+  // @see https://github.com/cypress-io/cypress/issues/24564
+  // @see https://chat.openai.com/share/f09deddb-821e-450d-b78d-444977b4aeab
+  delete viteConfig.server.https;
+}
 
 export default defineConfig({
   projectId: "3wpvob",
@@ -11,6 +22,7 @@ export default defineConfig({
     devServer: {
       framework: "vue",
       bundler: "vite",
+      viteConfig
     },
   },
 });
