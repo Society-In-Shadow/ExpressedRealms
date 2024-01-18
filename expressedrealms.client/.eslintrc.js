6cb0a730754e82d2d2389a1f5148127ec08f683a
Noremac Skich <noremacskich@gmail.com>
2024-01-18 17:27:01 -0600

    Added more plugins to eslint, and renamed it to be a js file

--- expressedrealms.client/.eslintrc.js
+++ expressedrealms.client/.eslintrc.js
@@ -3,10 +3,17 @@ require('@rushstack/eslint-patch/modern-module-resolution')
 
 module.exports = {
   root: true,
+  plugins: [
+    'vue',
+    'cypress',
+    'sonarjs'
+  ],
   'extends': [
-    'plugin:vue/vue3-essential',
+    'plugin:vue/vue3-recommended',
+    'plugin:cypress/recommended',
+    'plugin:sonarjs/recommended',
     'eslint:recommended',
-    '@vue/eslint-config-typescript'
+    '@vue/eslint-config-typescript',
   ],
   parserOptions: {
     ecmaVersion: 'latest'
