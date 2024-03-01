48444ad64797eb5cc961aa9f48d3c8de15e80cc2
noremacskich <noremacskich@gmail.com>
2024-03-01 01:56:13 -0600

    Some basic cleanup, added tile for main content to be in

--- expressedrealms.client/src/components/LoggedInLayout.vue
+++ expressedrealms.client/src/components/LoggedInLayout.vue
@@ -2,15 +2,22 @@
 <template>
   <main-nav-bar />
   
-  <router-view />
+  <Card class="m-3 p-0" id="baseTile">
+    <template #content>
+      <router-view />
+    </template>
+  </Card>
 </template>
 
 <script setup>
 import { RouterView } from 'vue-router'
 import MainNavBar from "@/components/navbar/MainNavBar.vue";
-
+import Card from 'primevue/card';
 </script>
 
-<style scoped>
-
+<style>
+  #baseTile > div > div{
+    padding-top: 0 !important;
+    padding-bottom: 0 !important;
+  }
 </style>
