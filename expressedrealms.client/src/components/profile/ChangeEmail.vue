bb18d4253026725968cfcbb4199161f9e3fe2b4b
noremacskich <noremacskich@gmail.com>
2024-03-04 03:04:25 -0600

    Confirm email should now be up and running

--- expressedrealms.client/src/components/profile/ChangeEmail.vue
+++ expressedrealms.client/src/components/profile/ChangeEmail.vue
@@ -4,44 +4,37 @@ import InputText from 'primevue/inputtext';
 import Button from 'primevue/button';
 import Card from 'primevue/card';
 import axios from "axios";
-import Router from "@/router";
 import { useForm } from 'vee-validate';
 import { object, string, ref }  from 'yup';
-import { useRoute } from 'vue-router'
+import {ref as vueRef} from "vue";
+import Message from "primevue/message";
+import {resetEmailConfirmation} from "@/services/Authentication";
 
 const { defineField, handleSubmit, errors } = useForm({
   validationSchema: object({
-    email: string().required()
-        .email()
-        .label('Email address'),
-    password: string()
+    email: string()
         .required()
-        .min(8)
-        .matches(/[0-9]/, 'Password requires a number')
-        .matches(/[a-z]/, 'Password requires a lowercase letter')
-        .matches(/[A-Z]/, 'Password requires an uppercase letter')
-        .matches(/[^\w]/, 'Password requires a symbol')
-        .label('Password'),
-    confirmPassword: string().required()
-        .oneOf([ref('password')], 'Passwords must match')
+        .email()
+        .label('Email'),
+    confirmEmail: string().required()
+        .oneOf([ref('email')], 'Emails must match')
         .label('Confirm password')
   })
 });
 
 const [email] = defineField('email');
 const [confirmEmail] = defineField('confirmEmail');
+const successfullyChangedEmail = vueRef(false);
 
-const route = useRoute();
-
-const onEmailSubmit = handleSubmit((values) => {
-  axios.post('/api/auth/resetPassword',
+const onEmailSubmit = handleSubmit((values, { resetForm }) => {
+  axios.post('/api/auth/manage/info',
       {
-        email: values.email,
-        resetCode: route.query.resetToken,
-        newPassword: values.confirmPassword
+        newEmail: values.confirmEmail
       }).then(() => {
-    Router.push('login?resetPassword=1');
-  });
+        successfullyChangedEmail.value = true;
+        resetEmailConfirmation();
+        resetForm();
+      });
 });
 
 </script>
@@ -51,19 +44,22 @@ const onEmailSubmit = handleSubmit((values) => {
   <Card class="mb-3">
     <template #title>Change Email</template>
     <template #content>
+      <Message v-if="successfullyChangedEmail" severity="success" :closable="false" data-cy="successful-change-password">
+        Email confirmation was sent to the provided email.  Once you click the link, it will update your email address. 
+      </Message>
       <form @submit="onEmailSubmit">
         <div class="flex flex-column gap-2 mb-3">
           <label for="email">Email</label>
           <InputText
-              id="email" v-model="email" data-cy="email" type="text" class=""
+              id="email" v-model="email" data-cy="email" type="text"
               :class="{ 'p-invalid': errors.email }"
           />
           <small data-cy="email-help" class="text-danger">{{ errors.email }}</small>
         </div>
         <div class="flex flex-column gap-2 mb-3">
-          <label for="email">Confirm Email</label>
+          <label for="confirm-email">Confirm Email</label>
           <InputText
-              id="confirm-email" v-model="confirmEmail" data-cy="confirm-email" type="text" class=""
+              id="confirm-email" v-model="confirmEmail" data-cy="confirm-email" type="text"
               :class="{ 'p-invalid': errors.email }"
           />
           <small data-cy="email-help" class="text-danger">{{ errors.email }}</small>
