af6a2976522afe9915c1b3e68faac4487e753ac2
noremacskich <noremacskich@gmail.com>
2024-03-06 19:24:35 -0600

    Created a InputText wrapper to simplify creating forms

---
+++ expressedrealms.client/src/CustomFormElements/TextInput.vue
@@ -0,0 +1,36 @@
+<script setup lang="ts">
+
+import InputText from "primevue/inputtext";
+
+const model = defineModel();
+
+const props = defineProps({
+  friendlyFieldName: {
+    type: String,
+    required: true,
+  },
+  dataCyTag: {
+    type: String,
+    required: true
+  },
+  errorText: {
+    required: true
+  }
+});
+
+</script>
+
+<template>
+  <div class="mb-3">
+    <label :for="dataCyTag">{{ props.friendlyFieldName }}</label>
+    <InputText
+        :id="dataCyTag" v-model="model" :data-cy="dataCyTag" class="w-100"
+        :class="{ 'p-invalid': errorText }"
+    />
+    <small :data-cy="dataCyTag + '-help'" class="text-danger">{{ errorText }}</small>
+  </div>
+</template>
+
+<style scoped>
+
+</style>
