4b9030465e1a16c9b19315ddb31368b9177e8b48
noremacskich <noremacskich@gmail.com>
2024-03-08 13:16:01 -0600

    Restructured Pinia to follow better practices

--- expressedrealms.client/src/stores/userStore.ts
+++ expressedrealms.client/src/stores/userStore.ts
@@ -1,12 +1,12 @@
-import { ref } from 'vue'
 import { defineStore } from 'pinia'
 
-export const userStore = defineStore('user', () => {
-    const userEmail = ref("");
-    const hasConfirmedEmail = ref(false);
-    const isPlayerSetup = ref(false);
-
-    return { userEmail, hasConfirmedEmail, isPlayerSetup }
-}, {
-    persist: true
-})
+export const userStore = 
+defineStore('user', {
+    state: () => {
+        return {
+            userEmail: "" as String,
+            hasConfirmedEmail: false as Boolean,
+            isPlayerSetup: false as Boolean            
+        }
+    },
+});
