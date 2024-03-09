7cae330c5082c3c3ebb2e2bb5477ce7348865535
noremacskich <noremacskich@gmail.com>
2024-03-08 19:38:11 -0600

    Fixed nullable stuff

--- ExpressedRealms.Server/EndPoints/PlayerEndpoints/DTOs/CreatePlayerDTO.cs
+++ ExpressedRealms.Server/EndPoints/PlayerEndpoints/DTOs/CreatePlayerDTO.cs
@@ -2,8 +2,8 @@ namespace ExpressedRealms.Server.EndPoints.PlayerEndpoints.DTOs;
 
 public class CreatePlayerDTO
 {
-    public string Name { get; set; }
-    public string PhoneNumber { get; set; }
-    public string City { get; set; }
-    public string State { get; set; }
+    public string Name { get; set; } = null!;
+    public string PhoneNumber { get; set; } = null!;
+    public string City { get; set; } = null!;
+    public string State { get; set; } = null!;
 }
