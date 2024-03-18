ea65f8d33988ddab4756ae952e53cb6bb4393085
noremacskich <noremacskich@gmail.com>
2024-03-18 02:01:32 -0500

    Created endpoint to create characters

--- ExpressedRealms.Server/EndPoints/CharacterEndPoints/CharacterEndPoints.cs
+++ ExpressedRealms.Server/EndPoints/CharacterEndPoints/CharacterEndPoints.cs
@@ -1,7 +1,10 @@
 using ExpressedRealms.DB;
+using ExpressedRealms.DB.Characters;
+using ExpressedRealms.Server.EndPoints.DTOs;
 using ExpressedRealms.Server.Extensions;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.EntityFrameworkCore;
+using SharpGrip.FluentValidation.AutoValidation.Endpoints.Extensions;
 
 namespace ExpressedRealms.Server.EndPoints;
 
@@ -9,7 +12,9 @@ internal static class CharacterEndPoints
 {
     internal static void AddCharacterEndPoints(this WebApplication app)
     {
-        app.MapGet("/characters", [Authorize] async (ExpressedRealmsDbContext dbContext, HttpContext http) =>
+        var endpointGroup = app.MapGroup("characters").AddFluentValidationAutoValidation();
+        
+        endpointGroup.MapGet("", [Authorize] async (ExpressedRealmsDbContext dbContext, HttpContext http) =>
             {
                 return await dbContext.Characters
                     .Where(x => x.Player.UserId == http.User.GetUserId()).ToListAsync();
@@ -17,6 +22,28 @@ internal static class CharacterEndPoints
             .WithName("Characters")
             .WithOpenApi()
             .RequireAuthorization();
-        
+
+        endpointGroup.MapPost("", async (CreateCharacterDTO dto, ExpressedRealmsDbContext dbContext, HttpContext http) =>
+        {
+            var playerId = await dbContext.Players
+                .Where(x => x.UserId == http.User.GetUserId())
+                .Select(x => x.Id)
+                .FirstAsync();
+
+            var newCharacter = new Character()
+            {
+                PlayerId = playerId,
+                Name = dto.Name,
+                Background = dto.Background
+            };
+            
+            dbContext.Characters.Add(newCharacter);
+
+            await dbContext.SaveChangesAsync();
+
+            return Results.Created("/characters", newCharacter.Id);
+        })
+        .WithOpenApi()
+        .RequireAuthorization();
     }
 }
