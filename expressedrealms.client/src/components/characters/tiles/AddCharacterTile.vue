1fb162e04eb0d9d8c0ccba80ded38864a597870e
noremacskich <noremacskich@gmail.com>
2024-03-18 02:03:29 -0500

    Add character tile should now be able to redirect to add user page

--- expressedrealms.client/src/components/characters/tiles/AddCharacterTile.vue
+++ expressedrealms.client/src/components/characters/tiles/AddCharacterTile.vue
@@ -2,13 +2,16 @@
 
 import Button from "primevue/button";
 import Card from "primevue/card";
+import Router from "@/router";
+
+
 </script>
 
 <template>
   <Card class="mb-3 characterTile">
     <template #content>
       <div style="text-align: center;" class="align-self-center">
-        <Button data-cy="character-delete-button" label="Add" type="submit" />
+        <Button data-cy="character-delete-button" label="Add" @click="Router.push('/characters/add')"/>
       </div>
     </template>
   </Card>
