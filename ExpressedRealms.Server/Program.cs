5f552c401fb39729d7813515ff38197b6aa0ec89
noremacskich <noremacskich@gmail.com>
2024-03-19 19:20:32 -0500

    Added Get and Put endpoint on character
    Changed the return types in general to be TypedResults which apparently works better with OpenAPI stuff.  Doesn't seem to be working, but it seems to be better practice. Standardized the return dtos to also help with that.

--- ExpressedRealms.Server/Program.cs
+++ ExpressedRealms.Server/Program.cs
@@ -1,6 +1,7 @@
 using ExpressedRealms.DB;
 using ExpressedRealms.DB.UserProfile.PlayerDBModels;
 using ExpressedRealms.Server.EndPoints;
+using ExpressedRealms.Server.EndPoints.CharacterEndPoints;
 using ExpressedRealms.Server.EndPoints.PlayerEndpoints;
 using ExpressedRealms.Server.Swagger;
 using FluentValidation;
