a944959a2d3b3b301f6ee461efe548182556afbb
noremacskich <noremacskich@gmail.com>
2024-03-24 14:46:20 -0500

    Grouped together all Auth Endpoints in Swagger and switched to using TypedResults

--- ExpressedRealms.Server/EndPoints/AuthEndPoints.cs
+++ ExpressedRealms.Server/EndPoints/AuthEndPoints.cs
@@ -1,6 +1,8 @@
 using System.Security.Claims;
 using ExpressedRealms.DB.UserProfile.PlayerDBModels;
 using Microsoft.AspNetCore.Antiforgery;
+using Microsoft.AspNetCore.Http.HttpResults;
+using SharpGrip.FluentValidation.AutoValidation.Endpoints.Extensions;
 
 namespace ExpressedRealms.Server.EndPoints;
 
@@ -8,21 +10,27 @@ internal static class AuthEndPoints
 {
     internal static void AddAuthEndPoints(this WebApplication app)
     {
-        app.MapGroup("auth").MapIdentityApi<User>();
-        app.MapGroup("auth").MapPost("/logoff", (HttpContext httpContext) => Results.SignOut());
-        app.MapGroup("auth").MapGet("/getAntiforgeryToken", (IAntiforgery antiforgery, HttpContext httpContext, ClaimsPrincipal user) =>
+        var endpointGroup = app.MapGroup("auth")
+            .AddFluentValidationAutoValidation()
+            .WithTags("Authentication")
+            .WithOpenApi();
+        
+        endpointGroup.MapIdentityApi<User>();
+        endpointGroup.MapPost("/logoff", (HttpContext httpContext) => Results.SignOut());
+        endpointGroup.MapGet("/getAntiforgeryToken", Results<NoContent, StatusCodeHttpResult>(IAntiforgery antiforgery, HttpContext httpContext, ClaimsPrincipal user) =>
         {
             var tokens = antiforgery.GetAndStoreTokens(httpContext);
 
             if (tokens.RequestToken is null)
             {
                 app.Logger.LogCritical("The anti-forgery token was not generated.");
-                return Results.StatusCode(500);
+                return TypedResults.StatusCode(500);
             }
             
             httpContext.Response.Cookies.Append("XSRF-TOKEN", tokens.RequestToken,
                 new CookieOptions() { HttpOnly = false });
-            return Results.Ok();
+            
+            return TypedResults.NoContent();
         });
     }
 }
