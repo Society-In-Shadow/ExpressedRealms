49f7f094eb2fa92f2f2bd6fb497949bab9dabf90
noremacskich <noremacskich@gmail.com>
2024-03-30 04:32:09 -0500

    Added Dropdown wrapper + added default slot to all wrappers

--- expressedrealms.client/src/FormWrappers/DropdownWrapper.vue
+++ expressedrealms.client/src/FormWrappers/DropdownWrapper.vue
@@ -1,9 +1,15 @@
 <script setup lang="ts">
 
-import InputText from "primevue/inputtext";
+import Dropdown from 'primevue/dropdown';
 import {computed} from "vue";
 
-const model = defineModel<string>({ required: true, default: "" });
+interface DropDown {
+  id: number,
+  name: string,
+  shortDescription: string
+}
+
+const model = defineModel({ required: true, default: {} });
 
 defineOptions({
   inheritAttrs: false
@@ -13,6 +19,14 @@ const props = defineProps({
   fieldName: {
     type: String,
     required: true,
+  },
+  options: {
+    type: Array,
+    required: true
+  },
+  optionLabel: {
+    type: String,
+    required: true
   },
   dataCyTag: {
     type: String,
@@ -36,8 +50,8 @@ const dataCyTagCalc = computed(() => {
 <template>
   <div class="mb-3">
     <label :for="dataCyTagCalc">{{ props.fieldName }}</label>
-    <InputText
-      :id="dataCyTagCalc" v-model="model" :data-cy="dataCyTagCalc" class="w-100"
+    <Dropdown
+      :id="dataCyTagCalc" v-model="model" :options="options" :optionLabel="optionLabel" :data-cy="dataCyTagCalc" class="w-100"
       :class="{ 'p-invalid': errorText }" v-bind="$attrs"
     />
     <small :data-cy="dataCyTagCalc + '-help'" class="text-danger">{{ errorText }}</small>
--- expressedrealms.client/src/FormWrappers/TextAreaWrapper.vue
+++ expressedrealms.client/src/FormWrappers/TextAreaWrapper.vue
@@ -43,6 +43,7 @@ const dataCyTagCalc = computed(() => {
       :class="{ 'p-invalid': errorText }" v-bind="$attrs" auto-resize
     />
     <small :data-cy="dataCyTagCalc + '-help'" class="text-danger">{{ errorText }}</small>
+    <slot></slot>
   </div>
 </template>
 
