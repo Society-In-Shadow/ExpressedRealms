659cf963770a6ddba5f008656127c4701516224e
noremacskich <noremacskich@gmail.com>
2024-04-20 02:03:53 -0500

    You should no longer be able to deselect an option, selecting options should now be much smoother

--- expressedrealms.client/src/components/characters/character/StatEditor.vue
+++ expressedrealms.client/src/components/characters/character/StatEditor.vue
@@ -11,6 +11,12 @@
   onMounted(() =>{
     axios.get(`/api/stats/${route.params.id}`)
         .then((response) => {
+          
+          response.data.forEach(function(stat) {
+            stat.showOptions = false;
+            stat.oldValue = stat.statLevel;
+          });
+          
           stats.value = response.data;
         })
   });
@@ -19,17 +25,26 @@
     return levels.find(x => x.level == level);
   }
 
+  function handleStatUpdate(stat){
+    // Don't allow them to unselect the option
+    if(stat.statLevel == undefined)
+      stat.statLevel = stat.oldValue;
+
+    stat.showOptions = !stat.showOptions
+    document.getElementById(stat.name).scrollIntoView(true)
+  }
 </script>
 
 <template>
 
-  <div v-for="stat in stats" style="max-width: 500px">
+  <div v-for="stat in stats" style="max-width: 500px" :id="stat.name">
     <h3>{{stat.name}}</h3>
     <div class="mb-3">{{stat.description}}</div>
     
-    <StatTile :state-info="getSelectedStatInfo(stat.statLevel, stat.statLevels)"></StatTile>
-    
-    <Listbox v-model="stat.statLevel" :options="stat.statLevels" option-value="level">
+    <div v-if="!stat.showOptions" class="p-listbox">
+      <StatTile :state-info="getSelectedStatInfo(stat.statLevel, stat.statLevels)" @click="stat.showOptions = !stat.showOptions" class="p-3"></StatTile>
+    </div>
+    <Listbox v-else v-model="stat.statLevel" :options="stat.statLevels" option-value="level" @change="handleStatUpdate(stat)">
       <template #option="slotProps">
         <StatTile :state-info="slotProps.option"></StatTile>
       </template>
