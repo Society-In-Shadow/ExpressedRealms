b8c3fd22427eff023005d0097c15a647a6820496
noremacskich <noremacskich@gmail.com>
2024-04-21 18:43:20 -0500

    Created skeleton wrapper to avoid having two identical structures when loading

---
+++ expressedrealms.client/src/FormWrappers/SkeletonWrapper.vue
@@ -0,0 +1,23 @@
+<script setup lang="ts">
+
+import Skeleton from 'primevue/skeleton';
+
+const props = defineProps({
+  showSkeleton: {
+    type: Boolean,
+    required: true,
+  },
+});
+
+</script>
+
+<template>
+  <div>
+    <skeleton v-if="showSkeleton" v-bind="$attrs"></skeleton>
+    <slot v-else></slot>
+  </div>
+</template>
+
+<style scoped>
+
+</style>
