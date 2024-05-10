10b05140685bbc50620d36b0b7776ae09bf8164b
noremacskich <noremacskich@gmail.com>
2024-05-10 00:17:01 -0500

    Created new dropdown specifically for info lookups

---
+++ expressedrealms.client/src/FormWrappers/DropdownInfoWrapper.vue
@@ -0,0 +1,74 @@
+<script setup lang="ts">
+
+import Dropdown from 'primevue/dropdown';
+import {computed} from "vue";
+import Skeleton from 'primevue/skeleton';
+import InputGroup from 'primevue/inputgroup';
+import Button from 'primevue/button'
+import Router from "@/router";
+
+const model = defineModel({ required: true, default: {}, type: Object });
+
+defineOptions({
+  inheritAttrs: false
+})
+
+const props = defineProps({
+  fieldName: {
+    type: String,
+    required: true,
+  },
+  options: {
+    type: Array,
+    required: true
+  },
+  optionLabel: {
+    type: String,
+    required: true
+  },
+  dataCyTag: {
+    type: String,
+    default: ""
+  },
+  errorText: {
+    type: String,
+    default: ""
+  },
+  showSkeleton: {
+    type: Boolean,
+    default: false
+  },
+  redirectUrl:{
+    type: String,
+    required: true
+  }
+});
+
+const dataCyTagCalc = computed(() => {
+  if(props.dataCyTag != ""){
+    return props.dataCyTag;
+  }
+  return props.fieldName.replace(" ", "-").toLowerCase();
+});
+
+function redirectUser() {
+  Router.push(props.redirectUrl);
+}
+
+</script>
+
+<template>
+  <div class="mb-3">
+    <label :for="dataCyTagCalc">{{ props.fieldName }}</label>
+    <Skeleton v-if="showSkeleton" :id="dataCyTagCalc + '-skeleton'" class="w-100" height="3em" />
+    <InputGroup v-else>
+      <Dropdown
+          :id="dataCyTagCalc" v-model="model" :options="options" :option-label="optionLabel" :data-cy="dataCyTagCalc"
+          class="w-100" :class="{ 'p-invalid': errorText }" v-bind="$attrs"
+      />
+      <Button icon="pi pi-info-circle" severity="info" @click="redirectUser"></Button>
+    </InputGroup>
+    <small :data-cy="dataCyTagCalc + '-help'" class="text-danger">{{ errorText }}</small>
+    <slot />
+  </div>
+</template>
