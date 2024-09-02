5eeb26727d591028890bb5cf0c764ee0fe88f35a
noremacskich <noremacskich@gmail.com>
2024-09-02 17:13:34 -0500

    Fixed login check for prod

--- client/src/services/Authentication.ts
+++ client/src/services/Authentication.ts
@@ -16,7 +16,7 @@ export function resetEmailConfirmation() {
 }
 
 export function isLoggedIn() {
-    document.cookie = ".AspNetCore.Identity.Bearer=1;path=/";
+    document.cookie = ".AspNetCore.Identity.Bearer=1;path=/;domain=.societyinshadows.org;";
     if(document.cookie.indexOf(".AspNetCore.Identity.Bearer") >= 0){
         document.cookie = ".AspNetCore.Identity.Bearer=; expires=Thu, 01 Jan 1970 00:00:00 UTC; path=/;";
         return false
