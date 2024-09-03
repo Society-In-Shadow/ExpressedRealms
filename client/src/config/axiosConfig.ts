50a942c3b6e0778f9de8d716873ff14113dac50b
noremacskich <noremacskich@gmail.com>
2024-09-03 00:19:45 -0500

    Base URL should now be configurable
    Removed default value on ARG just in case it was overriding the passed in value

--- client/src/config/axiosConfig.ts
+++ client/src/config/axiosConfig.ts
@@ -26,7 +26,7 @@ function setupErrorHandlingInterceptors() {
 }
 
 function setAPIUrl() {
-    axios.defaults.baseURL = "https://api.societyinshadows.org"
+    axios.defaults.baseURL = import.meta.env.VITE_API_BASE_URL
     axios.defaults.withCredentials = true;
 }
 
