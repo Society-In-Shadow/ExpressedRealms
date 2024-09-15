ba389c97985da3d36cb7775a7f57e4b635ef88bb
noremacskich <noremacskich@gmail.com>
2024-09-15 01:46:52 -0500

    removed my user name from the update scripts

--- api/ExpressedRealms.DB/Migrations/20240915043328_GiveMyUserDBPermissions.cs
+++ api/ExpressedRealms.DB/Migrations/20240915043328_GiveMyUserDBPermissions.cs
@@ -11,13 +11,13 @@ namespace ExpressedRealms.DB.Migrations
         protected override void Up(MigrationBuilder migrationBuilder)
         {
             migrationBuilder.Sql(
-                "GRANT SELECT, INSERT, UPDATE, DELETE ON ALL TABLES IN SCHEMA public TO \"NoremacSkich_live.com#EXT#@NoremacSkichlive.onmicrosoft.com\";");
+                "GRANT SELECT, INSERT, UPDATE, DELETE ON ALL TABLES IN SCHEMA public TO \"\";");
             migrationBuilder.Sql(
-                "ALTER DEFAULT PRIVILEGES FOR ROLE aad_postgresql_86cde IN SCHEMA public GRANT SELECT, INSERT, UPDATE, DELETE ON TABLES TO \"NoremacSkich_live.com#EXT#@NoremacSkichlive.onmicrosoft.com\";");
+                "ALTER DEFAULT PRIVILEGES FOR ROLE aad_postgresql_86cde IN SCHEMA public GRANT SELECT, INSERT, UPDATE, DELETE ON TABLES TO \"\";");
             migrationBuilder.Sql(
-                "GRANT SELECT, INSERT, UPDATE, DELETE ON ALL TABLES IN SCHEMA efcore TO \"NoremacSkich_live.com#EXT#@NoremacSkichlive.onmicrosoft.com\";");
+                "GRANT SELECT, INSERT, UPDATE, DELETE ON ALL TABLES IN SCHEMA efcore TO \"\";");
             migrationBuilder.Sql(
-                "ALTER DEFAULT PRIVILEGES FOR ROLE aad_postgresql_86cde IN SCHEMA efcore GRANT SELECT, INSERT, UPDATE, DELETE ON TABLES TO \"NoremacSkich_live.com#EXT#@NoremacSkichlive.onmicrosoft.com\";");
+                "ALTER DEFAULT PRIVILEGES FOR ROLE aad_postgresql_86cde IN SCHEMA efcore GRANT SELECT, INSERT, UPDATE, DELETE ON TABLES TO \"\";");
 
         }
 
