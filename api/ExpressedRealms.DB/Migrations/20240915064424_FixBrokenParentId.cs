298d861325c964422392d88db0ab5e9b6e1f974b
noremacskich <noremacskich@gmail.com>
2024-09-15 01:47:07 -0500

    Created script to fix existing db

---
+++ api/ExpressedRealms.DB/Migrations/20240915064424_FixBrokenParentId.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace ExpressedRealms.DB.Migrations
+{
+    /// <inheritdoc />
+    public partial class FixBrokenParentId : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.Sql(
+                "ALTER TABLE public.\"ExpressionSections\" ALTER COLUMN \"ParentId\" SET NOT NULL;");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
