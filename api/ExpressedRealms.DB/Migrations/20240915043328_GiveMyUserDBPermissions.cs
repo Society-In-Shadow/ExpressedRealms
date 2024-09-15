61fb7b5a0e8892962e21442f110f72656f6a2461
noremacskich <noremacskich@gmail.com>
2024-09-15 00:37:22 -0500

    Added migration to allow my user to modify things
    Included ability to deploy from pipeline

---
+++ api/ExpressedRealms.DB/Migrations/20240915043328_GiveMyUserDBPermissions.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace ExpressedRealms.DB.Migrations
+{
+    /// <inheritdoc />
+    public partial class GiveMyUserDBPermissions : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.Sql(
+                "GRANT SELECT, INSERT, UPDATE, DELETE ON ALL TABLES IN SCHEMA public TO \"NoremacSkich_live.com#EXT#@NoremacSkichlive.onmicrosoft.com\";");
+            migrationBuilder.Sql(
+                "ALTER DEFAULT PRIVILEGES FOR ROLE aad_postgresql_86cde IN SCHEMA public GRANT SELECT, INSERT, UPDATE, DELETE ON TABLES TO \"NoremacSkich_live.com#EXT#@NoremacSkichlive.onmicrosoft.com\";");
+            migrationBuilder.Sql(
+                "GRANT SELECT, INSERT, UPDATE, DELETE ON ALL TABLES IN SCHEMA efcore TO \"NoremacSkich_live.com#EXT#@NoremacSkichlive.onmicrosoft.com\";");
+            migrationBuilder.Sql(
+                "ALTER DEFAULT PRIVILEGES FOR ROLE aad_postgresql_86cde IN SCHEMA efcore GRANT SELECT, INSERT, UPDATE, DELETE ON TABLES TO \"NoremacSkich_live.com#EXT#@NoremacSkichlive.onmicrosoft.com\";");
+
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
