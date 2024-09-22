fca2bb1383e2f488ab7095cdde908a4c185d086f
noremacskich <noremacskich@gmail.com>
2024-09-22 01:02:40 -0500

    Slight tweaking to test deployments

--- api/ExpressedRealms.Server/Program.cs
+++ api/ExpressedRealms.Server/Program.cs
@@ -58,7 +58,7 @@ try
     builder.Host.UseSerilog();
     
     // Since we are in a container, we need to keep track of the data keys manually
-    string blobStorageEndpoint = Environment.GetEnvironmentVariable("AZURE_STORAGEBLOB_RESOURCEENDPOINT") ?? "";
+    var blobStorageEndpoint = Environment.GetEnvironmentVariable("AZURE_STORAGEBLOB_RESOURCEENDPOINT") ?? "";
     if (!string.IsNullOrEmpty(blobStorageEndpoint))
     {
         builder.Services.AddDataProtection()
