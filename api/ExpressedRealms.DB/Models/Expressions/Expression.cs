3762ce84af12f61269aa40380874b81a10ba4862
noremacskich <noremacskich@gmail.com>
2024-10-12 01:30:52 -0500

    Expressions now have a publish status type

--- api/ExpressedRealms.DB/Models/Expressions/Expression.cs
+++ api/ExpressedRealms.DB/Models/Expressions/Expression.cs
@@ -9,9 +9,11 @@ public class Expression : ISoftDelete
     public string Name { get; set; } = null!;
     public string ShortDescription { get; set; } = null!;
     public string NavMenuImage { get; set; } = null!;
+    public int PublishStatusId { get; set; }
     public bool IsDeleted { get; set; }
     public DateTimeOffset? DeletedAt { get; set; }
 
+    public virtual ExpressionPublishStatus PublishStatus { get; set; } = null!;
     public virtual List<ExpressionSection> ExpressionSections { get; set; } = null!;
     public virtual List<Character> Characters { get; set; } = null!;
 
