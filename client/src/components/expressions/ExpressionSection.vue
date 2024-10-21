f15a91cbf360b5533ec9d0e31fed1a2a0a86baf3
noremacskich <noremacskich@gmail.com>
2024-10-21 01:32:33 -0500

    Fixed console warning about missing property

--- client/src/components/expressions/ExpressionSection.vue
+++ client/src/components/expressions/ExpressionSection.vue
@@ -27,7 +27,7 @@ const props = defineProps({
   <div v-for="(value) in props.sections" :key="value.id">
     <EditExpressionSection :sectionInfo="value" :current-level="currentLevel" :show-skeleton="showSkeleton" :show-edit="showEdit"/>
     <div>
-      <ExpressionSection v-if="value.subSections" :sections="value.subSections" :current-level="props.currentLevel + 1" :show-skeleton="showSkeleton" />
+      <ExpressionSection v-if="value.subSections" :sections="value.subSections" :current-level="props.currentLevel + 1" :show-skeleton="showSkeleton" :show-edit="showEdit"/>
     </div>
   </div>
 </template>
