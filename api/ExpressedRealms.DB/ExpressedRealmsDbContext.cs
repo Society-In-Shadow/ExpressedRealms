a4de481e70093340bce68ab6b94e6962c2d1288e
noremacskich <noremacskich@gmail.com>
2024-10-26 00:48:06 -0500

    Refix namespace scope stuff

--- api/ExpressedRealms.DB/ExpressedRealmsDbContext.cs
+++ api/ExpressedRealms.DB/ExpressedRealmsDbContext.cs
@@ -8,55 +8,56 @@ using ExpressedRealms.DB.UserProfile.PlayerDBModels;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.EntityFrameworkCore;
 
-namespace ExpressedRealms.DB;
-
-[AuditDbContext(Mode = AuditOptionMode.OptIn)]
-public class ExpressedRealmsDbContext : AuditIdentityDbContext<IdentityUser>
+namespace ExpressedRealms.DB
 {
-    protected override void OnModelCreating(ModelBuilder builder)
+    [AuditDbContext(Mode = AuditOptionMode.OptIn)]
+    public class ExpressedRealmsDbContext : AuditIdentityDbContext<IdentityUser>
     {
-        base.OnModelCreating(builder);
-        builder.ApplyConfiguration(new CharacterConfiguration());
-        builder.ApplyConfiguration(new PlayerConfiguration());
-        builder.ApplyConfiguration(new ExpressionConfiguration());
-        builder.ApplyConfiguration(new ExpressionSectionsConfiguration());
-        builder.ApplyConfiguration(new ExpressionSectionTypeConfiguration());
-        builder.ApplyConfiguration(new StatTypeConfiguration());
-        builder.ApplyConfiguration(new StatLevelConfiguration());
-        builder.ApplyConfiguration(new StatDescriptionMappingConfiguration());
-        builder.ApplyConfiguration(new ExpressionPublishStatusConfiguration());
+        protected override void OnModelCreating(ModelBuilder builder)
+        {
+            base.OnModelCreating(builder);
+            builder.ApplyConfiguration(new CharacterConfiguration());
+            builder.ApplyConfiguration(new PlayerConfiguration());
+            builder.ApplyConfiguration(new ExpressionConfiguration());
+            builder.ApplyConfiguration(new ExpressionSectionsConfiguration());
+            builder.ApplyConfiguration(new ExpressionSectionTypeConfiguration());
+            builder.ApplyConfiguration(new StatTypeConfiguration());
+            builder.ApplyConfiguration(new StatLevelConfiguration());
+            builder.ApplyConfiguration(new StatDescriptionMappingConfiguration());
+            builder.ApplyConfiguration(new ExpressionPublishStatusConfiguration());
 
-        builder.ApplyConfiguration(new CharacterSkillsMappingConfiguration());
-        builder.ApplyConfiguration(new ModifierTypeConfiguration());
-        builder.ApplyConfiguration(new SkillLevelConfiguration());
-        builder.ApplyConfiguration(new SkillLevelBenefitConfiguration());
-        builder.ApplyConfiguration(new SkillSubTypeConfiguration());
-        builder.ApplyConfiguration(new SkillTypeConfiguration());
+            builder.ApplyConfiguration(new CharacterSkillsMappingConfiguration());
+            builder.ApplyConfiguration(new ModifierTypeConfiguration());
+            builder.ApplyConfiguration(new SkillLevelConfiguration());
+            builder.ApplyConfiguration(new SkillLevelBenefitConfiguration());
+            builder.ApplyConfiguration(new SkillSubTypeConfiguration());
+            builder.ApplyConfiguration(new SkillTypeConfiguration());
+        }
+
+        public ExpressedRealmsDbContext(DbContextOptions<ExpressedRealmsDbContext> options)
+            : base(options)
+        {
+            Audit.Core.Configuration.Setup()
+                .UseEntityFramework(x => x
+                    .AuditTypeNameMapper(typeName => typeName + "_AuditTrail"));
+        }
+
+        public DbSet<Character> Characters { get; set; }
+        public DbSet<Player> Players { get; set; }
+        public DbSet<Expression> Expressions { get; set; }
+        public DbSet<ExpressionSection> ExpressionSections { get; set; }
+        public DbSet<ExpressionSectionType> ExpressionSectionTypes { get; set; }
+        public DbSet<ExpressionPublishStatus> ExpressionPublishStatus { get; set; }
+
+        public DbSet<StatType> StateTypes { get; set; }
+        public DbSet<StatLevel> StatLevels { get; set; }
+        public DbSet<StatDescriptionMapping> StatDescriptionMappings { get; set; }
+
+        public DbSet<CharacterSkillsMapping> CharacterSkillsMappings { get; set; }
+        public DbSet<ModifierType> ModifierTypes { get; set; }
+        public DbSet<SkillLevel> SkillLevels { get; set; }
+        public DbSet<SkillLevelBenefit> SkillLevelBenefits { get; set; }
+        public DbSet<SkillSubType> SkillSubTypes { get; set; }
+        public DbSet<SkillType> SkillTypes { get; set; }
     }
-
-    public ExpressedRealmsDbContext(DbContextOptions<ExpressedRealmsDbContext> options)
-        : base(options)
-    {
-        Audit.Core.Configuration.Setup()
-            .UseEntityFramework(x => x
-                .AuditTypeNameMapper(typeName => typeName + "_AuditTrail"));
-    }
-
-    public DbSet<Character> Characters { get; set; }
-    public DbSet<Player> Players { get; set; }
-    public DbSet<Expression> Expressions { get; set; }
-    public DbSet<ExpressionSection> ExpressionSections { get; set; }
-    public DbSet<ExpressionSectionType> ExpressionSectionTypes { get; set; }
-    public DbSet<ExpressionPublishStatus> ExpressionPublishStatus { get; set; }
-
-    public DbSet<StatType> StateTypes { get; set; }
-    public DbSet<StatLevel> StatLevels { get; set; }
-    public DbSet<StatDescriptionMapping> StatDescriptionMappings { get; set; }
-
-    public DbSet<CharacterSkillsMapping> CharacterSkillsMappings { get; set; }
-    public DbSet<ModifierType> ModifierTypes { get; set; }
-    public DbSet<SkillLevel> SkillLevels { get; set; }
-    public DbSet<SkillLevelBenefit> SkillLevelBenefits { get; set; }
-    public DbSet<SkillSubType> SkillSubTypes { get; set; }
-    public DbSet<SkillType> SkillTypes { get; set; }
 }
