3868fcdff1ab2862e4a6257f9a453ca6a3301b5a
noremacskich <noremacskich@gmail.com>
2025-01-17 02:11:19 -0600

    Moved audit configuration to separate configuration class

--- api/ExpressedRealms.DB/ExpressedRealmsDbContext.cs
+++ api/ExpressedRealms.DB/ExpressedRealmsDbContext.cs
@@ -2,6 +2,7 @@ using System.Text.Json;
 using Audit.Core;
 using Audit.EntityFramework;
 using ExpressedRealms.DB.Characters;
+using ExpressedRealms.DB.Configuration;
 using ExpressedRealms.DB.Interceptors;
 using ExpressedRealms.DB.Models.Expressions;
 using ExpressedRealms.DB.Models.Skills;
@@ -41,45 +42,7 @@ namespace ExpressedRealms.DB
         public ExpressedRealmsDbContext(DbContextOptions<ExpressedRealmsDbContext> options)
             : base(options)
         {
-            Audit.Core.Configuration.Setup()
-                .UseEntityFramework(x => x
-                    .AuditTypeExplicitMapper(m => m
-                        .Map<ExpressionSection, ExpressionSectionAuditTrail>((section, audit) =>
-                        {
-                            audit.SectionId = section.Id;
-                            audit.ExpressionId = section.ExpressionId;
-                            return true;
-                        })
-                        .AuditEntityAction<IAuditTable>((evt, entry, audit) =>
-                        {
-                            audit.Action = entry.Action;
-                            audit.Timestamp = DateTime.UtcNow;
-                            audit.UserId = evt.Environment.UserName;
-
-                            var changes = new List<ChangedRecord>();
-                            if (string.Compare(audit.Action, "insert", StringComparison.InvariantCultureIgnoreCase) == 0)
-                            {
-                                changes = entry.ColumnValues.Select(x => new ChangedRecord(x.Key, null, x.Value?.ToString()))
-                                    .ToList();
-                            }
-                            else
-                            {
-                                changes = entry.Changes
-                                    .Where(x =>
-                                        x.NewValue == null
-                                            ? x.OriginalValue != null
-                                            : !x.NewValue.Equals(x.OriginalValue))
-                                    .Select(x => new ChangedRecord(x.ColumnName, x.OriginalValue?.ToString(),
-                                        x.NewValue?.ToString()))
-                                    .ToList();
-                            }
-                            
-                            audit.ChangedProperties = JsonSerializer.Serialize(changes);
-                            
-                            return true;
-                        })
-                    )
-                    .IgnoreMatchedProperties(true));
+            SetupDatabaseAudit.SetupAudit();
         }
 
         public DbSet<Character> Characters { get; set; }
