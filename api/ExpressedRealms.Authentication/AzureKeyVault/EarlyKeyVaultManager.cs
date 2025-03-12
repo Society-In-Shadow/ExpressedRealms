a0a68d84e99a2a04fcf9f9a6efabf293ef271a2e
noremacskich <noremacskich@gmail.com>
2025-03-11 20:40:08 -0500

    Revert "Think I fixed permission issues, so trying the original approach again"
    This reverts commit 64717e861dfae74c8bcbd22e82419a263ee8b04b.

--- api/ExpressedRealms.Authentication/AzureKeyVault/EarlyKeyVaultManager.cs
+++ api/ExpressedRealms.Authentication/AzureKeyVault/EarlyKeyVaultManager.cs
@@ -1,5 +1,3 @@
-using Azure.Identity;
-using Azure.Security.KeyVault.Secrets;
 using Dapr.Client;
 using ExpressedRealms.Authentication.AzureKeyVault.Secrets.Config;
 
@@ -7,8 +5,8 @@ namespace ExpressedRealms.Authentication.AzureKeyVault;
 
 public class EarlyKeyVaultManager
 {
-    private readonly SecretClient _secretClient;
-    //private readonly DaprClient _secretClient;
+    
+    private readonly DaprClient _secretClient;
     private readonly bool _isProduction;
     
     public EarlyKeyVaultManager(bool isProduction)
@@ -16,13 +14,13 @@ public class EarlyKeyVaultManager
         _isProduction = isProduction;
         if (isProduction)
         {
-            //_secretClient = new DaprClientBuilder().Build();
-            var keyVaultUri = Environment.GetEnvironmentVariable("AZURE_KEYVAULT_RESOURCEENDPOINT");
+            _secretClient = new DaprClientBuilder().Build();
+            /*var keyVaultUri = Environment.GetEnvironmentVariable("AZURE_KEYVAULT_RESOURCEENDPOINT");
             if (string.IsNullOrEmpty(keyVaultUri) || !Uri.IsWellFormedUriString(keyVaultUri, UriKind.Absolute))
             {
                 throw new InvalidOperationException("The Azure Key Vault endpoint URI is not valid. Ensure 'AZURE_KEYVAULT_RESOURCEENDPOINT' is set and correctly formatted.");
             }
-            _secretClient = new SecretClient(new Uri(keyVaultUri), new DefaultAzureCredential());
+            _secretClient = new SecretClient(new Uri(keyVaultUri), new DefaultAzureCredential())*/;
         }
     }
     
@@ -36,12 +34,11 @@ public class EarlyKeyVaultManager
             var secretStoreName = "azure-key-vault"; // The name of the configured Dapr secret store
                 
             // Cache miss: Fetch secret from Azure Key Vault
-            //var keyValueSecret = (await _secretClient.GetSecretAsync(secretStoreName, secretName.Name)).Values.FirstOrDefault();
-            var keyValueSecret = await _secretClient.GetSecretAsync(secretStoreName);
-            if (keyValueSecret.HasValue == false)
+            var keyValueSecret = (await _secretClient.GetSecretAsync(secretStoreName, secretName.Name)).Values.FirstOrDefault();
+            if (keyValueSecret is null)
                 throw new Exception($"Secret {secretName.Name} not found in Key Vault");
             
-            secret = keyValueSecret.Value.Value;
+            secret = keyValueSecret;
         }
         else
         {
