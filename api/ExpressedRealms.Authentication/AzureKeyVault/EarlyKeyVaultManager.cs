64717e861dfae74c8bcbd22e82419a263ee8b04b
noremacskich <noremacskich@gmail.com>
2025-03-11 20:36:14 -0500

    Think I fixed permission issues, so trying the original approach again

--- api/ExpressedRealms.Authentication/AzureKeyVault/EarlyKeyVaultManager.cs
+++ api/ExpressedRealms.Authentication/AzureKeyVault/EarlyKeyVaultManager.cs
@@ -1,3 +1,5 @@
+using Azure.Identity;
+using Azure.Security.KeyVault.Secrets;
 using Dapr.Client;
 using ExpressedRealms.Authentication.AzureKeyVault.Secrets.Config;
 
@@ -5,8 +7,8 @@ namespace ExpressedRealms.Authentication.AzureKeyVault;
 
 public class EarlyKeyVaultManager
 {
-    
-    private readonly DaprClient _secretClient;
+    private readonly SecretClient _secretClient;
+    //private readonly DaprClient _secretClient;
     private readonly bool _isProduction;
     
     public EarlyKeyVaultManager(bool isProduction)
@@ -14,13 +16,13 @@ public class EarlyKeyVaultManager
         _isProduction = isProduction;
         if (isProduction)
         {
-            _secretClient = new DaprClientBuilder().Build();
-            /*var keyVaultUri = Environment.GetEnvironmentVariable("AZURE_KEYVAULT_RESOURCEENDPOINT");
+            //_secretClient = new DaprClientBuilder().Build();
+            var keyVaultUri = Environment.GetEnvironmentVariable("AZURE_KEYVAULT_RESOURCEENDPOINT");
             if (string.IsNullOrEmpty(keyVaultUri) || !Uri.IsWellFormedUriString(keyVaultUri, UriKind.Absolute))
             {
                 throw new InvalidOperationException("The Azure Key Vault endpoint URI is not valid. Ensure 'AZURE_KEYVAULT_RESOURCEENDPOINT' is set and correctly formatted.");
             }
-            _secretClient = new SecretClient(new Uri(keyVaultUri), new DefaultAzureCredential())*/;
+            _secretClient = new SecretClient(new Uri(keyVaultUri), new DefaultAzureCredential());
         }
     }
     
@@ -34,11 +36,12 @@ public class EarlyKeyVaultManager
             var secretStoreName = "azure-key-vault"; // The name of the configured Dapr secret store
                 
             // Cache miss: Fetch secret from Azure Key Vault
-            var keyValueSecret = (await _secretClient.GetSecretAsync(secretStoreName, secretName.Name)).Values.FirstOrDefault();
-            if (keyValueSecret is null)
+            //var keyValueSecret = (await _secretClient.GetSecretAsync(secretStoreName, secretName.Name)).Values.FirstOrDefault();
+            var keyValueSecret = await _secretClient.GetSecretAsync(secretStoreName);
+            if (keyValueSecret.HasValue == false)
                 throw new Exception($"Secret {secretName.Name} not found in Key Vault");
             
-            secret = keyValueSecret;
+            secret = keyValueSecret.Value.Value;
         }
         else
         {
