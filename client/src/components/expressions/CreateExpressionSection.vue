ba1181b4b8368b3660737812beff90cae4cdcdcf
noremacskich <noremacskich@gmail.com>
2025-03-30 03:53:09 -0500

    UI now has a dedicated section for the overall expression information
    Will eventually expand to have pic, but that can be for later
    
    Have two tabs now, one for background info and one for powers, powers is a stub for now

--- client/src/components/expressions/CreateExpressionSection.vue
+++ client/src/components/expressions/CreateExpressionSection.vue
@@ -1,7 +1,7 @@
 <script setup lang="ts">
 
 import Button from "primevue/button";
-import {onMounted, ref} from "vue";
+import {onMounted, ref, watch} from "vue";
 import axios from "axios";
 import {useForm} from "vee-validate";
 import {object, string} from "yup";
@@ -22,11 +22,26 @@ const props = defineProps({
   parentId: {
     type: Number
   },
+  addExpressionHeader:{
+    type: Boolean
+  }
 });
 
 onMounted(() => {
-  loadSectionInfo();
+  if(expressionInfo.isDoneLoading) {
+    loadSectionInfo();
+  }
 })
+
+const stopWatch = watch(
+    () => expressionInfo.isDoneLoading,
+    (newValue) => {
+      if (newValue) {
+        console.log("isDoneLoading changed to true");
+        loadSectionInfo();
+      }
+    }
+);
 
 const showOptionLoader = ref(true);
 const sectionTypeOptions = ref([]);
@@ -44,8 +59,13 @@ function loadSectionInfo(){
   if(!showOptionLoader.value) return; // Don't load in 2nd time
   axios.get(`/expressionSubSections/${expressionInfo.currentExpressionId}/0/options`)
       .then(async (response) => {
+        console.log("loaded data");
         sectionTypeOptions.value = response.data.sectionTypes;
         showOptionLoader.value = false;
+
+        if(props.addExpressionHeader) {
+          sectionType.value = sectionTypeOptions.value.find(obj => obj.name === "Expression");
+        }
       });
 }
 
@@ -78,14 +98,16 @@ const onSubmit = handleSubmit((values) => {
   });
 });
 
+const nameField = props.addExpressionHeader ? 'Expression Name' : 'Name';
+
 </script>
 
 <template>
   <div class="m-2">
     <form @submit="onSubmit">
-      <InputTextWrapper v-model="name" field-name="Name" :error-text="errors.name" />
+      <InputTextWrapper v-model="name" :field-name="nameField" :error-text="errors.name" />
       <EditorWrapper v-model="content" field-name="Content" :error-text="errors.content" />
-      <DropdownWrapper
+      <DropdownWrapper v-if="!props.addExpressionHeader"
         v-model="sectionType" option-label="name" :options="sectionTypeOptions" field-name="Section Types" :show-skeleton="showOptionLoader"
         :error-text="errors.sectionType"
       />
@@ -93,7 +115,7 @@ const onSubmit = handleSubmit((values) => {
         <div class="col-flex flex-grow-1">
           <div class="float-end">
             <Button label="Reset" class="m-2" @click="reset()" />
-            <Button label="Cancel" class="m-2" @click="cancelEdit()" />
+            <Button v-if="!props.addExpressionHeader" label="Cancel" class="m-2" @click="cancelEdit()" />
             <Button label="Add" class="m-2" @click="onSubmit" />
           </div>
         </div>
