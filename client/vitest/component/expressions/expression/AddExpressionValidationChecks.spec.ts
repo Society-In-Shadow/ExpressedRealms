1318a9350694f6b81899eb030cc7f17c3c442880
noremacskich <noremacskich@gmail.com>
2025-04-14 02:11:29 -0500

    Added missing tests to Add Expression Validation tests + moved it

--- client/vitest/component/expressions/expression/AddExpressionValidationChecks.spec.ts
+++ client/vitest/component/expressions/expression/AddExpressionValidationChecks.spec.ts
@@ -1,5 +1,6 @@
 import {describe, it, expect} from "vitest";
 import {nameField, shortDescriptionField, navMenuImageField, handleSubmit} from "../../../../src/components/expressions/expression/AddExpressionValidation";
+import {name} from "../../../../src/components/expressions/powers/Validations/AddPowerValidations";
 
 describe("Add Expression Schema - Field Validations", () => {
     
@@ -8,12 +9,16 @@ describe("Add Expression Schema - Field Validations", () => {
             nameField.field.value = "a".repeat(51);
             await handleSubmit(() => {})();
             expect(nameField.error.value).toEqual("Name must be at most 50 characters");
-
         });
         it("Says it's required when not filled in", async () => {
             nameField.field.value = "";
             await handleSubmit(() => {})();
             expect(nameField.error.value).toEqual("Name is a required field");
+        });
+        it("No Errors when it's a valid value", async () => {
+            nameField.field.value = "asdf";
+            await handleSubmit(() => {})();
+            expect([undefined, '']).toContain(name.error.value);
         });
         it("Label is correct", async () => {
             expect(nameField.label).toEqual("Name");
@@ -25,7 +30,6 @@ describe("Add Expression Schema - Field Validations", () => {
             shortDescriptionField.field.value = "a".repeat(126);
             await handleSubmit(() => {})();
             expect(shortDescriptionField.error.value).toEqual("Short Description must be at most 125 characters");
-
         });
         it("Says it's required when not filled in", async () => {
             shortDescriptionField.field.value = "";
@@ -35,6 +39,11 @@ describe("Add Expression Schema - Field Validations", () => {
         it("Label is correct", async () => {
             expect(shortDescriptionField.label).toEqual("Short Description");
         })
+        it("No Errors when it's a valid value", async () => {
+            shortDescriptionField.field.value = "asdf";
+            await handleSubmit(() => {})();
+            expect([undefined, '']).toContain(shortDescriptionField.error.value);
+        });
     });
     
     describe("Nav Menu Icon", () => {
@@ -45,6 +54,11 @@ describe("Add Expression Schema - Field Validations", () => {
             navMenuImageField.field.value = "";
             await handleSubmit(() => {})();
             await expect(navMenuImageField.error.value).toEqual("Nav Menu Icon is a required field");
+        });
+        it("No Errors when it's a valid value", async () => {
+            navMenuImageField.field.value = "asdf";
+            await handleSubmit(() => {})();
+            expect([undefined, '']).toContain(navMenuImageField.error.value);
         });
         it("Label is correct", async () => {
             expect(navMenuImageField.label).toEqual("Nav Menu Icon");
