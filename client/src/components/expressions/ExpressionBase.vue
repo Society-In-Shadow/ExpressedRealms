47b3ac611142b41a4374c34eae4e02dbd2123f65
noremacskich <noremacskich@gmail.com>
2025-04-14 03:26:02 -0500

    Got power tab up and running, with ability to add power
    The actual submit needs to be fixed, but everything seems to be working now

--- client/src/components/expressions/ExpressionBase.vue
+++ client/src/components/expressions/ExpressionBase.vue
@@ -22,6 +22,8 @@ import '@he-tree/vue/style/default.css'
 import '@he-tree/vue/style/material-design.css'
 import ExpressionToC from "@/components/expressions/ExpressionToC.vue";
 import EditExpressionSection from "@/components/expressions/EditExpressionSection.vue";
+import PowerTab from "@/components/expressions/powers/PowerTab.vue";
+
 let sections = ref([
   {
     id: 1,
@@ -145,10 +147,7 @@ onBeforeRouteUpdate(async (to, from) => {
                   </article>
                 </TabPanel>
                 <TabPanel value="1">
-                  <p class="m-0">
-                    Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae ab illo inventore veritatis et quasi architecto beatae vitae dicta sunt explicabo. Nemo enim
-                    ipsam voluptatem quia voluptas sit aspernatur aut odit aut fugit, sed quia consequuntur magni dolores eos qui ratione voluptatem sequi nesciunt. Consectetur, adipisci velit, sed quia non numquam eius modi.
-                  </p>
+                  <PowerTab :expression-id="expressionInfo.currentExpressionId" />
                 </TabPanel>
               </TabPanels>
             </Tabs>
---
+++ client/src/components/expressions/powers/PowerTab.vue
@@ -0,0 +1,19 @@
+<script setup lang="ts">
+
+import AddPower from "@/components/expressions/powers/AddPower.vue";
+
+const props = defineProps({
+  expressionId: {
+    type: Number
+  }
+});
+
+</script>
+
+<template>
+ <AddPower :expression-id="props.expressionId" />
+</template>
+
+<style scoped>
+
+</style>
