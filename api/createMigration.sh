86cac7b91a98de069275183a67621d24fe0707c6
noremacskich <noremacskich@gmail.com>
2025-05-23 04:48:48 -0500

    Fixed github action, and addressed megalinter finding

--- api/createMigration.sh
+++ api/createMigration.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-read -p "Enter migration name: (CamelCase)" MIGRATION_NAME
+read -r -p "Enter migration name (CamelCase): " MIGRATION_NAME
 dotnet ef migrations add "$MIGRATION_NAME" --project ExpressedRealms.DB --startup-project ExpressedRealms.MigrationProject
