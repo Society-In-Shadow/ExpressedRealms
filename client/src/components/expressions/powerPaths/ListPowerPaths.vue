9f335ebcf9666e845cfc58dc063f623ec06f2f84
noremacskich <noremacskich@gmail.com>
2025-06-24 01:59:20 -0500

    Powers now support isReadOnly attribute
    Updated said components to respect isReadOnly

--- client/src/components/expressions/powerPaths/ListPowerPaths.vue
+++ client/src/components/expressions/powerPaths/ListPowerPaths.vue
@@ -29,25 +29,30 @@ const toggleAddPower = () => {
   showAddPower.value = !showAddPower.value;
 }
 
+const readOnly = ref(false);
+const toggleReadOnly = () => {
+  readOnly.value = !readOnly.value;
+}
+
 </script>
 
 <template>
-  <PowerPathReorder v-if="userInfo.hasUserRole(UserRoles.PowerManagementRole)" />
+  <PowerPathReorder v-if="userInfo.hasUserRole(UserRoles.PowerManagementRole)" @toggle-preview="toggleReadOnly" />
   <div v-for="path in powerPaths.powerPaths" :key="path.id">
     <Divider />
-    <ShowPowerPath :path="path" :expression-id="props.expressionId" />
+    <ShowPowerPath :path="path" :expression-id="props.expressionId" :is-read-only="readOnly" />
    
     <Divider />
     <h2>Powers</h2>
-    <ListPowers :power-path-id="path.id" :powers="path.powers" />
+    <ListPowers :power-path-id="path.id" :powers="path.powers" :is-read-only="readOnly" />
   </div>
 
   <Button
-    v-if="!showAddPower && userInfo.hasUserRole(UserRoles.PowerManagementRole)" class="w-100 m-2"
+    v-if="!showAddPower && userInfo.hasUserRole(UserRoles.PowerManagementRole) && !readOnly" class="w-100 m-2"
     label="Add Power Path" @click="toggleAddPower"
   />
   <AddPowerPath
-    v-if="showAddPower && userInfo.hasUserRole(UserRoles.PowerManagementRole)"
+    v-if="showAddPower && userInfo.hasUserRole(UserRoles.PowerManagementRole) && !readOnly"
     :expression-id="props.expressionId" @canceled="toggleAddPower"
   />
 </template>
