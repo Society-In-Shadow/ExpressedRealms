7aae1fa84f0df6d668ff011185bbb596a6633273
noremacskich <noremacskich@gmail.com>
2025-07-03 09:22:16 -0500

    Created missing use case

---
+++ api/ExpressedRealms.Powers.Repository/PowerPrerequisites/GetPrerequisiteUseCase/IGetPrerequisiteUseCase.cs
@@ -0,0 +1,6 @@
+using ExpressedRealms.Shared;
+using FluentResults;
+
+namespace ExpressedRealms.Powers.Repository.PowerPrerequisites.GetPrerequisiteUseCase;
+
+public interface IGetPrerequisiteUseCase : IGenericUseCase<Result<GetPrerequisiteData?>, GetPrerequisiteModel> { }
