e468c14f6fb732afd1da8e5eef5dfc37969300fc
noremacskich <noremacskich@gmail.com>
2025-07-03 12:39:16 -0500

    Prerequisites should now be passed through where needed

---
+++ api/ExpressedRealms.Powers.Repository/Powers/DTOs/PowerList/PrerequisiteDetails.cs
@@ -0,0 +1,7 @@
+namespace ExpressedRealms.Powers.Repository.Powers.DTOs.PowerList;
+
+public class PrerequisiteDetails
+{
+    public int RequiredAmount { get; set; }
+    public List<string> Powers { get; set; }
+}
