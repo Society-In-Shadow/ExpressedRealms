463e9bdc4418f13d88a8629d35b230262164203a
noremacskich <noremacskich@gmail.com>
2025-07-06 05:41:58 -0500

    Created a shared use case project, to store validation helper plus supporting functions
    Also added the ability for validators to handle already deleted cases.  Updated existing calls

---
+++ api/ExpressedRealms.Shared.UseCases/CommonFailureTypes/FluentValidationFailure.cs
@@ -0,0 +1,8 @@
+using FluentResults;
+
+namespace ExpressedRealms.UseCases.Shared.CommonFailureTypes;
+
+public sealed class FluentValidationFailure(IDictionary<string, string[]> validationFailures) : Error
+{
+    public IDictionary<string, string[]> ValidationFailures { get; } = validationFailures;
+}
