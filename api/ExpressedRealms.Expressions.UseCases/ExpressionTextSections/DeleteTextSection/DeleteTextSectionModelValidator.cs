6a0c990bfc04d2bcd042bd41ae4f17545a64daff
noremacskich <noremacskich@gmail.com>
2025-07-06 05:46:07 -0500

    Created use case project for expressions and updated repo for use with the delete use case

---
+++ api/ExpressedRealms.Expressions.UseCases/ExpressionTextSections/DeleteTextSection/DeleteTextSectionModelValidator.cs
@@ -0,0 +1,37 @@
+using ExpressedRealms.Expressions.Repository.Expressions;
+using ExpressedRealms.Expressions.Repository.ExpressionTextSections;
+using FluentValidation;
+using JetBrains.Annotations;
+
+namespace ExpressedRealms.Expressions.UseCases.ExpressionTextSections.DeleteTextSection;
+
+[UsedImplicitly]
+internal sealed class DeleteTextSectionModelValidator : AbstractValidator<DeleteTextSectionModel>
+{
+    public DeleteTextSectionModelValidator(
+        IExpressionTextSectionRepository textRepository,
+        IExpressionRepository expressionRepository
+    )
+    {
+        RuleFor(x => x.ExpressionId)
+            .NotEmpty()
+            .WithMessage("ExpressionId is required.")
+            .MustAsync(async (x, y) => await expressionRepository.GetExpressionForDeletion(x) != null)
+            .WithErrorCode("NotFound")
+            .WithMessage("This is not a valid expression.")
+            .MustAsync(async (x, y) => !(await expressionRepository.GetExpressionForDeletion(x))!.IsDeleted)
+            .WithErrorCode("AlreadyDeleted")
+            .WithMessage("This is expression has been deleted.");
+
+        RuleFor(x => x.Id)
+            .NotEmpty()
+            .WithMessage("Id is required.")
+            .MustAsync(async (x, y) => await textRepository.GetExpressionSectionForDeletion(x) != null)
+            .WithErrorCode("NotFound")
+            .WithMessage("This is not a valid expression section.")
+            .MustAsync(async (x, y) => !(await textRepository.GetExpressionSectionForDeletion(x))!.IsDeleted)
+            .WithErrorCode("AlreadyDeleted")
+            .WithMessage("This is expression section has been deleted.");
+
+    }
+}
