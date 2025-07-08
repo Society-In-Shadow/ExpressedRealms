15f0a3029afe32b3642e926a0bfca9c4720e8e12
noremacskich <noremacskich@gmail.com>
2025-07-08 16:37:22 -0500

    Fixed sonar cloud findings

--- api/ExpressedRealms.Knowledges.UseCases/Knowledges/GetKnowledges/KnowledgeModel.cs
+++ api/ExpressedRealms.Knowledges.UseCases/Knowledges/GetKnowledges/KnowledgeModel.cs
@@ -3,9 +3,9 @@ namespace ExpressedRealms.Knowledges.UseCases.Knowledges.GetKnowledges;
 public class KnowledgeModel
 {
     public int Id { get; set; }
-    public string Name { get; set; }
-    public string Description { get; set; }
-    public string TypeName { get; set; }
-    public string TypeDescription { get; set; }
+    public required string Name { get; set; }
+    public required string Description { get; set; }
+    public required string TypeName { get; set; }
+    public required string TypeDescription { get; set; }
     public int TypeId { get; set; }
 }
