48040db010383bbbf09cd5dee50333ce32ace3ef
noremacskich <noremacskich@gmail.com>
2025-07-15 14:19:50 -0500

    Got basic file outline in place and most of the supporting functions

---
+++ client/src/components/knowledges/stores/knowledgeStore.ts
@@ -0,0 +1,75 @@
+import {defineStore} from "pinia";
+import axios from "axios";
+
+import type {ListItem} from "@/types/ListItem";
+import toaster from "@/services/Toasters";
+import type {EditKnowledge, EditKnowledgeRequest, Knowledge} from "@/components/knowledges/types";
+import type {KnowledgeForm} from "@/components/knowledges/Validations/powerPathValidations";
+
+
+export const knowledgeStore =
+    defineStore(`powers`, {
+        state: () => {
+            return {
+                knowledgeTypes: [] as ListItem[],
+                haveKnowledgeTypes: false,
+                haveKnowledges: false,
+                knowledges: [] as Knowledge[]
+            }
+        },
+        actions: {
+            async getOptions() {
+                if (this.haveKnowledgeTypes)
+                    return;
+
+                await axios.get(`/knowledgetypes`)
+                    .then((response) => {
+                        this.knowledgeTypes = response.data.knowledgeTypes;
+                        this.haveKnowledgeTypes = true;
+                    });
+
+            },
+            async updateKnowledges(){
+                const response = await axios.get<Knowledge[]>(`/knowledges`);
+                this.knowledges = response.data
+            },
+            getKnowledge: async function (id: number): Promise<EditKnowledge> {
+                await this.getOptions()
+                
+                const response = await axios.get<EditKnowledgeRequest>(`/knowledges/${id}`);
+
+                return {
+                    id: response.data.id,
+                    name: response.data.name,
+                    description: response.data.description,
+                    knowledgeType: this.knowledgeTypes.find((x: ListItem) => x.id == response.data.knowledgeTypeId) as ListItem
+                };
+            },
+            updateKnowledge: async function (values:KnowledgeForm, id: number): Promise<void> {
+                await axios.put(`/knowledges/${id}`, {
+                    id: id,
+                    name: values.name,
+                    description: values.description,
+                    powerDurationId: values.knowledgeType.id,
+                })
+                    .then(async () => {
+                        await this.updateKnowledges();
+                        toaster.success("Successfully Updated Power!");
+                    });
+            },
+            deleteKnowledge: async function(id: number){
+                
+                let name = "knowledge";
+                
+                const knowledge = this.knowledges.find((x: Knowledge) => x.id == id);
+                if(knowledge)
+                    name = knowledge.name;
+                
+                await axios.delete(`/knowledges/${id}`)
+                    .then(async () => {
+                        await this.updateKnowledges()
+                        toaster.success(`Successfully Deleted ${name}!`);
+                    });
+            }
+        }
+    });
