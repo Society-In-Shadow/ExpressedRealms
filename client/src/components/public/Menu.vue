fd47c6594573d73d23110888d5fedb69850df7ff
noremacskich <noremacskich@gmail.com>
2025-07-16 23:49:14 -0500

    Got all the basic components in place

---
+++ client/src/components/public/Menu.vue
@@ -0,0 +1,62 @@
+<script setup lang="ts">
+
+import MegaMenu from "primevue/megamenu";
+import Avatar from "primevue/avatar";
+import {useRouter} from "vue-router";
+import {ref} from "vue";
+
+
+const router = useRouter();
+
+const items = ref([
+  { root: true, label: 'Home', icon: 'pi pi-file', subtext: 'Characters', command: () => router.push("/"),  items: [] },
+  { root: true, label: 'About', command: () => router.push("/about") },
+  { root: true, label: 'Expressions', icon: 'pi pi-file', subtext: 'Stone Puller', command: () => router.push("/expressions") },
+  { root: true, label: 'Contact Us', icon: 'pi pi-file', subtext: 'Stone Puller', command: () => router.push("/contact-us") },
+]);
+</script>
+
+<template>
+  <MegaMenu :model="items" class="ms-0 me-0 mt-2 mb-2 m-md-2">
+    <template #start>
+      <img src="/favicon.png" alt="A white, black, blue, red, green, and transparent marbles organized in a pentagon pattern. The white stone is at the top and the transparent stone is in the center." height="50" width="50" class="m-2">
+    </template>
+    <template #item="{ item }">
+      <a v-if="item.root" class="flex items-center cursor-pointer px-4 py-2 overflow-hidden relative font-semibold text-lg uppercase" style="border-radius: 2rem">
+        <span>{{ item.label }}</span>
+      </a>
+      <a v-else-if="!item.image" class="flex items-center p-4 cursor-pointer mb-2 gap-3">
+            <span class="inline-flex items-center justify-center rounded-full bg-primary text-primary-contrast w-12 h-12">
+                <i :class="[item.icon, 'text-lg']"></i>
+            </span>
+        <span class="inline-flex flex-col gap-1">
+                <span class="font-bold text-lg">{{ item.label }}</span>
+                <span class="whitespace-nowrap">{{ item.subtext }}</span>
+            </span>
+      </a>
+      <div v-else class="flex flex-col items-start gap-4 p-2">
+        <img alt="megamenu-demo" :src="item.image" class="w-full" />
+        <span>{{ item.subtext }}</span>
+        <Button :label="item.label" outlined />
+      </div>
+    </template>
+    <template #end>
+      <a class="flex align-items-center p-3 cursor-pointer mb-2 gap-2 no-underline text-100" href="https://discord.gg/NSv3GxSAj7" target="_blank">
+        <Avatar class="pi pi-discord" shape="circle" size="large" />
+        <div class="hideIfSmall">Discord</div>
+      </a>
+      <a class="flex align-items-center p-3 cursor-pointer mb-2 gap-2 no-underline text-100" href="/login" target="_blank">
+        <Avatar class="pi pi-user" shape="circle" size="large" />
+        <div class="hideIfSmall">Login</div>
+      </a>
+    </template>
+  </MegaMenu>
+</template>
+
+<style scoped>
+@media(max-width: 576px){
+  .hideIfSmall{
+    display: none;
+  }
+}
+</style>
---
+++ client/src/components/public/PublicLayout.vue
@@ -0,0 +1,20 @@
+<script setup lang="ts">
+
+import Message from 'primevue/message';
+import {RouterView} from "vue-router";
+import Menu from "@/components/public/Menu.vue";
+
+</script>
+
+<template>
+  <Message icon="pi pi-exclamation-triangle" class="sticky-top ms-0 me-0 mt-2 mb-2 m-md-2" severity="warn">
+    This site is currently in a BETA state, not all features or content is in place.
+  </Message>
+  <Menu />
+  <router-view />
+</template>
+
+<style scoped>
+
+
+</style>
