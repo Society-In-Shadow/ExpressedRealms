a469faeff3af5b556ebb9dbeadf4511ac0c11a19
noremacskich <noremacskich@gmail.com>
2025-07-18 00:51:06 -0500

    Updated about page with additional information and fixed a few mobile view issues

--- client/src/components/public/PublicLayout.vue
+++ client/src/components/public/PublicLayout.vue
@@ -8,22 +8,22 @@ import Card from 'primevue/card';
 </script>
 
 <template>
-  <div class="d-flex flex-column min-vh-100">
+  <div class="d-flex flex-column min-vh-98">
     <Message icon="pi pi-exclamation-triangle" class="sticky-top ms-0 me-0 mt-2 mb-2 m-md-2" severity="warn">
       This site is currently in a BETA state, not all features or content is in place.
     </Message>
     <Menu />
-    <div class="container-fluid constrain-width flex-grow-1">
+    <div class="container-fluid constrain-width flex-grow-1 mb-3 mt-3">
       <router-view />
     </div>
-    <Card class="mt-auto">
+    <Card class="mt-auto m-0 p-0">
       <template #content>
-        <div class="d-flex justify-content-between align-items-center">
+        <div class="d-flex flex-column flex-md-row justify-content-between align-items-center">
           <div>Society in Shadows © {{new Date().getFullYear()}}</div>
-          <div>
-            <RouterLink to="/terms-of-service" class="p-3">Terms of Service</RouterLink>
-            <RouterLink to="/privacy-policy" class="p-3">Privacy Policy</RouterLink>
-            <RouterLink to="/code-of-conduct" class="p-3">Code of Conduct</RouterLink>
+          <div class="d-flex flex-column flex-md-row text-center">
+            <RouterLink to="/terms-of-service" class="p-md-3">Terms of Service</RouterLink>
+            <RouterLink to="/privacy-policy" class="p-md-3">Privacy Policy</RouterLink>
+            <RouterLink to="/code-of-conduct" class="p-md-3">Code of Conduct</RouterLink>
           </div>
         </div>
       </template>
@@ -35,6 +35,10 @@ import Card from 'primevue/card';
 
  .constrain-width {
    max-width: 1000px;
+ }
+ 
+ .min-vh-98 {
+   min-height: 98vh;
  }
 
 </style>
