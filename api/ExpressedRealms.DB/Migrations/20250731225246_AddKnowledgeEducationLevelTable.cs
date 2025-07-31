02931528ffee29ac6cd24eb03b6e992dc6d9647e
noremacskich <noremacskich@gmail.com>
2025-07-31 18:16:09 -0500

    Ran Migration

---
+++ api/ExpressedRealms.DB/Migrations/20250731225246_AddKnowledgeEducationLevelTable.cs
@@ -0,0 +1,40 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
+
+#nullable disable
+
+namespace ExpressedRealms.DB.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddKnowledgeEducationLevelTable : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "knowledge_education_level",
+                columns: table => new
+                {
+                    id = table.Column<int>(type: "integer", nullable: false)
+                        .Annotation("Npgsql:ValueGenerationStrategy", NpgsqlValueGenerationStrategy.IdentityByDefaultColumn),
+                    name = table.Column<string>(type: "character varying(50)", maxLength: 50, nullable: false),
+                    level = table.Column<int>(type: "integer", nullable: false),
+                    specialization_count = table.Column<int>(type: "integer", nullable: false),
+                    stone_modifier = table.Column<int>(type: "integer", nullable: false),
+                    general_xp_cost = table.Column<int>(type: "integer", nullable: false),
+                    unknown_xp_cost = table.Column<int>(type: "integer", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_knowledge_education_level", x => x.id);
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "knowledge_education_level");
+        }
+    }
+}
