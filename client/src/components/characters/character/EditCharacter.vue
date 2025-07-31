fce61809be91d8421eace9e11e166567ff81ee52
noremacskich <noremacskich@gmail.com>
2025-07-30 21:08:55 -0500

    Removed tabs that are not filled in

--- client/src/components/characters/character/EditCharacter.vue
+++ client/src/components/characters/character/EditCharacter.vue
@@ -36,9 +36,6 @@ import TrackableProficiencies from "@/components/characters/character/proficienc
       <TabList>
         <Tab value="0">Proficiencies</Tab>
         <Tab value="1">Skills</Tab>
-        <Tab value="2">Knowledges</Tab>
-        <Tab value="3">Powers</Tab>
-        <Tab value="4">Advantages / Disadvantages / Mixed Blessings</Tab>
       </TabList>
       <TabPanels class="p-2 p-md-3">
         <TabPanel value="0">
@@ -46,24 +43,6 @@ import TrackableProficiencies from "@/components/characters/character/proficienc
         </TabPanel>
         <TabPanel value="1">
           <SkillTile />
-        </TabPanel>
-        <TabPanel value="2">
-          <p class="m-0">
-            Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae ab illo inventore veritatis et quasi architecto beatae vitae dicta sunt explicabo. Nemo enim
-            ipsam voluptatem quia voluptas sit aspernatur aut odit aut fugit, sed quia consequuntur magni dolores eos qui ratione voluptatem sequi nesciunt. Consectetur, adipisci velit, sed quia non numquam eius modi.
-          </p>
-        </TabPanel>
-        <TabPanel value="3">
-          <p class="m-0">
-            At vero eos et accusamus et iusto odio dignissimos ducimus qui blanditiis praesentium voluptatum deleniti atque corrupti quos dolores et quas molestias excepturi sint occaecati cupiditate non provident, similique sunt in culpa
-            qui officia deserunt mollitia animi, id est laborum et dolorum fuga. Et harum quidem rerum facilis est et expedita distinctio. Nam libero tempore, cum soluta nobis est eligendi optio cumque nihil impedit quo minus.
-          </p>
-        </TabPanel>
-        <TabPanel value="4">
-          <p class="m-0">
-            At vero eos et accusamus et iusto odio dignissimos ducimus qui blanditiis praesentium voluptatum deleniti atque corrupti quos dolores et quas molestias excepturi sint occaecati cupiditate non provident, similique sunt in culpa
-            qui officia deserunt mollitia animi, id est laborum et dolorum fuga. Et harum quidem rerum facilis est et expedita distinctio. Nam libero tempore, cum soluta nobis est eligendi optio cumque nihil impedit quo minus.
-          </p>
         </TabPanel>
       </TabPanels>
     </Tabs>
