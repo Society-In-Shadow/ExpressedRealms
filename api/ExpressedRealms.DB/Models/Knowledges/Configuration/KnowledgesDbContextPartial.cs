cd47b642eb0c01fd2a2415e6b8ff84975f06e0f7
noremacskich <noremacskich@gmail.com>
2025-07-31 20:03:59 -0500

    Added new tales to db context

--- api/ExpressedRealms.DB/Models/Knowledges/Configuration/KnowledgesDbContextPartial.cs
+++ api/ExpressedRealms.DB/Models/Knowledges/Configuration/KnowledgesDbContextPartial.cs
@@ -1,4 +1,6 @@
 using ExpressedRealms.DB.Models.Knowledges;
+using ExpressedRealms.DB.Models.Knowledges.CharacterKnowledgeMappings;
+using ExpressedRealms.DB.Models.Knowledges.CharacterKnowledgeSpecializations;
 using ExpressedRealms.DB.Models.Knowledges.KnowledgeEducationLevels;
 using ExpressedRealms.DB.Models.Knowledges.KnowledgeModels;
 using ExpressedRealms.DB.Models.Knowledges.KnowledgeModels.Audit;
@@ -12,4 +14,6 @@ public partial class ExpressedRealmsDbContext
     public DbSet<KnowledgeAuditTrail> KnowledgeAuditTrails { get; set; }
     public DbSet<KnowledgeType> KnowledgeTypes { get; set; }
     public DbSet<KnowledgeEducationLevel> KnowledgeEducationLevels { get; set; }
+    public DbSet<CharacterKnowledgeMapping> CharacterKnowledgeMappings { get; set; }
+    public DbSet<CharacterKnowledgeSpecialization> CharacterKnowledgeSpecializations { get; set; }
 }
