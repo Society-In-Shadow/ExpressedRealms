57e084d3c397922d9542ff974d6a9d04d43f642c
Noremac Skich <noremacskich@gmail.com>
2025-08-02 19:36:38 -0500

    Delete knowledge from character #464 (#479)
    Added use case + unit tests

---
+++ api/ExpressedRealms.Knowledges.UseCases/CharacterKnowledgeMappings/Delete/DeleteKnowledgeFromCharacterModelValidator.cs
@@ -0,0 +1,23 @@
+using ExpressedRealms.Characters.Repository;
+using ExpressedRealms.Knowledges.Repository;
+using ExpressedRealms.Knowledges.Repository.CharacterKnowledgeMappings;
+using ExpressedRealms.Knowledges.Repository.Knowledges;
+using FluentValidation;
+using JetBrains.Annotations;
+
+namespace ExpressedRealms.Knowledges.UseCases.CharacterKnowledgeMappings.Delete;
+
+[UsedImplicitly]
+internal sealed class DeleteKnowledgeFromCharacterModelValidator : AbstractValidator<DeleteKnowledgeFromCharacterModel>
+{
+    public DeleteKnowledgeFromCharacterModelValidator(
+        ICharacterKnowledgeRepository mappingRepository
+    )
+    {
+        RuleFor(x => x.MappingId)
+            .NotEmpty()
+            .WithMessage("Mapping Id is required.")
+            .MustAsync(async (x, y) => await mappingRepository.MappingAlreadyExists(x))
+            .WithMessage("The Knowledge Mapping does not exist.");
+    }
+}
---
+++ api/ExpressedRealms.Knowledges.UseCases/CharacterKnowledgeMappings/Delete/DeleteKnowledgeFromCharacterUseCase.cs
@@ -0,0 +1,34 @@
+using ExpressedRealms.DB.Interceptors;
+using ExpressedRealms.Knowledges.Repository.CharacterKnowledgeMappings;
+using ExpressedRealms.Knowledges.UseCases.CharacterKnowledgeMappings.Edit;
+using ExpressedRealms.UseCases.Shared;
+using FluentResults;
+
+namespace ExpressedRealms.Knowledges.UseCases.CharacterKnowledgeMappings.Delete;
+
+internal sealed class DeleteKnowledgeFromCharacterUseCase(
+    ICharacterKnowledgeRepository mappingRepository,
+    DeleteKnowledgeFromCharacterModelValidator validator,
+    CancellationToken cancellationToken
+) : IDeleteKnowledgeFromCharacterUseCase
+{
+    public async Task<Result> ExecuteAsync(DeleteKnowledgeFromCharacterModel model)
+    {
+        var result = await ValidationHelper.ValidateAndHandleErrorsAsync(
+            validator,
+            model,
+            cancellationToken
+        );
+
+        if (result.IsFailed)
+            return Result.Fail(result.Errors);
+
+        var mapping = await mappingRepository.GetCharacterKnowledgeMappingForEditing(model.MappingId);
+
+        mapping.SoftDelete();
+
+        await mappingRepository.UpdateCharacterKnowledgeMapping(mapping);
+
+        return Result.Ok();
+    }
+}
