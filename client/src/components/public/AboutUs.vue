4657ac35fa006263989d50818cc86f9dbffb5c8b
Noremac Skich <noremacskich@gmail.com>
2025-08-03 17:08:53 -0500

    Updated marketing materials (#484)

--- client/src/components/public/AboutUs.vue
+++ client/src/components/public/AboutUs.vue
@@ -1,9 +1,56 @@
 <template>
+  <h1>About Game</h1>
+  <p>
+    Society in Shadows is a Live Action Role Playing game set in the modern day of our world. We are an urban fantasy
+    setting, with elements of secrecy and politics, but with a penchant for saving the world. Players can range from a
+    former pirate to a mad scientist recreating Tesla experiments, but the sky is the limit with your imagination.
+  </p>
+  <p>
+    The Society in Shadows rules utilize a six stones game system intended to serve as tools and guidelines for game
+    officials and players. The goal is for everyone to have fun and weave a good story. Before playing the game each
+    player will create their own character, utilizing the rules found within this website. Beyond play with your local
+    community your character can be played at any official Society in Shadows events held at various conventions
+    throughout the Midwest and gain experience as a part of an ongoing Chronicle told with players from all over the
+    region! Help and discussion can be found on our discord linked in the upper right hand corner of the site.
+  </p>
+  <p>
+    Society in Shadows events typically take place over the entire weekend of an event or convention, and our players
+    will be able to participate in scenes of combat, investigation, political intrigue and more with other players all
+    throughout the event space. Players may hop in or hop out of play using badges they are given at check-in so they
+    can participate in other events without interruption.
+  </p>
+  <p>
+    Events are facilitated by our Game Officials or GOs (G-Oh)s, they will be moving around the event spaces with plenty of
+    things for you to participate in, and to help you accomplish what you want to work towards. Feel free to ask them
+    questions at any time! We are working on a digital tool to manage and run your character sheet throughout the
+    convention, and having mobile phone or device with internet access is encouraged, all resources needed to play the
+    game at an event will be provided (This includes a printed character sheet, a bag of six stones, a character badge,
+    and resource tracking strips)!
+  </p>
+  <p>Society in Shadows conventions are contained in something we call a Chronicle - a collection of stories take place
+    at these conventions over the course of a few years. Think of it like a TTRPG campaign, except with many more
+    players! We are kicking off this chronicle in August 2025! You may play the same character over the course of an
+    entire chronicle, however be aware the Society in Shadows can be a dangerous place, lethality is expected! But not
+    to fear, even if your character faces their demise you can always create a new character and see the world from
+    fresh eyes!
+  </p>
+  <p>We strive to create an open, friendly atmosphere and welcome new players to try us out at many conventions across
+    the Midwestern area. Players are encouraged to dress up in an outfit befitting them, though that is not a requirement
+    for play.
+  </p>
+  <p>
+    We do not tolerate any form of discrimination, whether that be Homophobia, Transphobia, Racism, Ablism, Sexism, etc.
+    Our main rule is to have fun, and not to infringe on others doing so as well.
+  </p>
+  <p>
+    Take a look at our upcoming events for more information on where we are next!
+  </p>
+  
   <h1>History</h1>
   <p>
-    Society in Shadows is a long running Live Action RolePlaying game boasting players from Kansas City all the way to
-    Minneapolis and further. With a former peak of 87 players and averaging around 30-40 players, and after a brief
-    interim we have returned, better than ever. Our Main Staff has been playing the game for many years, and have been
+    Society in Shadows has been around since the mid 1990's.  We've had players from Kansas City all the way to 
+    Minneapolis and further. With a former peak of 87 players and averaging around 30-40 players, and after a brief 
+    interim, we have returned, better than ever. Our Main Staff has been playing the game for many years, and have been
     hard at work making this the best iteration of the game it can be.
   </p>
   
@@ -17,32 +64,6 @@
     experience level with the medium
   </p>
 
-  <h1>About Game</h1>
-  <p>
-    Society in Shadows (SiS) is a game set in the modern day of our world. We are an urban fantasy setting, with 
-    elements of secrecy and politics, but with a penchant for saving the world. Players can range from a former 
-    pirate to a mad scientist recreating Tesla experiments, but the sky is the limit with your imagination.
-  </p>
-  <p>
-    Each player has an opportunity to create a character that has an “Expression,” of which there are Six in total. 
-    They then get to choose from an array of powers, and using them normally requires a test, using our unique “Six 
-    Stone” system for success vs. failure.
-  </p>
-    
-  <h1>General Expectations</h1>
-  <p>
-    We strive to create an open, friendly atmosphere and welcome new players to try us out at many conventions across 
-    the Midwestern area. Players are encouraged to dress up in an outfit befitting them, though that is not a 
-    requirement for play.
-  </p>
-  <p>
-    We do not tolerate any form of discrimination, whether that be Homophobia, Transphobia, Racism, Ablism, Sexism, 
-    etc. Our main rule is to have fun, and not to infringe on others doing so as well.
-  </p>
-  <p>
-    Take a look at our upcoming events for more information on where we are next!
-  </p>
-
   <h1>Concerns</h1>
   <p>
     For those under the age of 18, we do require Parental Consent from a legal Guardian. We do inform our staff, and 
@@ -50,4 +71,7 @@
     We do ask for an emergency contact in case things go awry and you inform us of any potential concerns as well. 
     (i.e. medical, physical)  
   </p>
+  <p><a href="https://docs.google.com/document/d/1ogaB9BuM5qFATvslI7SRd7Rc4jjlV8kL0-j-oQKmGdI/edit?usp=sharing">Parental Consent Form</a></p>
 </template>
+<script setup lang="ts">
+</script>
--- client/src/components/public/HomePage.vue
+++ client/src/components/public/HomePage.vue
@@ -6,6 +6,51 @@ import Card from "primevue/card";
 </script>
 
 <template>
+
+  <h1>Society in Shadows</h1>
+  <p>
+    These pages—once passed around as a heavily guarded manuscript—are no longer confined to dusty shelves or hidden 
+    archives. They've been updated for the times, translated into a format more suited to the modern world. Some will 
+    call that reckless. Others, necessary.
+  </p>
+  <p>
+    When this manual existed only in printed form, it was already controversial. In certain Realms, even possessing a 
+    copy was considered a direct violation of the First Pillar, the One Law. Now that it’s available online—well, let’s
+    just say the risks haven’t diminished.
+  </p>
+  <p>
+    Why make it available at all, then? Because despite the dangers it may pose, knowledge still matters. And because 
+    our past mistakes cannot be allowed to repeat.
+  </p>
+  <p>
+    The Society, for all its mystery, has survived in part because we’ve kept our stories alive—through memory, through 
+    whispers, through guarded teaching. But oral tradition can only go so far. Tales change in the retelling. Pieces are 
+    forgotten, others altered to suit those in power. Some truths were lost. Others were buried.
+  </p>
+  
+  <HomePageCarousel />
+  
+  <p>
+    This version doesn’t claim to be definitive. It’s a starting point—a foundation for those new to the Society, and a 
+    reminder for those who’ve forgotten why we guard what we do. You’ll find contradictions, gaps, even disagreements 
+    among the stories shared here. But within each, there’s a trace of the truth. A thread of something real.
+  </p>
+  <p>
+    If you’re unfamiliar with our world, you may be tempted to dismiss all of this as fiction. A setting for a game. A 
+    strange creative project. And if that helps you sleep better, you’re welcome to read it that way. But know this: 
+    your disbelief doesn’t make these things any less real.
+  </p>
+  <p>
+    To the initiated—read carefully. Reflect deeply. Ask questions. The Society’s greatest danger has never been from 
+    outside forces. It’s been from within, from forgetting who we are and how close we’ve come to losing everything 
+    before.
+  </p>
+  <p>
+    Let this be a light in the dark. Just don’t mistake it for the whole truth. There’s always more beneath the surface.
+    Before we go any further, there is three things you need to know if we want to make sure you survive to see tomorrow.
+    A vital code each of us lives by to make sure the bit of safety we have carved out in this world doesn't come 
+    crumbling down. We call them, the Three Pillars.
+  </p>
   <h1>The Three Pillars</h1>
   <div class="d-flex flex-column flex-md-row">
     <Card class="constrain-pillars m-2">
@@ -35,7 +80,7 @@ import Card from "primevue/card";
     </Card>
   </div>
   
-  <HomePageCarousel />
+
   
   <h1>Before the Shadows</h1>
 
