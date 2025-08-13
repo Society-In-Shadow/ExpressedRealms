ff08cde52570d785c1b64ead370757b9546c7fb6
noremacskich <noremacskich@gmail.com>
2025-08-13 04:10:08 -0500

    Got basic formatting in place

--- api/ExpressedRealms.Powers.Reporting/powerCards/PowerCardReport.cs
+++ api/ExpressedRealms.Powers.Reporting/powerCards/PowerCardReport.cs
@@ -1,3 +1,4 @@
+using System.ComponentModel;
 using QuestPDF.Fluent;
 using QuestPDF.Helpers;
 using QuestPDF.Infrastructure;
@@ -10,63 +11,101 @@ public static class PowerCardReport
     {
         QuestPDF.Settings.License = LicenseType.Community;
         
+        var chunks = powerCards.Chunk(6);
+        
+        var secondaryColor = Color.FromARGB(100, 0, 0, 0);
+        
         return Document.Create(container =>
             {
                 container.Page(page =>
                 {
                     page.Size(PageSizes.Letter.Landscape());
-                    page.DefaultTextStyle(x => x.FontSize(12));
+                    page.DefaultTextStyle(x => x.FontSize(7f));
 
                     float rowHeight = (PageSizes.Letter.Landscape().Height / 3);
 
-                    // 2 rows × 3 columns of equal-sized sections
                     page.Content().Column(col =>
                     {
-                        col.Spacing(0); // set to 0 for no gap between sections
-                        
-                        // Top row
-                        col.Item().Height(rowHeight).Row(row =>
+                        foreach (var chunk in chunks)
                         {
-                            for (int i = 0; i < 2; i++)
+                            for (int pageRow = 0; pageRow < 3; pageRow++)
                             {
-                                row.RelativeItem()
-                                    .Border(1)
-                                    .AlignCenter()
-                                    .AlignMiddle()
-                                    .Text($"Section {i + 1}");
+                                col.Item().Height(rowHeight).Row(row =>
+                                {
+                                    for (int f = pageRow * 2; f < pageRow * 2 + 2; f++)
+                                    {
+                                        if (chunk.Length < f + 1)
+                                            break;
+                                    
+                                        var power = chunk[f];
+                                    
+                                        row.RelativeItem()
+                                            .Border(1)
+                                            .Column(cell =>
+                                            {
+                                                FillPowerCard(cell, power, secondaryColor);
+                                            });
+                                    }
+                                });
                             }
-                        });
-
-                        // Bottom row
-                        col.Item().Height(rowHeight).Row(row =>
-                        {
-                            for (int i = 2; i < 4; i++)
-                            {
-                                row.RelativeItem()
-                                    .Border(1)
-                                    .AlignCenter()
-                                    .AlignMiddle()
-                                    .Text($"Section {i + 1}");
-                            }
-                        });
-                        
-                        col.Item().Height(rowHeight).Row(row =>
-                        {
-                            for (int i = 4; i < 6; i++)
-                            {
-                                row.RelativeItem()
-                                    .Border(1)
-                                    .AlignCenter()
-                                    .AlignMiddle()
-                                    .Text($"Section {i + 1}");
-                            }
-                        });
+                        }
                     });
                 });
+        });
+    }
+
+    private static void FillPowerCard(ColumnDescriptor card, PowerCardData power, Color secondaryColor)
+    {
+        card.Item().Padding(15).Row(row =>
+        {
+            row.RelativeItem().PaddingRight(5).Column(leftSide =>
+            {
+                leftSide.Item().Text(text =>
+                {
+                    text.Span(power.Name).Bold().FontSize(9);
+
+                    if (power.Category?.Count > 0)
+                    {
+                        text.Span($" - {string.Join(", ", power.Category)}").Italic().FontSize(10)
+                            .FontColor(secondaryColor);
+                    }
+                });
+                leftSide.Item().Text($"{power.ExpressionName} > {power.PathName}").Italic().FontSize(6)
+                    .FontColor(secondaryColor);
+                leftSide.Item().Text(power.Description);
+                leftSide.Item().Text("Game Mechanic Effect:").Bold();
+                leftSide.Item().Text(power.GameMechanicEffect);
+
+                if (power.Limitation is not null)
+                {
+                    leftSide.Item().Text("Limitation:").Bold();
+                    leftSide.Item().Text(power.Limitation);
+                }
             });
 
-        
-        
-        
+            row.ConstantItem(1.75f, Unit.Inch).Column(rightSide =>
+            {
+                rightSide.FormatAttributeSection("Level", power.PowerLevel);
+                rightSide.FormatAttributeSection("Power Use", power.IsPowerUse ? "Yes" : "No");
+                rightSide.FormatAttributeSection("Cost", power.Cost);
+                rightSide.FormatAttributeSection("Power Duration", power.PowerDuration);
+                rightSide.FormatAttributeSection("Area of Effect", power.AreaOfEffect);
+                rightSide.FormatAttributeSection("Power Duration", power.PowerDuration);
+                rightSide.FormatAttributeSection("Activation Type", power.PowerActivationType);
+                
+                rightSide.Item().Text(power.PrerequisitesNeeded);
+                rightSide.Item().Text(power.Prerequisites?.PrerequisiteNames.Count ?? 0);
+            });
+        });
+
+    }
+    
+    private static void FormatAttributeSection(this ColumnDescriptor cell, string attributeName, string attributeValue)
+    {
+        cell.Item().Text(text =>
+        {
+            text.Span(attributeName).Bold();
+            text.Span($": {attributeValue}");
+        });
     }
 }
