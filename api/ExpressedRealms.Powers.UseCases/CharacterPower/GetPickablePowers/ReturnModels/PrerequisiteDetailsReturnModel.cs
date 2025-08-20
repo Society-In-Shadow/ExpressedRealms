9858dccf0c1abc07e6521b25b72914a8fd50d983
noremacskich <noremacskich@gmail.com>
2025-08-20 04:47:19 -0500

    Fixed sonar cloud finding

--- api/ExpressedRealms.Powers.UseCases/CharacterPower/GetPickablePowers/ReturnModels/PrerequisiteDetailsReturnModel.cs
+++ api/ExpressedRealms.Powers.UseCases/CharacterPower/GetPickablePowers/ReturnModels/PrerequisiteDetailsReturnModel.cs
@@ -3,5 +3,5 @@ namespace ExpressedRealms.Powers.UseCases.CharacterPower.GetPickablePowers.Retur
 public class PrerequisiteDetailsReturnModel
 {
     public int RequiredAmount { get; set; }
-    public List<string> Powers { get; set; }
+    public List<string> Powers { get; set; } = new ();
 }
