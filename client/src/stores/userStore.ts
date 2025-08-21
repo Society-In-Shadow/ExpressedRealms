576a7c72aa16e21720db7e45dd865a4a789340f5
noremacskich <noremacskich@gmail.com>
2025-08-20 21:43:50 -0500

    Added new feature flag

--- client/src/stores/userStore.ts
+++ client/src/stores/userStore.ts
@@ -14,6 +14,7 @@ export const UserRoles = {
 export const FeatureFlags = {
     ShowMarketingContactUs: "show-marketing-contact-us",
     ShowInventoryPage: "show-inventory-page",
+    ShowCharacterPowers: "show-character-powers"
 } as const;
 
 export type UserRole = (typeof UserRoles)[keyof typeof UserRoles];
