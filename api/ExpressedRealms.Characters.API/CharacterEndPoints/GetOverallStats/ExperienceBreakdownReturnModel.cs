38cb7689cb92a799bd0616af0fbde4f0810c2937
noremacskich <noremacskich@gmail.com>
2025-08-23 01:32:37 -0500

    Added API Endpoint

---
+++ api/ExpressedRealms.Characters.API/CharacterEndPoints/GetOverallStats/ExperienceBreakdownReturnModel.cs
@@ -0,0 +1,14 @@
+namespace ExpressedRealms.Characters.API.CharacterEndPoints.GetOverallStats;
+
+public class ExperienceBreakdownResponse
+{
+    public int KnowledgeXp { get; set; }
+    public int SkillsXp { get; set; }
+    public int PowersXp { get; set; }
+    public int StatsXp { get; set; }
+    public int SetupKnowledgeXp { get; set; }
+    public int SetupPowersXp { get; set; }
+    public int SetupStatsXp { get; set; }
+    public int SetupSkillsXp { get; set; }
+    public int Total { get; set; }
+}
