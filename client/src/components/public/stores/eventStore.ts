d8d5c00af1b0f318c3fa117ff975da81cc200578
Noremac Skich <noremacskich@gmail.com>
2025-08-23 16:54:41 -0500

    Added nuke con info to the site, tweaked layout a bit (#579)

--- client/src/components/public/stores/eventStore.ts
+++ client/src/components/public/stores/eventStore.ts
@@ -14,11 +14,19 @@ export const eventStore =
                 this.events = [{
                     id: 1,
                     name: 'Sioux City Geek Con',
-                    location: '801 4th St Sioux City, Iowa',
+                    location: 'Sioux City Convention Center Sioux City, Iowa',
                     startDate: new Date(2025, 7, 22),
                     endDate: new Date(2025, 7, 24),
                     conWebsiteName: 'Sioux City Table Top RPG',
                     conWebsiteUrl: 'https://tabletop.events/conventions/sioux-city-geek-con-fall-2025',
+                }, {
+                    id: 2,
+                    name: 'Nuke-Con',
+                    location: 'Mid-America Center, Council Bluffs, IA 51501',
+                    startDate: new Date(2025, 9, 3),
+                    endDate: new Date(2025, 9, 5),
+                    conWebsiteName: 'Nuke-Con',
+                    conWebsiteUrl: 'https://www.nuke-con.com/',
                 }]
             },
         }
