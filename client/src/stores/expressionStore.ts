8a83d9f4b7e6b4d49cad637d1819b66a5da1b1bf
noremacskich <noremacskich@gmail.com>
2025-08-29 01:19:20 -0500

    CMS now relies on Slugs to populate pages
    It will do a slug lookup UI side to avoid a round trip

--- client/src/stores/expressionStore.ts
+++ client/src/stores/expressionStore.ts
@@ -1,9 +1,10 @@
 import {defineStore} from 'pinia'
 import axios from "axios";
 import {UserRoles, userStore} from "@/stores/userStore";
+import {cmsStore} from "@/stores/cmsStore.ts";
 
 const userInfo = userStore();
-
+const cmsInfo = cmsStore();
 export const expressionStore = 
 defineStore('expression', {
     state: () => {
@@ -18,12 +19,12 @@ defineStore('expression', {
     },
     actions: {
         async getExpressionId(route: object){
-            if(route.meta.isCMS){
-                console.log("is cms");
-                await axios.get(`/expression/getCmsByName/${route.meta.id}`)
-                    .then(async (json) => {
-                        this.currentExpressionId = json.data.id;
-                    })
+            if(route.meta.isRuleBook){
+                await cmsInfo.getCmsInformation();
+                this.currentExpressionId = cmsInfo.rulebookItems.filter(x => x.slug == route.params.slug)[0].id;
+            } else if(route.meta.isWorldBackground){
+                await cmsInfo.getCmsInformation();
+                this.currentExpressionId = cmsInfo.worldBackgroundItems.filter(x => x.slug == route.params.slug)[0].id;
             }
             else{
                 await axios.get(`/expression/getByName/${route.params.name}`)
