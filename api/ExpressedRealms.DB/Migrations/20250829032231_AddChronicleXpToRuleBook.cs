ac7491b87c2ee75a23433220b6a6ae23c66ea4ee
noremacskich <noremacskich@gmail.com>
2025-08-29 04:48:02 -0500

    Added Icon Update Statements for SQL

--- api/ExpressedRealms.DB/Migrations/20250829032231_AddChronicleXpToRuleBook.cs
+++ api/ExpressedRealms.DB/Migrations/20250829032231_AddChronicleXpToRuleBook.cs
@@ -26,6 +26,19 @@ namespace ExpressedRealms.DB.Migrations
                                     update public.expression
                                     set expression_type_id = 14
                                     where id in (6, 10, 11, 12)");
+
+            migrationBuilder.Sql(@"update public.expression set nav_menu_item = 'emergency_home';
+update public.expression set nav_menu_item = 'user_attributes' where id = 17;
+update public.expression set nav_menu_item = 'article_person' where id = 13;
+update public.expression set nav_menu_item = 'cognition_2' where id = 14;
+update public.expression set nav_menu_item = 'chess_bishop_2' where id = 15;
+update public.expression set nav_menu_item = 'swords' where id = 16;
+update public.expression set nav_menu_item = 'backpack' where id = 18;
+update public.expression set nav_menu_item = 'auto_stories' where id = 6;
+update public.expression set nav_menu_item = 'skull_list' where id = 10;
+update public.expression set nav_menu_item = 'safety_divider' where id = 11;
+update public.expression set nav_menu_item = 'hub' where id = 12;
+update public.expression set nav_menu_item = 'psychology' where expression_type_id = 1;");
             
             migrationBuilder.InsertData(
                 table: "expression",
