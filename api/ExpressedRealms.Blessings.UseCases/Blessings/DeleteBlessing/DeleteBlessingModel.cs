cb73f767ff5640b9e0fe83fc11ce998ccee4e7d1
noremacskich <noremacskich@gmail.com>
2025-08-30 04:33:29 -0500

    Got use case in place

---
+++ api/ExpressedRealms.Blessings.UseCases/Blessings/DeleteBlessing/DeleteBlessingModel.cs
@@ -0,0 +1,6 @@
+namespace ExpressedRealms.Blessings.UseCases.Blessings.DeleteBlessing;
+
+public class DeleteBlessingModel
+{
+    public int Id { get; set; }
+}
---
+++ api/ExpressedRealms.Blessings.UseCases/Blessings/DeleteBlessing/IDeleteBlessingUseCase.cs
@@ -0,0 +1,7 @@
+using ExpressedRealms.Blessings.UseCases.Blessings.DeleteBlessing;
+using ExpressedRealms.Shared;
+using FluentResults;
+
+namespace ExpressedRealms.Knowledges.UseCases.Knowledges.DeleteKnowledge;
+
+public interface IDeleteBlessingUseCase : IGenericUseCase<Result, DeleteBlessingModel> { }
