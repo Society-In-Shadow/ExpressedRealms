e1f625b76b194b21c563f697aa1f62f16509a3bd
noremacskich <noremacskich@gmail.com>
2025-09-04 03:15:34 -0500

    Got add blessing in place

---
+++ client/src/components/blessings/AddBlessing.vue
@@ -0,0 +1,44 @@
+<script setup lang="ts">
+
+import FormInputTextWrapper from "@/FormWrappers/FormInputTextWrapper.vue";
+import Button from "primevue/button";
+import FormTextAreaWrapper from "@/FormWrappers/FormTextAreaWrapper.vue";
+import {getValidationInstance} from "@/components/blessings/validations/blessingForm.ts";
+import {blessingsStore} from "@/components/blessings/stores/blessingsStore.ts";
+
+const store = blessingsStore();
+
+const form = getValidationInstance()
+const emit = defineEmits<{
+  canceled: []
+}>();
+
+
+const onSubmit = form.handleSubmit(async (values) => {
+  await store.addBlessing(values);
+  cancel();
+});
+
+const cancel = () => {
+  emit("canceled");
+}
+
+</script>
+
+<template>
+  <form @submit="onSubmit">
+
+    <FormInputTextWrapper v-model="form.type" />
+    
+    <FormInputTextWrapper v-model="form.subCategory" />
+    
+    <FormInputTextWrapper v-model="form.name" />
+
+    <FormTextAreaWrapper v-model="form.description" />
+
+    <div class="m-3 text-right">
+      <Button label="Cancel" class="m-2" type="reset" @click="cancel" />
+      <Button label="Add" class="m-2" type="submit" />
+    </div>
+  </form>
+</template>
