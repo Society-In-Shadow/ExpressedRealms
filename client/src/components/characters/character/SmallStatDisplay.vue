f99d8655c4148d6dad9185f22640a9861acdc97b
noremacskich <noremacskich@gmail.com>
2025-09-09 07:04:23 -0500

    Stats will now block edit functionality if flag is enabled

--- client/src/components/characters/character/SmallStatDisplay.vue
+++ client/src/components/characters/character/SmallStatDisplay.vue
@@ -7,6 +7,7 @@ import {useRoute} from 'vue-router'
 import StatTile from "@/components/characters/character/StatTile.vue";
 import SkeletonWrapper from "@/FormWrappers/SkeletonWrapper.vue";
 import {experienceStore} from "@/components/characters/character/stores/experienceBreakdownStore.ts";
+import {FeatureFlags, userStore} from "@/stores/userStore.ts";
 
 const route = useRoute()
 const experienceInfo = experienceStore();
@@ -14,13 +15,16 @@ const stats = ref([ {}, {}, {}, {}, {}, {}]);
 const showDetails = ref(false);
 const selectedStatType = ref(1);
 const isLoading = ref(true);
+const showCharacterWizard = ref(false);
+const userInfo = userStore();
 
-onMounted(() =>{
+onMounted(async () =>{
   axios.get(`/characters/${route.params.id}/stats`)
       .then((response) => {
         stats.value = response.data;
         isLoading.value = false;
-      })
+      });
+  showCharacterWizard.value = await userInfo.hasFeatureFlag(FeatureFlags.ShowCharacterWizard);
 });
 
 function showDetailedStat(statTypeId:number){
@@ -38,7 +42,7 @@ function updateStat(level:number, bonus:number){
 </script>
 
 <template>
-  <div class="text-right pb-3" v-if="experienceInfo.showAllExperience">{{ experienceInfo.experienceBreakdown.statsXp}} Total XP - {{experienceInfo.experienceBreakdown.setupStatsXp}} Creation XP = {{experienceInfo.experienceBreakdown.statsXp - experienceInfo.experienceBreakdown.setupStatsXp}} XP</div>
+  <div class="text-right pb-3" v-if="!showCharacterWizard && experienceInfo.showAllExperience">{{ experienceInfo.experienceBreakdown.statsXp}} Total XP - {{experienceInfo.experienceBreakdown.setupStatsXp}} Creation XP = {{experienceInfo.experienceBreakdown.statsXp - experienceInfo.experienceBreakdown.setupStatsXp}} XP</div>
   <div class="flex flex-wrap justify-content-center column-gap-3 row-gap-3 w-100">
     <div v-for="stat in stats" v-if="!showDetails" :key="stat.statTypeId" class="align-self-lg-start align-self-md-start align-self-xl-start align-self-sm-stretch m-0 p-0">
       <Fieldset class="statBlock mb-3" style="cursor: pointer;" @click="showDetailedStat(stat.statTypeId)">
