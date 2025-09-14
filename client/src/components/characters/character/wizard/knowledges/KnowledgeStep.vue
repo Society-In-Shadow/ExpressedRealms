f065b30dedefa6d1dffe61764df7e16ac1379136
noremacskich <noremacskich@gmail.com>
2025-09-14 04:29:59 -0500

    Simplification and knowledges will now update dyanamically in popup

--- client/src/components/characters/character/wizard/knowledges/KnowledgeStep.vue
+++ client/src/components/characters/character/wizard/knowledges/KnowledgeStep.vue
@@ -1,7 +1,7 @@
 <script setup lang="ts">
 
 import Button from "primevue/button";
-import {onBeforeMount, ref, watch} from "vue";
+import {computed, onBeforeMount, ref} from "vue";
 import {knowledgeStore} from "@/components/knowledges/stores/knowledgeStore";
 import {characterKnowledgeStore} from "@/components/characters/character/knowledges/stores/characterKnowledgeStore";
 import {useRoute} from "vue-router";
@@ -18,52 +18,49 @@ const wizardContentInfo = wizardContentStore();
 const knowledgeData = knowledgeStore();
 const characterKnowledgeData = characterKnowledgeStore();
 const route = useRoute();
-const availableKnowledgeGroups = ref<Array<KnowledgeGroup>>([]);
-const selectedKnowledgeGroups = ref<CharacterKnowledgeGroup[]>([]);
 
 onBeforeMount(async () => {
   await knowledgeData.getKnowledges();
   await characterKnowledgeData.getCharacterKnowledges(route.params.id)
 })
 
-watch(() => characterKnowledgeData.knowledges, () => {
-  populateAvailableKnowledges();
-  populateSelectedKnowledges()
+const availableKnowledgeGroups = computed<Array<KnowledgeGroup>>(() => {
+  const existingKnowledgeIds = characterKnowledgeData.knowledges.map(ck => ck.knowledge.id);
+  const selectedKnowledges = knowledgeData.knowledges
+      .filter(k => !existingKnowledgeIds.includes(k.id))
+      .sort((x, y) => x.name.localeCompare(y.name)) as Array<Knowledge>;
+
+  const typeNames = [...new Set(selectedKnowledges.map(i => i.typeName))].sort((x, y) => x.localeCompare(y));
+
+  return typeNames.map(type => ({
+    name: type,
+    knowledges: selectedKnowledges.filter(i => i.typeName === type)
+  } as KnowledgeGroup));
 });
 
-function populateAvailableKnowledges(){
 
-  const existingKnowledgeIds = characterKnowledgeData.knowledges.map(ck => ck.knowledge.id);
-  let selectedKnowledges =  knowledgeData.knowledges.filter(knowledge =>
-      !existingKnowledgeIds.includes(knowledge.id)
-  ).sort((x, y) => x.name.localeCompare(y.name)) as Array<Knowledge>;
-  availableKnowledgeGroups.value = [];
-  const typeNames = [...new Set(selectedKnowledges.map(i => i.typeName))].sort((x, y) => x.localeCompare(y));
-  typeNames.forEach(type => {
-    availableKnowledgeGroups.value.push({
-      name: type,
-      knowledges: selectedKnowledges.filter(i => i.typeName == type)
-    } as KnowledgeGroup)
-  })
-}
-
-function populateSelectedKnowledges(){
-
-  let selectedKnowledges =  characterKnowledgeData.knowledges
+const selectedKnowledgeGroups = computed<CharacterKnowledgeGroup[]>(() => {
+  const selectedKnowledges = [...characterKnowledgeData.knowledges]
       .sort((x, y) => x.knowledge.name.localeCompare(y.knowledge.name)) as Array<CharacterKnowledge>;
 
-  selectedKnowledgeGroups.value = [];
   const typeNames = [...new Set(selectedKnowledges.map(i => i.knowledge.type))].sort((x, y) => x.localeCompare(y));
-  typeNames.forEach(type => {
-    selectedKnowledgeGroups.value.push({
-      name: type,
-      knowledges: selectedKnowledges.filter(i => i.knowledge.type == type)
-    } as CharacterKnowledgeGroup)
-  })
+
+  return typeNames.map(type => ({
+    name: type,
+    knowledges: selectedKnowledges.filter(i => i.knowledge.type === type)
+  } as CharacterKnowledgeGroup));
+});
+
+const currentKnowledgeMappingId = ref<number | null>(null);
+const knowledge = computed(() => {
+  return characterKnowledgeData.knowledges.filter(x => x.mappingId == currentKnowledgeMappingId.value)[0];
+})
+const toggleEdit = (knowledge:CharacterKnowledge) => {
+  currentKnowledgeMappingId.value = knowledge.mappingId;
+  updateWizardContent();
 }
 
-
-const toggleEdit = (knowledge) => {
+const updateWizardContent = () => {
   wizardContentInfo.updateContent(
       {
         headerName: 'Knowledge',
@@ -83,7 +80,7 @@ const toggleEdit = (knowledge) => {
     <h1 v-if="characterKnowledgeData.knowledges.length > 0">Selected Knowledges</h1>
     <div v-for="knowledgeGroup in selectedKnowledgeGroups" :key="knowledgeGroup.name" class="mb-2">
       <h2 class="pb-0 mb-0">{{ knowledgeGroup.name }}</h2>
-      <div class="pl-3" v-for="knowledge in knowledgeGroup.knowledges" :key="knowledge.id">
+      <div class="pl-md-3" v-for="knowledge in knowledgeGroup.knowledges" :key="knowledge.id">
         <div class="d-flex flex-column flex-md-row align-self-center justify-content-between pt-2 pb-2">
           <div class="d-flex flex-column flex-grow-1 pr-3">
             <div class="d-flex flex-fill align-content-between d-block">
@@ -102,7 +99,7 @@ const toggleEdit = (knowledge) => {
               <div>Stones: +{{ knowledge.stoneModifier }}</div>
             </div>
           </div>
-          <div>
+          <div class="text-end">
             <Button label="View" size="small" @click="toggleEdit(knowledge)" />
           </div>
         </div>
@@ -115,7 +112,7 @@ const toggleEdit = (knowledge) => {
       <ShowXPCosts xp-name-tag="Knowledge XP" />
       <div v-for="knowledgeGroup in availableKnowledgeGroups" :key="knowledgeGroup.name" class="mb-2">
         <h2 class="pb-0 mb-3">{{ knowledgeGroup.name }}</h2>
-        <div v-for="knowledge in knowledgeGroup.knowledges" :key="knowledge.id" class="pl-3 pt-1 pb-1">
+        <div v-for="knowledge in knowledgeGroup.knowledges" :key="knowledge.id" class="pl-md-3 pt-1 pb-1">
           <SelectKnowledgeItem :is-read-only="false" :knowledge="knowledge" />
         </div>
       </div>
