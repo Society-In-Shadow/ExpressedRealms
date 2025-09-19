8e2d124b937adb8748ab3ae6a88e828d3f1bf553
noremacskich <noremacskich@gmail.com>
2025-09-19 11:48:40 -0500

    Final iteration

--- client/src/components/characters/character/OverallExperience.vue
+++ client/src/components/characters/character/OverallExperience.vue
@@ -9,6 +9,7 @@ const route = useRoute()
 const xpInfo = experienceStore();
 const disadvantageBucket = ref(0);
 const discretionaryBucket = ref(0);
+const overallDiscretionaryTotal = ref(0);
 
 onMounted(async () => {
   await xpInfo.updateExperience(route.params.id);  
@@ -17,11 +18,11 @@ onMounted(async () => {
 watch(xpInfo.calculatedValues, () => {
   const disadvantage = xpInfo.getExperienceInfoForSection(XpSectionTypes.disadvantage);
 
-  const overallTotal = 16 + disadvantage.total;
+  overallDiscretionaryTotal.value = 16 + disadvantage.total;
   const availableDiscretionary = xpInfo.availableDiscretionary;
 
   disadvantageBucket.value = Math.max(0, disadvantage.total - availableDiscretionary);
-  discretionaryBucket.value = overallTotal - availableDiscretionary >= 16 ? 16 : 16 + disadvantage.total - availableDiscretionary  ;
+  discretionaryBucket.value = overallDiscretionaryTotal.value - availableDiscretionary >= 16 ? 16 : 16 + disadvantage.total - availableDiscretionary  ;
 }, { immediate: true })
 
 </script>
@@ -34,8 +35,8 @@ watch(xpInfo.calculatedValues, () => {
       <th class="pr-2"></th>
       <th class="text-left">Name</th>
       <th class="text-center">Required</th>
-      <th class="text-center">Discretionary</th>
       <th class="text-center">Available</th>
+      <th class="text-right">Disc.</th>
     </tr>
     <tr v-for="section in xpInfo.calculatedValues">
       <td class="text-center pr-2">
@@ -46,7 +47,7 @@ watch(xpInfo.calculatedValues, () => {
           {{ disadvantageBucket == section.total ? "check_circle" : "warning" }}
         </span>
         <span v-else-if="section.sectionTypeId == XpSectionTypes.discretionary" class="material-symbols-outlined" title="You are required to spend all points">
-          {{ discretionaryBucket == 16 ? "check_circle" : "warning" }}
+          {{ overallDiscretionaryTotal - discretionaryBucket - disadvantageBucket == 0 ? "check_circle" : "warning" }}
         </span>
         <span v-else class="material-symbols-outlined" title="You are required to spend all points">
           {{ section.total >= section.characterCreateMax ? "check_circle" : "warning" }}
@@ -57,7 +58,7 @@ watch(xpInfo.calculatedValues, () => {
       <!-- Required XP -->
       <td class="text-center">
         <div v-if="section.sectionTypeId == XpSectionTypes.discretionary">
-          {{ discretionaryBucket + disadvantageBucket }} / 22
+          {{ discretionaryBucket + disadvantageBucket }} / {{overallDiscretionaryTotal}}
         </div>
         <div v-else-if="section.sectionTypeId == XpSectionTypes.advantage">
           --
@@ -66,35 +67,21 @@ watch(xpInfo.calculatedValues, () => {
           --
         </div>
         <div v-else>
-          {{ section.characterCreateMax }}
+          {{ section.requiredXp }} / {{ section.characterCreateMax }}
         </div>
       </td>
       
-      <!-- Discretionary XP -->
-      <td class="text-center">
-        <div v-if="section.sectionTypeId == XpSectionTypes.discretionary">
-          --
-        </div>
-        <div v-else-if="section.sectionTypeId == XpSectionTypes.disadvantage">
-          + {{ section.total }}
-        </div>
-        <div v-else-if="section.sectionTypeId == XpSectionTypes.advantage">
-          {{ section.total }}
-        </div>
-        <div v-else>
-          <div v-if="section.currentOptionalXp == 0">--</div>
-          <div v-else>{{ section.currentOptionalXp }}</div>
-        </div>
-      </td>
+
       
       <!-- Available XP -->
       <td class="text-center">
         <div v-if="section.sectionTypeId == XpSectionTypes.disadvantage ">
-          <div v-if="xpInfo.availableDiscretionary == 0">--</div>
-          <div v-else>{{ xpInfo.availableDiscretionary }}</div>
+          --
         </div>
         <div v-else-if="section.sectionTypeId == XpSectionTypes.discretionary ">
-          {{ 22 - discretionaryBucket - disadvantageBucket }}
+          <div v-if="overallDiscretionaryTotal - discretionaryBucket - disadvantageBucket == 0">--</div>
+          <div v-else>{{ overallDiscretionaryTotal - discretionaryBucket - disadvantageBucket }}</div>
+          
         </div>
         <div v-else-if="section.total < section.characterCreateMax && section.sectionTypeId != XpSectionTypes.discretionary && section.sectionTypeId != XpSectionTypes.advantage">
           {{ section.characterCreateMax - section.total }}
@@ -103,15 +90,34 @@ watch(xpInfo.calculatedValues, () => {
           --
         </div>
       </td>
+      <!-- Discretionary XP -->
+      <td class="text-center">
+        <div v-if="section.sectionTypeId == XpSectionTypes.discretionary">
+          --
+        </div>
+        <div v-else-if="section.sectionTypeId == XpSectionTypes.disadvantage">
+          <div v-if="section.total == 0">--</div>
+          <div v-else>({{ section.total }})</div>
+        </div>
+        <div v-else-if="section.sectionTypeId == XpSectionTypes.advantage">
+          <div v-if="section.total == 0">--</div>
+          <div v-else>{{ section.total }}</div>
+        </div>
+        <div v-else>
+          <div v-if="section.currentOptionalXp == 0">--</div>
+          <div v-else>{{ section.currentOptionalXp }}</div>
+        </div>
+      </td>
     </tr>
   </table>
   
   <Message severity="info" class="mt-3">
     <div>This is an breakdown of all the XP the current character has. The calculations are assuming you are spending everything you can. This will be changed later.</div>
     <ul>
+      <li>Status - These are the icons you see.  Checkmark means it's done.  Warning means it still needs work.  Dash means its optional</li>
       <li>Required - This column shows you the total xp for a section, and how much you have spent on it.</li>
-      <li>Discretionary - This is showing how much discretionary points you have spent in each section</li>
       <li>Available - This is the remaining XP you need to spend for the given category</li>
+      <li>Disc. - Discretionary - This is showing how much discretionary points you have spent in each section</li>
     </ul>
     <div>Experience Levels:</div>
     <ol>
