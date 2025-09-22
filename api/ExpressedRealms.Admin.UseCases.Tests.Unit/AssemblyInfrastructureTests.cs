109269d04e6a52eabb1c74151667e9dc50f3e4b6
noremacskich <noremacskich@gmail.com>
2025-09-22 16:58:47 -0500

    Added Use Case Unit Tests to new admin section

---
+++ api/ExpressedRealms.Admin.UseCases.Tests.Unit/AssemblyInfrastructureTests.cs
@@ -0,0 +1,9 @@
+﻿using ExpressedRealms.Shared.UseCases.Tests.Unit;
+using JetBrains.Annotations;
+
+namespace ExpressedRealms.Admin.UseCases.Tests.Unit;
+
+[UsedImplicitly]
+public class AssemblyInfrastructureTests : InfrastructureTests
+{
+}
