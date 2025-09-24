6a54f12dfeb102ff23be53ca680ab6fe693f10e6
noremacskich <noremacskich@gmail.com>
2025-09-24 04:17:05 -0500

    API/DB can now pass through and modify player number

--- api/ExpressedRealms.Admin.API/AdminCharacterList/UpdateCharacterXp/UpdateCharacterXpEndpoint.cs
+++ api/ExpressedRealms.Admin.API/AdminCharacterList/UpdateCharacterXp/UpdateCharacterXpEndpoint.cs
@@ -1,4 +1,5 @@
 using ExpressedRealms.Admin.UseCases.UpdateCharacterXp;
+using ExpressedRealms.Server.Shared;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Http.HttpResults;
 using Microsoft.AspNetCore.Mvc;
@@ -7,15 +8,21 @@ namespace ExpressedRealms.Admin.API.AdminCharacterList.UpdateCharacterXp;
 
 public static class UpdateCharacterXpEndpoint
 {
-    public static async Task<Ok> Execute(
+    public static async Task<Results<Ok, ValidationProblem, NotFound>> Execute(
         int characterId,
         [FromBody] UpdateCharacterXpRequest request,
         [FromServices] IUpdateCharacterXpUseCase useCase
     )
     {
-        await useCase.ExecuteAsync(
-            new UpdateCharacterXpModel() { Id = characterId, Xp = request.Xp }
+        var results = await useCase.ExecuteAsync(
+            new UpdateCharacterXpModel() { Id = characterId, Xp = request.Xp, PlayerNumber = request.PlayerNumber}
         );
+        
+        if (results.HasValidationError(out var validationProblem))
+            return validationProblem;
+        if (results.HasNotFound(out var notFound))
+            return notFound;
+        results.ThrowIfErrorNotHandled();
 
         return TypedResults.Ok();
     }
---
+++ api/ExpressedRealms.DB/Migrations/20250924085229_AddPlayerNumberToCharacter.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace ExpressedRealms.DB.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddPlayerNumberToCharacter : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<int>(
+                name: "player_number",
+                table: "Characters",
+                type: "integer",
+                nullable: false,
+                defaultValue: 0);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "player_number",
+                table: "Characters");
+        }
+    }
+}
