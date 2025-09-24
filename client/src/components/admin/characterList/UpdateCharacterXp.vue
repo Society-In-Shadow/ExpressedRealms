5b18c89e5254629377195381876f9aaaf72ff638
noremacskich <noremacskich@gmail.com>
2025-09-24 04:22:37 -0500

    Added quick note

--- client/src/components/admin/characterList/UpdateCharacterXp.vue
+++ client/src/components/admin/characterList/UpdateCharacterXp.vue
@@ -5,6 +5,7 @@ import {getValidationInstance} from "@/components/admin/characterList/validators
 import FormInputNumberWrapper from "@/FormWrappers/FormInputNumberWrapper.vue";
 import Button from "primevue/button";
 import {adminCharacterListStore} from "@/components/admin/characterList/stores/characterListStore.ts";
+import Message from "primevue/message";
 
 const form = getValidationInstance();
 const store = adminCharacterListStore();
@@ -32,6 +33,7 @@ const cancel = () => {
 
   <form @submit="onSubmit">
     <FormInputNumberWrapper v-model="form.fields.playerNumber" />
+    <Message class="mb-3">This will not do duplicate checking for player number, as multiple characters can exist for one player.</Message>
     <FormInputNumberWrapper v-model="form.fields.xp" />
     <Button label="Update" class="m-2" type="submit" />
   </form>
