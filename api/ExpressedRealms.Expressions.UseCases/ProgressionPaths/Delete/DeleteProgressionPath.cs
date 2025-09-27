aaec022f90e0eb04b629cb6b74413f6e2948b0ab
Noremac Skich <noremacskich@gmail.com>
2025-09-26 23:15:55 -0500

    Added Delete Progression Path use case (#732)

---
+++ api/ExpressedRealms.Expressions.UseCases/ProgressionPaths/Delete/DeleteProgressionPath.cs
@@ -0,0 +1,7 @@
+using ExpressedRealms.Shared;
+using FluentResults;
+
+namespace ExpressedRealms.Expressions.UseCases.ProgressionPaths.Delete;
+
+public interface IDeleteProgressionPathUseCase
+    : IGenericUseCase<Result, DeleteProgressionPathModel> { }
---
+++ api/ExpressedRealms.Expressions.UseCases/ProgressionPaths/Delete/DeleteProgressionPathUseCase.cs
@@ -0,0 +1,31 @@
+using ExpressedRealms.DB.Interceptors;
+using ExpressedRealms.Expressions.Repository.ProgressionPaths;
+using ExpressedRealms.UseCases.Shared;
+using FluentResults;
+
+namespace ExpressedRealms.Expressions.UseCases.ProgressionPaths.Delete;
+
+internal sealed class DeleteProgressionPathUseCase(
+    IProgressionPathRepository repository,
+    DeleteProgressionPathModelValidator validator,
+    CancellationToken cancellationToken
+) : IDeleteProgressionPathUseCase
+{
+    public async Task<Result> ExecuteAsync(DeleteProgressionPathModel model)
+    {
+        var result = await ValidationHelper.ValidateAndHandleErrorsAsync(
+            validator,
+            model,
+            cancellationToken
+        );
+
+        if (result.IsFailed)
+            return Result.Fail(result.Errors);
+
+        var progressionPath = await repository.GetProgressionPathForEditing(model.Id);
+
+        progressionPath.SoftDelete();
+
+        return Result.Ok();
+    }
+}
