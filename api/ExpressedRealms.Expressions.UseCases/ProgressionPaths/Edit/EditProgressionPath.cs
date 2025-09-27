a63dc2d1b5bcce220ebc7d72ea99c4e6abd39c0b
noremacskich <noremacskich@gmail.com>
2025-09-26 22:42:18 -0500

    Added Edit Progression Path Use Case

---
+++ api/ExpressedRealms.Expressions.UseCases/ProgressionPaths/Edit/EditProgressionPath.cs
@@ -0,0 +1,7 @@
+using ExpressedRealms.Shared;
+using FluentResults;
+
+namespace ExpressedRealms.Expressions.UseCases.ProgressionPaths.Edit;
+
+public interface IEditProgressionPathUseCase
+    : IGenericUseCase<Result, EditProgressionPathModel> { }
---
+++ api/ExpressedRealms.Expressions.UseCases/ProgressionPaths/Edit/EditProgressionPathUseCase.cs
@@ -0,0 +1,31 @@
+using ExpressedRealms.Expressions.Repository.ProgressionPaths;
+using ExpressedRealms.UseCases.Shared;
+using FluentResults;
+
+namespace ExpressedRealms.Expressions.UseCases.ProgressionPaths.Edit;
+
+internal sealed class EditProgressionPathUseCase(
+    IProgressionPathRepository repository,
+    EditProgressionPathModelValidator validator,
+    CancellationToken cancellationToken
+) : IEditProgressionPathUseCase
+{
+    public async Task<Result> ExecuteAsync(EditProgressionPathModel model)
+    {
+        var result = await ValidationHelper.ValidateAndHandleErrorsAsync(
+            validator,
+            model,
+            cancellationToken
+        );
+
+        if (result.IsFailed)
+            return Result.Fail(result.Errors);
+
+        var progressionPath = await repository.GetProgressionPathForEditing(model.Id);
+
+        progressionPath.Name = model.Name;
+        progressionPath.Description = model.Description;
+
+        return Result.Ok();
+    }
+}
