d00e2503e01077affba8d5d3475231b78e79ff26
noremacskich <noremacskich@gmail.com>
2025-09-29 17:19:11 -0500

    Fixed Form Wrapper not updating values

--- client/src/FormWrappers/FormDropdownWrapper.vue
+++ client/src/FormWrappers/FormDropdownWrapper.vue
@@ -44,7 +44,7 @@ const dataCyTagCalc = computed(() => {
     <label :for="dataCyTagCalc">{{ model.label }}<span v-if="model.isRequired" class="text-danger font-italic"> (Required)</span></label>
     <Skeleton v-if="showSkeleton" :id="dataCyTagCalc + '-skeleton'" class="w-100" height="3em" />
     <Select
-      v-else :id="dataCyTagCalc" v-model="model.field" :options="options" :option-label="optionLabel"
+      v-else :id="dataCyTagCalc" v-model="model.field.value" :options="options" :option-label="optionLabel"
       :data-cy="dataCyTagCalc"
       class="w-100" :class="{ 'p-invalid': model.error && model.error.length > 0 }" v-bind="$attrs"
     />
