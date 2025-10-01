1376787c953c455d6bab7412583d8182220ba660
noremacskich <noremacskich@gmail.com>
2025-09-30 22:16:06 -0500

    Powers will now correctly

--- api/ExpressedRealms.Powers.UseCases/CharacterPower/GetPickablePowers/GetAvailablePowersUseCase.cs
+++ api/ExpressedRealms.Powers.UseCases/CharacterPower/GetPickablePowers/GetAvailablePowersUseCase.cs
@@ -60,15 +60,19 @@ internal sealed class GetAvailablePowersUseCase(
 
                 // Primary element gets all powers
                 // Secondary element gets all powers that are intermediate or below (<= 2)
-                filteredPowers = filteredPowers
-                    .Where(x =>
-                        x.Powers.Any(y =>
-                            y.Category!.Any(z => z.Name == primaryElement)
-                            || y.Category!.Any(z => z.Name == secondaryElement)
-                                && y.PowerLevel.Id <= 2
-                        )
-                    )
-                    .ToList();
+                filteredPowers = filteredPowers.Select(x => new PowerPathToc()
+                {
+                    Id = x.Id,
+                    Description = x.Description,
+                    Name = x.Name,
+                    Powers = x.Powers.Where(y => 
+                            (y.Category!.Any(z => z.Name == primaryElement)) ||
+                            (y.Category!.Any(z => z.Name.Equals("general", StringComparison.InvariantCultureIgnoreCase))) 
+                             || (y.Category!.Any(z => z.Name.Equals(secondaryElement,
+                                     StringComparison.InvariantCultureIgnoreCase))
+                                 && y.PowerLevel.Id < 3)).ToList()
+                }).Where(x => x.Powers.Any()).ToList();
+
             }
         }
 
