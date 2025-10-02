40e1ebe042db28759c8e47185f18801dad108565
Noremac Skich <noremacskich@gmail.com>
2025-10-02 13:59:21 -0500

    Player Name page now Redirects Correctly (#761)
    We needed to update player info after submitting the new name, at which
    point it can redirect again

--- api/ExpressedRealms.Server/EndPoints/PlayerEndpoints/DTOs/PlayerDTO.cs
+++ api/ExpressedRealms.Server/EndPoints/PlayerEndpoints/DTOs/PlayerDTO.cs
@@ -7,22 +7,4 @@ public class PlayerDTO
     /// </summary>
     /// <example>John Doe</example>
     public string Name { get; set; } = null!;
-
-    /// <summary>
-    /// 10 Digit Phone Number
-    /// </summary>
-    /// <example>(555) 555-5555</example>
-    public string PhoneNumber { get; set; } = null!;
-
-    /// <summary>
-    /// Player Location
-    /// </summary>
-    /// <example>Chicago</example>
-    public string City { get; set; } = null!;
-
-    /// <summary>
-    /// State Abbreviation
-    /// </summary>
-    /// <example>IL</example>
-    public string State { get; set; } = null!;
 }
