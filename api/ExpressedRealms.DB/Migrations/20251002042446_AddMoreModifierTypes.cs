7c58b215133d7aa443708f6e1a1ed0b7c1ed9909
noremacskich <noremacskich@gmail.com>
2025-10-01 23:31:56 -0500

    Added additional Proficiencies

---
+++ api/ExpressedRealms.DB/Migrations/20251002042446_AddMoreModifierTypes.cs
@@ -0,0 +1,37 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace ExpressedRealms.DB.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddMoreModifierTypes : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.Sql(@"
+                insert into public.stat_modifier(id, name)
+                values 
+                (12, 'Strike'),
+                (13, 'Thrust'),
+                (14, 'Throw'),
+                (15, 'Shoot'),
+                (16, 'Cast'),
+                (17, 'Project'),
+                (18, 'Dodge'),
+                (19, 'Parry'),
+                (20, 'Evade Throw'),
+                (21, 'Evade Shoot'),
+                (22, 'Ward'),
+                (23, 'Deflect')
+            ");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
