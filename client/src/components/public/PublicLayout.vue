1c026519e9cc8f88826733c85dd300097708df2b
noremacskich <noremacskich@gmail.com>
2025-11-01 02:09:14 -0500

    Public Pages needed Dynamic Dialog

--- client/src/components/public/PublicLayout.vue
+++ client/src/components/public/PublicLayout.vue
@@ -4,6 +4,8 @@ import Message from 'primevue/message'
 import { RouterView } from 'vue-router'
 import Card from 'primevue/card'
 import PublicMenu from '@/components/public/PublicMenu.vue'
+
+import DynamicDialog from 'primevue/dynamicdialog'
 </script>
 
 <template>
@@ -11,6 +13,7 @@ import PublicMenu from '@/components/public/PublicMenu.vue'
     <Message icon="pi pi-exclamation-triangle" class="sticky-top ms-0 me-0 mt-2 mb-2 m-md-2" severity="warn">
       This site is currently in a BETA state, not all features or content is in place.
     </Message>
+    <DynamicDialog />
     <PublicMenu />
     <div class="container-fluid constrain-width flex-grow-1 mb-3 mt-3">
       <router-view />
