487f0ea5117b7e253f7733987fc0e4108672f690
Noremac Skich <noremacskich@gmail.com>
2025-11-02 08:11:29 -0600

    Fixed Delete Character and Added Confirmation Message (#837)

--- client/src/components/expressions/BlessingToC.vue
+++ client/src/components/expressions/BlessingToC.vue
@@ -1,9 +1,9 @@
 <script setup lang="ts">
 
-import {makeIdSafe} from '@/utilities/stringUtilities'
+import { makeIdSafe } from '@/utilities/stringUtilities'
 import Skeleton from 'primevue/skeleton'
-import {scrollToSection} from '@/components/expressions/expressionUtilities'
-import {blessingsStore} from '@/components/blessings/stores/blessingsStore.ts'
+import { scrollToSection } from '@/components/expressions/expressionUtilities'
+import { blessingsStore } from '@/components/blessings/stores/blessingsStore.ts'
 
 const store = blessingsStore()
 
@@ -21,10 +21,10 @@ const props = defineProps({
     <Skeleton v-if="props.showSkeleton" id="toc-skeleton" class="mb-2" height="1.5em" />
     <a v-else class="p-1 tocItem" :href="'#' + makeIdSafe(type.name)" @click.prevent="scrollToSection(type.name)">{{ type.name }}</a>
     <div v-for="path in type.subCategories" :key="path.name" class="ps-4">
-      <Skeleton v-if="props.showSkeleton" id="toc-skeleton" class="mb-2" height="1.5em" />
+      <Skeleton v-if="props.showSkeleton" id="sub-category-skeleton" class="mb-2" height="1.5em" />
       <a v-else class="p-1 tocItem" :href="'#' + makeIdSafe(path.name)" @click.prevent="scrollToSection(path.name)">{{ path.name }}</a>
       <div v-for="power in path.blessings" :key="power.id" class="ps-4">
-        <Skeleton v-if="props.showSkeleton" id="toc-skeleton" class="mb-2" height="1.5em" />
+        <Skeleton v-if="props.showSkeleton" id="blessings-skeleton" class="mb-2" height="1.5em" />
         <a v-else class="p-1 tocItem" :href="'#' + makeIdSafe(power.name)" @click.prevent="scrollToSection(power.name)">{{ power.name }}</a>
       </div>
     </div>
