205d281e595dff1d17ae8827031bfd1f569b53d9
noremacskich <noremacskich@gmail.com>
2025-11-17 12:36:50 -0600

    Fixed Sonar Cloud Finding

--- api/ExpressedRealms.Characters.API/AssignedXp/Get/GetAssignedXpResponse.cs
+++ api/ExpressedRealms.Characters.API/AssignedXp/Get/GetAssignedXpResponse.cs
@@ -2,5 +2,5 @@ namespace ExpressedRealms.Characters.API.AssignedXp.Get;
 
 public class GetAssignedXpResponse
 {
-    public List<AssignedXpInfo> AssignedXpInfo { get; set; }
+    public List<AssignedXpInfo> AssignedXpInfo { get; set; } = new();
 }
