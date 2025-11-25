903a43bedd1ebd0b07a8591530a83cf75a705fd7
noremacskich <noremacskich@gmail.com>
2025-11-25 03:00:53 -0600

    Simplified Experience Breakdown Use Case
    And updated to match new XP source with feature flags

--- api/ExpressedRealms.Characters.UseCases/ExperienceBreakdown/GetCharacterExperienceBreakdownModelValidator.cs
+++ api/ExpressedRealms.Characters.UseCases/ExperienceBreakdown/GetCharacterExperienceBreakdownModelValidator.cs
@@ -17,7 +17,7 @@ internal sealed class GetCharacterExperienceBreakdownModelValidator
         RuleFor(x => x.CharacterId)
             .NotEmpty()
             .WithMessage("Character Id is required.")
-            .MustAsync(async (x, y) => await characterRepository.CharacterExistsAsync(x))
-            .WithMessage("The Character does not exist.");
+            .MustAsync(async (x, y) => await characterRepository.FindCharacterAsync(x) is not null)
+            .WithMessage("The Character Id does not exist.");
     }
 }
