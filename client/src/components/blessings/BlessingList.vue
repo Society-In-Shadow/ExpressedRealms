bee1ed4aeb261bc5d5f2a3abbc042ddf3c098f11
Noremac Skich <noremacskich@gmail.com>
2025-12-06 04:40:29 -0600

    Split Add / DisAds and Added Help Info (#866)
    Also, on mobile, it will now show the overall navigation list on the
    wizard on initial load
    
    Sub Categories for Blessings now have unique ids on ToC

--- client/src/components/blessings/BlessingList.vue
+++ client/src/components/blessings/BlessingList.vue
@@ -1,12 +1,12 @@
 <script setup lang="ts">
 
-import {onMounted, ref} from 'vue'
-import {blessingsStore} from '@/components/blessings/stores/blessingsStore'
+import { onMounted, ref } from 'vue'
+import { blessingsStore } from '@/components/blessings/stores/blessingsStore'
 import BlessingItem from '@/components/blessings/BlessingItem.vue'
-import {UserRoles, userStore} from '@/stores/userStore.ts'
+import { UserRoles, userStore } from '@/stores/userStore.ts'
 import AddBlessing from '@/components/blessings/AddBlessing.vue'
 import Button from 'primevue/button'
-import {makeIdSafe} from '@/utilities/stringUtilities.ts'
+import { makeIdSafe } from '@/utilities/stringUtilities.ts'
 
 const store = blessingsStore()
 const userInfo = userStore()
@@ -43,7 +43,7 @@ onMounted(async () => {
       {{ type.name }}
     </h1>
     <div v-for="subCategory in type.subCategories" :key="subCategory.name">
-      <h2 :id="makeIdSafe(subCategory.name)">
+      <h2 :id="makeIdSafe(`${subCategory.name}-${type.name}`)">
         {{ subCategory.name }}
       </h2>
       <div v-for="blessing in subCategory.blessings" :key="blessing.id">
---
+++ client/src/components/characters/character/wizard/blessings/BlessingInfo.vue
@@ -0,0 +1,36 @@
+<script setup lang="ts">
+
+const props = defineProps({
+  type: {
+    type: String,
+    required: true,
+  },
+})
+
+</script>
+
+<template>
+  <div v-if="props.type === 'advantage'">
+    <p>
+      Advantages are an optional way of extending your character with Discretionary XP.
+    </p>
+    <p>
+      You may only spend up to 8 XP on advantages.
+    </p>
+    <p>
+      Choose your advantages carefully as they may only be taken during character creation.
+    </p>
+  </div>
+  <div v-else>
+    <p>Disadvantages are an optional way of gaining more Discretionary XP.</p>
+    <p>You may only spend up to 8 XP on disadvantages.</p>
+    <p>
+      Choose your disadvantages carefully as they may only be taken during character creation.
+    </p>
+    <p>
+      Disadvantages may be overcome, at a cost. A character has the ability of overcoming a disadvantage
+      by spending twice the experience that it granted them. Removal of a disadvantage also requires
+      approval from your GO and appropriate in game reasons.
+    </p>
+  </div>
+</template>
