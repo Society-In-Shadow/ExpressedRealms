0ac727f04450e0af3d2d38b29127f295d0abf8c8
noremacskich <noremacskich@gmail.com>
2025-12-08 07:13:08 -0600

    Added use case to find and handle event update messages

---
+++ api/ExpressedRealms.Events.API.Tests.Unit/Events/EventReminderHandlerUseCaseTests.cs
@@ -0,0 +1,116 @@
+using ExpressedRealms.DB.Models.Events.EventSetup;
+using ExpressedRealms.Events.API.Repositories.Events;
+using ExpressedRealms.Events.API.UseCases.Events.SendEventPublishedMessages;
+using ExpressedRealms.Events.API.UseCases.Events.TriggerEventReminder;
+using FakeItEasy;
+using Microsoft.Extensions.Internal;
+using Xunit;
+
+namespace ExpressedRealms.Events.API.Tests.Unit.Events;
+
+public class EventReminderHandlerUseCaseTests
+{
+    private readonly EventReminderHandlerUseCase _useCase;
+    private readonly IEventRepository _repository;
+    private readonly List<Event> _dbModel;
+    private readonly ISendEventPublishedMessagesUseCase _sendMessageUseCase;
+    private readonly ISystemClock _systemClock;
+
+    public EventReminderHandlerUseCaseTests()
+    {
+        _dbModel = new List<Event>()
+        {
+            new()
+            {
+                Id = 1,
+                Name = "Test Event 1",
+                Location = "Location 1",
+                StartDate = DateOnly.Parse("10/31/2025"),
+                EndDate = DateOnly.Parse("11/02/2025"),
+                WebsiteName = "Website Name 1",
+                AdditionalNotes = "Additional Notes 1",
+                WebsiteUrl = "https://societyinshadows.org",
+                TimeZoneId = "UTC",
+                ConExperience = 20,
+                IsPublished = true,
+            },
+            new()
+            {
+                Id = 2,
+                Name = "Test Event 2",
+                Location = "Location 2",
+                StartDate = DateOnly.Parse("10/30/2025"),
+                EndDate = DateOnly.Parse("11/04/2025"),
+                WebsiteName = "Website Name 2",
+                AdditionalNotes = "Additional Notes 2",
+                WebsiteUrl = "https://societyinshadows.org",
+                TimeZoneId = "UTC",
+                ConExperience = 25,
+                IsPublished = false,
+            },
+        };
+
+        _repository = A.Fake<IEventRepository>();
+        _sendMessageUseCase = A.Fake<ISendEventPublishedMessagesUseCase>();
+        _systemClock = A.Fake<ISystemClock>();
+        
+        A.CallTo(() => _systemClock.UtcNow).Returns(new DateTime(2025, 10, 30, 12, 0, 0));
+        A.CallTo(() => _repository.GetCurrenOrFutureEvents()).Returns(_dbModel);
+
+        _useCase = new EventReminderHandlerUseCase(_repository, _sendMessageUseCase, _systemClock);
+    }
+
+    [Fact]
+    public async Task UseCase_WillProcessInternalReminder_5WeeksOut()
+    {
+        _dbModel[0].StartDate = DateOnly.FromDateTime(_systemClock.UtcNow.DateTime).AddMonths(1).AddDays(7);
+        await _useCase.ExecuteAsync();
+
+        A.CallTo(() => _sendMessageUseCase.ExecuteAsync(A<SendEventPublishedMessagesModel>.That
+                .Matches(k => k.Id == 1 && k.PublishType == PublishType.InternalReminder)))
+            .MustHaveHappenedOnceExactly();
+    }
+
+    [Fact]
+    public async Task UseCase_WillProcessOneWeekReminder_1WeekOut()
+    {
+        _dbModel[0].StartDate = DateOnly.FromDateTime(_systemClock.UtcNow.DateTime).AddDays(7);
+        await _useCase.ExecuteAsync();
+
+        A.CallTo(() => _sendMessageUseCase.ExecuteAsync(A<SendEventPublishedMessagesModel>.That
+                .Matches(k => k.Id == 1 && k.PublishType == PublishType.OneWeekReminder)))
+            .MustHaveHappenedOnceExactly();
+    }
+    
+    [Fact]
+    public async Task UseCase_WillProcessOneMonthReminder_1MonthOut()
+    {
+        _dbModel[0].StartDate = DateOnly.FromDateTime(_systemClock.UtcNow.DateTime).AddMonths(1);
+        await _useCase.ExecuteAsync();
+
+        A.CallTo(() => _sendMessageUseCase.ExecuteAsync(A<SendEventPublishedMessagesModel>.That
+                .Matches(k => k.Id == 1 && k.PublishType == PublishType.OneMonthReminder)))
+            .MustHaveHappenedOnceExactly();
+    }
+    
+    [Theory]
+    [InlineData("10/29/2025")]
+    [InlineData("10/30/2025")]
+    [InlineData("10/31/2025")]
+    public async Task UseCase_WillProcessDayOfReminder_BetweenStartAndEndDatesInclusive(string dayOfString)
+    {
+        _dbModel[0].StartDate = DateOnly.Parse("10/29/2025");
+        _dbModel[0].EndDate = DateOnly.Parse("10/31/2025");
+
+        var testDate = DateTimeOffset.Parse(dayOfString).Date;
+        A.CallTo(() => _systemClock.UtcNow).Returns(testDate);
+
+        await _useCase.ExecuteAsync();
+
+        A.CallTo(() => _sendMessageUseCase.ExecuteAsync(
+                A<SendEventPublishedMessagesModel>.That.Matches(k =>
+                    k.Id == 1 &&
+                    k.PublishType == PublishType.DayOfReminder)))
+            .MustHaveHappenedOnceExactly();
+    }
+}
---
+++ api/ExpressedRealms.Events.API/UseCases/Events/TriggerEventReminder/EventReminderHandlerUseCase.cs
@@ -0,0 +1,55 @@
+using ExpressedRealms.DB.Models.Events.EventSetup;
+using ExpressedRealms.Events.API.Repositories.Events;
+using ExpressedRealms.Events.API.UseCases.Events.SendEventPublishedMessages;
+using FluentResults;
+using Microsoft.Extensions.Internal;
+
+namespace ExpressedRealms.Events.API.UseCases.Events.TriggerEventReminder;
+
+internal sealed class EventReminderHandlerUseCase(
+    IEventRepository eventRepository, 
+    ISendEventPublishedMessagesUseCase sendEventPublishedMessagesUseCase,
+    ISystemClock systemClock)
+    : IEventReminderHandlerUseCase
+{
+    public async Task<Result> ExecuteAsync()
+    {
+        var events = await eventRepository.GetCurrenOrFutureEvents();
+        
+        var today = DateOnly.FromDateTime(systemClock.UtcNow.DateTime);
+        
+        var eventsToPublish = events
+            .Select(x => GetEventTypeOrDefault(x, today))
+            .Where(x => x.HasValue)
+            .ToList();
+        
+        foreach(var publishEvents in eventsToPublish)
+        {
+            await sendEventPublishedMessagesUseCase.ExecuteAsync(new SendEventPublishedMessagesModel()
+            {
+                Id = publishEvents!.Value.Key,
+                PublishType = publishEvents.Value.Value
+            });
+        }
+        
+        return Result.Ok();
+        
+    }
+    
+    private static KeyValuePair<int, PublishType>? GetEventTypeOrDefault(Event @event, DateOnly today)
+    {
+        if (@event.StartDate == today.AddMonths(1).AddDays(7))
+            return new KeyValuePair<int, PublishType>(@event.Id, PublishType.InternalReminder);
+        
+        if(@event.StartDate == today.AddDays(7))
+            return new KeyValuePair<int, PublishType>(@event.Id, PublishType.OneWeekReminder);
+        
+        if(@event.StartDate == today.AddMonths(1))
+            return new KeyValuePair<int, PublishType>(@event.Id, PublishType.OneMonthReminder);
+        
+        if (@event.StartDate <= today && @event.EndDate >= today)
+            return new KeyValuePair<int, PublishType>(@event.Id, PublishType.DayOfReminder);
+        
+        return null;
+    }
+}
