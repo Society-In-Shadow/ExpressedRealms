8a315bab6f6dcca25cf424321251ca664d204dc2
noremacskich <noremacskich@gmail.com>
2025-12-27 21:28:29 -0600

    Added Sync Permissions project
    Will create a typescript version of the permissions and automatically update the frontend on rebuild

---
+++ api/ExpressedRealms.PermissionSync/Program.cs
@@ -0,0 +1,28 @@
+﻿using System.Text;
+using ExpressedRealms.Authentication.PermissionCollection.PermissionManager.PermissionHelper;
+
+var resources = PermissionManagerHelper.GetCodeSidePermissions();
+
+var builder = new StringBuilder();
+
+builder.AppendLine("/**");
+builder.AppendLine(" * Auto-Generated, Do Not Edit");
+builder.AppendLine(" */");
+
+builder.AppendLine("export const Permissions = {");
+foreach (var resource in resources)
+{
+    builder.AppendLine($"  {resource.Name}: {{");
+    
+    foreach (var permission in resource.Permissions)
+    {
+        builder.AppendLine($"    {permission.Name}: \'{permission.Key}\',");
+    }
+    builder.AppendLine("  } as const,");
+}
+
+builder.AppendLine("} as const");
+builder.AppendLine();
+builder.Append("export type Permission = typeof Permissions[keyof typeof Permissions]");
+
+Console.WriteLine(builder.ToString());
---
+++ client/src/types/Permissions.ts
@@ -0,0 +1,20 @@
+/**
+ * Auto-Generated, Do Not Edit
+ */
+export const Permissions = {
+  Event: {
+    Edit: 'event.edit',
+    View: 'event.view',
+    Create: 'event.create',
+    Delete: 'event.delete',
+    Publish: 'event.publish',
+  } as const,
+  EventScheduleItem: {
+    Edit: 'eventscheduleitem.edit',
+    View: 'eventscheduleitem.view',
+    Create: 'eventscheduleitem.create',
+    Delete: 'eventscheduleitem.delete',
+  } as const,
+} as const
+
+export type Permission = typeof Permissions[keyof typeof Permissions]
