5587dba53e16e2fb41fd3b20c0f661055550ee44
noremacskich <noremacskich@gmail.com>
2026-01-12 06:13:20 -0600

    Fixed megalinter findings

--- api/ExpressedRealms.DB/Models/Authorization/RolePermissionMappingSetup/Audit/RolePermissionMappingAuditTrailConfiguration.cs
+++ api/ExpressedRealms.DB/Models/Authorization/RolePermissionMappingSetup/Audit/RolePermissionMappingAuditTrailConfiguration.cs
@@ -16,7 +16,7 @@ internal class RolePermissionMappingAuditTrailConfiguration
         builder
             .Property(e => e.RolePermissionMappingId)
             .HasColumnName("role_permission_mapping_id");
-        
+
         builder.Property(e => e.RoleId).HasColumnName("role_id").IsRequired();
         builder.Property(e => e.PermissionId).HasColumnName("permission_id");
     }
