7bd1b6c899375eed9b84418267c6dae7ac0d638c
noremacskich <noremacskich@gmail.com>
2026-01-20 14:09:01 -0600

    Added permission checks to Role List
    Forms now have an overall is-disabled attribute that will flow down to sub components

--- client/src/FormWrappers/FormInputTextWrapper.vue
+++ client/src/FormWrappers/FormInputTextWrapper.vue
@@ -30,6 +30,7 @@ const dataCyTagCalc = computed(() => {
 })
 
 const showSkeleton = props.showSkeleton ?? inject('showSkeleton', false)
+const isDisabled = inject('isDisabled', false)
 const isInvalid = computed(() => (model.value.error.value ?? '').length > 0)
 
 </script>
@@ -41,7 +42,7 @@ const isInvalid = computed(() => (model.value.error.value ?? '').length > 0)
     <InputText
       v-else
       :id="dataCyTagCalc" v-model="model.field.value" :data-cy="dataCyTagCalc" class="w-100"
-      :invalid="isInvalid" v-bind="$attrs"
+      :invalid="isInvalid" v-bind="$attrs" :disabled="isDisabled"
     />
     <small v-if="isInvalid" :data-cy="dataCyTagCalc + '-help'" class="text-danger">{{ model.error }}</small>
     <slot />
