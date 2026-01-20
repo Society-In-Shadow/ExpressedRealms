fe61e78bd55c927797b335a93913e4f9a802b130
noremacskich <noremacskich@gmail.com>
2026-01-20 17:08:44 -0600

    Users now have Role Manage in API

--- api/ExpressedRealms.Admin.API/AdminEndpoints/AdminEndpoints.cs
+++ api/ExpressedRealms.Admin.API/AdminEndpoints/AdminEndpoints.cs
@@ -1,4 +1,6 @@
+using ExpressedRealms.Admin.API.AdminEndpoints.AddRoleUser;
 using ExpressedRealms.Admin.API.AdminEndpoints.BypassEmailConfirmation;
+using ExpressedRealms.Admin.API.AdminEndpoints.DeleteRoleUserMapping;
 using ExpressedRealms.Admin.API.AdminEndpoints.DisableUser;
 using ExpressedRealms.Admin.API.AdminEndpoints.Dtos;
 using ExpressedRealms.Admin.API.AdminEndpoints.EnableUser;
@@ -42,6 +44,14 @@ public static class AdminEndpoints
             .MapGet("users/{userId}/roles", GetRolesForUserEndpoint.Execute)
             .RequirePermission(Permissions.Player.ManageRoles);
 
+        endpointGroup
+            .MapPost("users/{userid}/roles", AddRoleUserEndpoint.Execute)
+            .RequirePermission(Permissions.Player.ManageRoles);
+        
+        endpointGroup
+            .MapDelete("users/{userid}/roles/{roleId}", DeleteRoleUserMappingEndpoint.Execute)
+            .RequirePermission(Permissions.Player.ManageRoles);
+        
         endpointGroup
             .MapPut("user/{userid}/role", UpdateUserRoleEndpoint.Execute)
             .RequirePermission(Permissions.Player.ManageRoles);
---
+++ api/ExpressedRealms.Admin.API/AdminEndpoints/DeleteRoleUserMapping/DeleteRoleUserMappingEndpoint.cs
@@ -0,0 +1,26 @@
+using ExpressedRealms.Admin.UseCases.Roles.DeleteUserRole;
+using ExpressedRealms.Server.Shared;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Http.HttpResults;
+
+namespace ExpressedRealms.Admin.API.AdminEndpoints.DeleteRoleUserMapping;
+
+public static class DeleteRoleUserMappingEndpoint
+{
+    public static async Task<Results<Ok, ValidationProblem, NotFound>> Execute(
+        string userId,
+        int roleId,
+        IDeleteUserRoleUseCase useCase
+    )
+    {
+        var results = await useCase.ExecuteAsync(new() { RoleId = roleId, UserId = userId });
+
+        if (results.HasValidationError(out var validationProblem))
+            return validationProblem;
+        if (results.HasNotFound(out var notFound))
+            return notFound;
+        results.ThrowIfErrorNotHandled();
+
+        return TypedResults.Ok();
+    }
+}
