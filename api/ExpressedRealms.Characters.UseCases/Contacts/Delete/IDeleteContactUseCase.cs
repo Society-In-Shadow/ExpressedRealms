9cc8e65d1013b9eb560501381047b002b892679a
Noremac Skich <noremacskich@gmail.com>
2026-01-26 12:00:52 -0600

    Delete Contact Use Case and API (#947)

---
+++ api/ExpressedRealms.Characters.UseCases/Contacts/Delete/IDeleteContactUseCase.cs
@@ -0,0 +1,6 @@
+using ExpressedRealms.Shared;
+using FluentResults;
+
+namespace ExpressedRealms.Characters.UseCases.Contacts.Delete;
+
+public interface IDeleteContactUseCase : IGenericUseCase<Result, DeleteContactModel> { }
