18e0e2c6a2287ca0367834827632f317337b1788
noremacskich <noremacskich@gmail.com>
2026-01-28 19:31:33 -0600

    Fixed Sonar Cloud Finding

--- api/ExpressedRealms.Characters.Reports/CRB/Data/SupportingData/ContactInfo.cs
+++ api/ExpressedRealms.Characters.Reports/CRB/Data/SupportingData/ContactInfo.cs
@@ -3,7 +3,7 @@ namespace ExpressedRealms.Characters.Reports.CRB.Data.SupportingData;
 public class ContactInfo
 {
     public required string Name { get; set; }
-    public string? KnowledgeName { get; set; }
+    public required string KnowledgeName { get; set; }
     public int KnowledgeLevel { get; set; }
     public int NumberOfUses { get; set; }
 }
