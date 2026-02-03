5b889c8b6be60cd094eb72e8791509dd008ade9d
noremacskich <noremacskich@gmail.com>
2026-02-02 20:39:05 -0600

    Added Unit Tests

--- api/ExpressedRealms.Events.API/UseCases/EventQuestions/Create/CreateEventQuestionModelValidator.cs
+++ api/ExpressedRealms.Events.API/UseCases/EventQuestions/Create/CreateEventQuestionModelValidator.cs
@@ -13,20 +13,27 @@ internal sealed class CreateEventQuestionModelValidator
     {
         RuleFor(x => x.Question)
             .NotEmpty()
-            .WithMessage("Question is required.");
+            .WithMessage("Question is required.")
+            .MaximumLength(500)
+            .WithMessage("Question must be between 1 and 500 characters.");
         
         RuleFor(x => x)
             .MustAsync(async (x, y) => !await questionRepository.IsExistingEventQuestion(x.EventId, x.Question))
-            .WithMessage("Question already exists.");
+            .WithMessage("Question already exists.")
+            .WithName(nameof(CreateEventQuestionModel.Question));
 
         RuleFor(x => x.QuestionTypeId)
+            .NotEmpty()
+            .WithMessage("Question Type is required.")
             .MustAsync(async (x, y) => await questionRepository.IsExistingCustomizableQuestionType(x))
-            .WithMessage("Question Type does not exist.");
+            .WithMessage("Question Type does not exist.")
+            .WithName(nameof(CreateEventQuestionModel.QuestionTypeId));
         
         RuleFor(x => x.EventId)
             .NotEmpty()
             .WithMessage("Event Id is required.")
             .MustAsync(async (x, y) => await repository.IsExistingEvent(x))
+            .WithErrorCode("NotFound")
             .WithMessage("Event does not exist.");
     }
 }
