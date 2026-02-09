d995015671d4b771e51e27d3541251e619fba967
noremacskich <noremacskich@gmail.com>
2026-02-08 22:59:51 -0600

    Added LookupId to Player in Database

---
+++ api/ExpressedRealms.DB/Migrations/20260209045818_AddLookupIdToPlayer.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace ExpressedRealms.DB.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddLookupIdToPlayer : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "lookup_id",
+                table: "Players",
+                type: "char(8)",
+                nullable: true);
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Players_lookup_id",
+                table: "Players",
+                column: "lookup_id",
+                unique: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropIndex(
+                name: "IX_Players_lookup_id",
+                table: "Players");
+
+            migrationBuilder.DropColumn(
+                name: "lookup_id",
+                table: "Players");
+        }
+    }
+}
--- api/ExpressedRealms.DB/UserProfile/PlayerDBModels/PlayerSetup/PlayerConfiguration.cs
+++ api/ExpressedRealms.DB/UserProfile/PlayerDBModels/PlayerSetup/PlayerConfiguration.cs
@@ -14,6 +14,15 @@ internal class PlayerConfiguration : IEntityTypeConfiguration<Player>
         builder.Property(x => x.UserId).IsRequired();
 
         builder.Property(x => x.Id).IsRequired();
+        
+        builder.Property(x => x.LookupId)
+            .HasColumnType("char(8)")
+            .HasColumnName("lookup_id");
+            
+        /*.IsRequired()*/
+
+        builder.HasIndex(x => x.LookupId)
+            .IsUnique();
 
         builder
             .HasOne(x => x.User)
