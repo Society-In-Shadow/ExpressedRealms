d752be2feb8a44e25b89cd70339db20154ebf6e0
noremacskich <noremacskich@gmail.com>
2026-02-10 07:56:49 -0600

    Split the scanner into it's own separate component

--- client/src/components/conCheckin/GoCheckinPage.vue
+++ client/src/components/conCheckin/GoCheckinPage.vue
@@ -1,6 +1,6 @@
 <script setup lang="ts">
 
-import { nextTick, onBeforeMount, ref } from 'vue'
+import { onBeforeMount, ref } from 'vue'
 import { FeatureFlags, userStore } from '@/stores/userStore.ts'
 import { EventCheckinStore } from '@/components/conCheckin/stores/eventCheckinStore.ts'
 import { useRouter } from 'vue-router'
@@ -8,10 +8,9 @@ import Stepper from 'primevue/stepper'
 import StepItem from 'primevue/stepitem'
 import Step from 'primevue/step'
 import StepPanel from 'primevue/steppanel'
-import { QrcodeStream } from 'vue-qrcode-reader'
 import Button from 'primevue/button'
-import InputText from 'primevue/inputtext'
 import StonePuller from '@/components/stonePuller/StonePuller.vue'
+import CharacterScanner from '@/components/conCheckin/support/CharacterScanner.vue'
 
 const eventCheckinInfo = EventCheckinStore()
 const userInfo = userStore()
@@ -28,52 +27,11 @@ onBeforeMount(async () => {
   }
 })
 
-const loading = ref(true)
-const destroyed = ref(false)
-const result = ref('')
 const stepperStep = ref('1')
 
-function onCameraOn() {
-  loading.value = false
-}
-
-async function reload() {
-  destroyed.value = true
-  await nextTick()
-  destroyed.value = false
-  loading.value = true
-}
-
 async function onDetect(detectedCodes) {
-  result.value = detectedCodes.map(code => code.rawValue)[0]
-  await eventCheckinInfo.getGoCheckinInfo(result.value)
+  await eventCheckinInfo.getGoCheckinInfo(detectedCodes)
   stepperStep.value = '2'
-}
-
-async function lookupManual() {
-  await eventCheckinInfo.getGoCheckinInfo(result.value)
-  stepperStep.value = '2'
-}
-
-function clearResult() {
-  result.value = ''
-}
-
-function paintOutline(detectedCodes, ctx) {
-  for (const detectedCode of detectedCodes) {
-    const [firstPoint, ...otherPoints] = detectedCode.cornerPoints
-
-    ctx.strokeStyle = 'red'
-
-    ctx.beginPath()
-    ctx.moveTo(firstPoint.x, firstPoint.y)
-    for (const { x, y } of otherPoints) {
-      ctx.lineTo(x, y)
-    }
-    ctx.lineTo(firstPoint.x, firstPoint.y)
-    ctx.closePath()
-    ctx.stroke()
-  }
 }
 
 </script>
@@ -85,26 +43,7 @@ function paintOutline(detectedCodes, ctx) {
       <StepPanel>
         <p>Ask the user to open up their account, and click on the "Event Checkin Details" tile at the top of the page</p>
         <p>This should pull up a QR code you can scan below</p>
-        <div class="w-100">
-          <qrcode-stream
-            v-if="!destroyed && result == ''"
-            :track="paintOutline"
-            @camera-on="onCameraOn"
-            @detect="onDetect"
-          >
-            <div
-              v-if="loading"
-              class="loading-indicator"
-            >
-              Loading...
-            </div>
-          </qrcode-stream>
-        </div>
-        <div>
-          <p>If the scanner is not working, you can manually put in the lookup below</p>
-          <InputText v-model="result" placeholder="Manual Lookup" minlength="8" maxlength="8" />
-          <Button label="Lookup" @click="lookupManual" />
-        </div>
+        <CharacterScanner @detected-code="onDetect" />
       </StepPanel>
     </StepItem>
     <StepItem value="2">
@@ -146,11 +85,3 @@ function paintOutline(detectedCodes, ctx) {
     </StepItem>
   </Stepper>
 </template>
-
-<style>
-@media(max-width: 768px){
-  .w-md-100{
-    width: 100% !important;
-  }
-}
-</style>
