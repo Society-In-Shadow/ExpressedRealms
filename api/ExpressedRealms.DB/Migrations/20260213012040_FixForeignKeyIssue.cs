3a3b65cba0fe4a12d2a7dba621cdee5372a1d33e
noremacskich <noremacskich@gmail.com>
2026-02-13 00:51:16 -0600

    forgot to commit migrations

---
+++ api/ExpressedRealms.DB/Migrations/20260213012040_FixForeignKeyIssue.cs
@@ -0,0 +1,78 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace ExpressedRealms.DB.Migrations
+{
+    /// <inheritdoc />
+    public partial class FixForeignKeyIssue : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_checkin_event_question_response_audit_trail_checkin_questio~",
+                table: "checkin_event_question_response_audit_trail");
+
+            migrationBuilder.DropIndex(
+                name: "IX_checkin_event_question_response_audit_trail_checkin_id",
+                table: "checkin_event_question_response_audit_trail");
+
+            migrationBuilder.DropIndex(
+                name: "IX_checkin_event_question_response_audit_trail_event_question_~",
+                table: "checkin_event_question_response_audit_trail");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_checkin_event_question_response_audit_trail_checkin_id_even~",
+                table: "checkin_event_question_response_audit_trail",
+                columns: new[] { "checkin_id", "event_question_id" });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_checkin_event_question_response_audit_trail_event_question_~",
+                table: "checkin_event_question_response_audit_trail",
+                column: "event_question_id");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_checkin_event_question_response_audit_trail_checkin_questio~",
+                table: "checkin_event_question_response_audit_trail",
+                columns: new[] { "checkin_id", "event_question_id" },
+                principalTable: "checkin_question_response",
+                principalColumns: new[] { "checkin_id", "event_question_id" },
+                onDelete: ReferentialAction.Restrict);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_checkin_event_question_response_audit_trail_checkin_questio~",
+                table: "checkin_event_question_response_audit_trail");
+
+            migrationBuilder.DropIndex(
+                name: "IX_checkin_event_question_response_audit_trail_checkin_id_even~",
+                table: "checkin_event_question_response_audit_trail");
+
+            migrationBuilder.DropIndex(
+                name: "IX_checkin_event_question_response_audit_trail_event_question_~",
+                table: "checkin_event_question_response_audit_trail");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_checkin_event_question_response_audit_trail_checkin_id",
+                table: "checkin_event_question_response_audit_trail",
+                column: "checkin_id");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_checkin_event_question_response_audit_trail_event_question_~",
+                table: "checkin_event_question_response_audit_trail",
+                columns: new[] { "event_question_id", "checkin_id" });
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_checkin_event_question_response_audit_trail_checkin_questio~",
+                table: "checkin_event_question_response_audit_trail",
+                columns: new[] { "event_question_id", "checkin_id" },
+                principalTable: "checkin_question_response",
+                principalColumns: new[] { "checkin_id", "event_question_id" },
+                onDelete: ReferentialAction.Restrict);
+        }
+    }
+}
