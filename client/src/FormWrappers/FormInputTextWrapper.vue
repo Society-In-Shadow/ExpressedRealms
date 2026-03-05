51b617038e4934749396d1264c83c75d59c1b1cb
noremacskich <noremacskich@gmail.com>
2026-03-05 02:13:13 -0600

    Is Disabled From Form Wrapper now has better support
    More things will now check the is disabled field from the form wrapper. Also fixed logic error

--- client/src/FormWrappers/FormInputTextWrapper.vue
+++ client/src/FormWrappers/FormInputTextWrapper.vue
@@ -38,7 +38,7 @@ const dataCyTagCalc = computed(() => {
 })
 
 const showSkeleton = props.showSkeleton ?? inject('showSkeleton', false)
-const isDisabled = props.isDisabled ?? inject('isDisabled', false)
+const isDisabled = props.isDisabled || inject('isDisabled', false)
 const isInvalid = computed(() => (model.value.error.value ?? '').length > 0)
 
 </script>
