a3577df112c7ef0ec22d022b836313537f34581b
noremacskich <noremacskich@gmail.com>
2026-03-09 05:10:30 -0500

    Fixed Sonar Cloud Finding

--- api/ExpressedRealms.Events.API/API/Events/Get/EventModel.cs
+++ api/ExpressedRealms.Events.API/API/Events/Get/EventModel.cs
@@ -9,7 +9,7 @@ public class EventModel
     public required string Location { get; set; } = null!;
     public required string WebsiteName { get; set; } = null!;
     public required string WebsiteUrl { get; set; } = null!;
-    public required string AdditionalNotes { get; set; } = null!;
+    public string? AdditionalNotes { get; set; }
     public required string TimeZoneId { get; set; }
     public int ConExperience { get; set; }
     public bool IsPublished { get; set; }
