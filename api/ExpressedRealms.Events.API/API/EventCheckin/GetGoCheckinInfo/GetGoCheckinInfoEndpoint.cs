831e0d38493c09cc6d3a607c149a99ac4b7874de
noremacskich <noremacskich@gmail.com>
2026-03-08 21:29:14 -0500

    Ran Csharpier

--- api/ExpressedRealms.Events.API/API/EventCheckin/GetGoCheckinInfo/GetGoCheckinInfoEndpoint.cs
+++ api/ExpressedRealms.Events.API/API/EventCheckin/GetGoCheckinInfo/GetGoCheckinInfoEndpoint.cs
@@ -8,9 +8,10 @@ namespace ExpressedRealms.Events.API.API.EventCheckin.GetGoCheckinInfo;
 
 public static class GetGoCheckinInfoEndpoint
 {
-    public static async Task<
-        Results<Ok<GetGoCheckinInfoResponse>, ValidationProblem>
-    > ExecuteAsync(string lookupId, [FromServices] IGetGoCheckinInfoUseCase useCase)
+    public static async Task<Results<Ok<GetGoCheckinInfoResponse>, ValidationProblem>> ExecuteAsync(
+        string lookupId,
+        [FromServices] IGetGoCheckinInfoUseCase useCase
+    )
     {
         var results = await useCase.ExecuteAsync(
             new GetGoCheckinInfoModel() { LookupId = lookupId }
@@ -20,12 +21,7 @@ public static class GetGoCheckinInfoEndpoint
             return validationProblem;
         if (results.HasNotFound(out var notFound))
         {
-            return TypedResults.Ok(
-                new GetGoCheckinInfoResponse()
-                {
-                    WasFound = false,
-                }
-            );
+            return TypedResults.Ok(new GetGoCheckinInfoResponse() { WasFound = false });
         }
 
         results.ThrowIfErrorNotHandled();
