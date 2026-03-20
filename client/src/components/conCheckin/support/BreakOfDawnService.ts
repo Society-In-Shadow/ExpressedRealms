55b5f5e99fc8b1ec27b0d6fa6e04160b1fd0f99e
noremacskich <noremacskich@gmail.com>
2026-03-20 12:11:43 -0500

    Fixed Vitality Issue

--- client/src/components/conCheckin/support/BreakOfDawnService.ts
+++ client/src/components/conCheckin/support/BreakOfDawnService.ts
@@ -41,21 +41,26 @@ export function handleAdeptSidheSorcerers(userDown: StripInfo, userMax: StripInf
     mortis: { amount: 0, reasons: [] },
   }
 
+  if (userDown.vitality > 0) {
+    changes.vitality.amount += 1
+    changes.vitality.reasons.push(`You get 1 free vitality, so it was increased by 1 for total of ${currentUserStats.vitality + 1}/${userMax.vitality}`)
+  }
+
   if (userDown.health > 0) {
-    if (currentUserStats.health >= currentUserStats.vitality) {
+    if (currentUserStats.health >= currentUserStats.vitality + changes.vitality.amount) {
       changes.health.reasons.push('You did not gain health as your vitality is lower than or equal to your health')
     }
-    else if (currentUserStats.health + 1 <= currentUserStats.vitality) {
+    else if (currentUserStats.health + 1 <= currentUserStats.vitality + changes.vitality.amount) {
       changes.health.amount += 1
       changes.health.reasons.push(`You get 1 free health, so it was increased by 1 for total of ${currentUserStats.health + 1}/${userMax.health}`)
     }
   }
 
   if (userDown.blood > 0) {
-    if (currentUserStats.blood >= currentUserStats.vitality) {
+    if (currentUserStats.blood >= currentUserStats.vitality + changes.vitality.amount) {
       changes.blood.reasons.push('You did not gain blood as your vitality is lower than or equal to your blood')
     }
-    else if (currentUserStats.blood + 1 <= currentUserStats.vitality) {
+    else if (currentUserStats.blood + 1 <= currentUserStats.vitality + changes.vitality.amount) {
       changes.blood.amount += 1
       changes.blood.reasons.push(`You get 1 free blood, so it was increased by 1 for total of ${currentUserStats.blood + 1}/${userMax.blood}`)
     }
@@ -64,15 +69,6 @@ export function handleAdeptSidheSorcerers(userDown: StripInfo, userMax: StripInf
   if (userDown.psyche > 0) {
     changes.psyche.amount += 1
     changes.psyche.reasons.push(`You get a free Psyche, so it was increased by 1 for total of ${currentUserStats.psyche + 1}/${userMax.psyche}`)
-  }
-
-  const healthFull = userMax.health == currentUserStats.health
-  if (healthFull && userDown.vitality > 0) {
-    changes.vitality.amount += 1
-    changes.vitality.reasons.push(`Your health is maxed, so vitality was increased by 1 for total of ${currentUserStats.vitality + 1}/${userMax.vitality}`)
-  }
-  else if (userDown.vitality > 0) {
-    changes.vitality.reasons.push(`Your health is not full, so no bonus to vitality`)
   }
 
   const psycheFull = userMax.psyche == currentUserStats.psyche + changes.psyche.amount
@@ -95,20 +91,4 @@ export function handleAdeptSidheSorcerers(userDown: StripInfo, userMax: StripInf
   }
 
   return changes
-  // End result should be something like
-  // They get:
-  //  x Vitality
-  //    Your health is maxed, vitality was increased by for total of x (Max Value)
-  //  x Health
-  //    Your health was increased by 1 for total of x (Max Value)
-  //    You did not gain health as your vitality is lower than your health
-  //  x blood
-  //    Your blood was increased by 1 for total of x (Max Value)
-  //    You did not gain blood as your vitality is lower than your blood
-  //  x RWP
-  //    Your blood was increased by 1 for total of x (Max Value)
-  //  x Psyche
-  //    Your Psyche was increased by 1 for total of x (Max Value)
-  //  x Mortis
-  //    Your Health, Blood, and Vitality are maxed, mortis was increased by 1 for total of x (Max Value)
 }
