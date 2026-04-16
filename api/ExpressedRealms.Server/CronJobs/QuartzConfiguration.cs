2e853261a9e7a44aa08bcfb3683b9d9276e22d2a
Noremac Skich <noremacskich@gmail.com>
2026-04-16 07:59:22 -0500

    Updated Daily Reminder Messages - each day now unique (#1077)
    Also, updated the time these get sent out to 7am, not 4am

--- api/ExpressedRealms.Server/CronJobs/QuartzConfiguration.cs
+++ api/ExpressedRealms.Server/CronJobs/QuartzConfiguration.cs
@@ -15,11 +15,11 @@ public static class QuartzConfiguration
                     opts.ForJob(EventCronJob.Key)
                         .WithIdentity("event-trigger", "cron-group")
                         .WithCronSchedule(
-                            "0 0 4 1/1 * ? *",
+                            "0 0 7 1/1 * ? *",
                             x =>
                                 x.InTimeZone(TimeZoneInfo.FindSystemTimeZoneById("America/Chicago"))
                         )
-                ); // Every day around 4am
+                ); // Every day around 7am
         });
         builder.Services.AddQuartzHostedService(options => options.WaitForJobsToComplete = true);
     }
