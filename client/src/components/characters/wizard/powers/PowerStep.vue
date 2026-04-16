c0b86452bb4a827f589217131f1c586df65ff1ce
noremacskich <noremacskich@gmail.com>
2026-04-16 12:35:38 -0500

    Character Wizard Power tab now stats Elements are required for Sorcerer

--- client/src/components/characters/wizard/powers/PowerStep.vue
+++ client/src/components/characters/wizard/powers/PowerStep.vue
@@ -1,14 +1,17 @@
 <script setup lang="ts">
 
-import { onBeforeMount, ref } from 'vue'
+import { computed, onBeforeMount, ref } from 'vue'
 import { useRoute } from 'vue-router'
 import { characterPowersStore } from '@/components/characters/character/powers/stores/characterPowerStore.ts'
 import PowerCard from '@/components/characters/wizard/powers/supporting/PowerCard.vue'
 import PickPowerCard from '@/components/characters/wizard/powers/supporting/PickPowerCard.vue'
 import ShowXPCosts from '@/components/characters/wizard/ShowXPCosts.vue'
 import { XpSectionTypes } from '@/components/characters/character/stores/experienceBreakdownStore.ts'
+import { characterStore } from '@/components/characters/character/stores/characterStore.ts'
+import Message from 'primevue/message'
 
 const characterKnowledgeData = characterPowersStore()
+const characterData = characterStore()
 const route = useRoute()
 
 onBeforeMount(async () => {
@@ -21,10 +24,18 @@ onBeforeMount(async () => {
 
 const noPowers = ref(false)
 
+const sorcererNeedsPaths = computed(() => characterData.expression === 'Sorcerers'
+  && (characterData.primaryProgressionId == null || characterData.secondaryProgressionId == null),
+)
+
 </script>
 
 <template>
   <div style="max-width: 650px; margin: 0 auto;">
+    <Message v-if="sorcererNeedsPaths" severity="warn">
+      You need to choose a primary and secondary elemental path on the basic info page in order to see all powers
+      you can choose for your sorcerer
+    </Message>
     <h1>Selected Powers</h1>
     <div v-for="path in characterKnowledgeData.powers">
       <h2>{{ path.name }}</h2>
