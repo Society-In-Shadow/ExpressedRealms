ec953a687b801e8d1930cf6ac5ac30ea64f77777
noremacskich <noremacskich@gmail.com>
2026-04-22 20:30:31 -0500

    View Character tile now correctly placed on odd entries

--- client/src/components/navbar/MainNavBar.vue
+++ client/src/components/navbar/MainNavBar.vue
@@ -92,7 +92,7 @@ const charactersMenu = computed(() => {
   if (characters.length > 0) {
     const insertSpot = isMobile.value
       ? characters.length
-      : Math.max(Math.floor(characters.length / 2), 1)
+      : Math.max(Math.ceil(characters.length / 2), 1)
     characters.splice(insertSpot, 0, { id: -1, name: 'View Characters', expression: '', state: CharacterState.Regular })
   }
 
