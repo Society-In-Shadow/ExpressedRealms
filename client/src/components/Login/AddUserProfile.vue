07d101d39028901c62996f30b09463c50b8a9f82
Noremac Skich <noremacskich@gmail.com>
2026-04-22 23:49:10 -0500

    Disable User Fix + Profile Name is now Preferred Name (#1101)
    - Disable user now works again
     - Renamed profile name to Preferred Name
    - And added message "Your preferred name is a nick name or actual name,
    something that we can reliably get your attention within a crowd."

--- client/src/components/Login/AddUserProfile.vue
+++ client/src/components/Login/AddUserProfile.vue
@@ -9,6 +9,7 @@ import { logOff } from '@/services/Authentication'
 import InputTextWrapper from '../../FormWrappers/InputTextWrapper.vue'
 import { userInfoQuery } from '@/auth/authStore.ts'
 import { useQueryWithLoading } from '@/utilities/queryOverride.ts'
+import Message from 'primevue/message'
 
 const Router = useRouter()
 const { refetch } = useQueryWithLoading(userInfoQuery)
@@ -32,13 +33,16 @@ const onSubmit = handleSubmit((values) => {
 </script>
 
 <template>
-  <form @submit="onSubmit">
+  <form style="max-width: 30em" @submit="onSubmit">
     <div class="mb-3">
       <h1 class="mt-md-0 pt-md-0">
         User Profile Setup
       </h1>
     </div>
-    <InputTextWrapper v-model="name" field-name="Name" :error-text="errors.name" />
+    <InputTextWrapper v-model="name" field-name="Preferred Name" :error-text="errors.name" />
+    <Message class="mb-3">
+      Your preferred name is a nick name or actual name, something that we can reliably get your attention within a crowd.
+    </Message>
     <Button data-cy="update-profile-button" label="Update Profile" class="w-100 mb-2" type="submit" />
   </form>
   <Button data-cy="logoff-button" label="Logoff" class="w-100 mb-2" @click="logOff(Router)" />
