78bb9e57fe9f375f74aba08b7c9852f2a3f11ddd
noremacskich <noremacskich@gmail.com>
2026-05-25 06:00:40 -0500

    Stats no longer have table formatting
    Forgot to convert it, but wound up liking the look, so kept it

--- client/src/components/characters/character/proficiency/ProficiencyAccordionPanel.vue
+++ client/src/components/characters/character/proficiency/ProficiencyAccordionPanel.vue
@@ -24,36 +24,31 @@ const props = defineProps({
       </div>
     </AccordionHeader>
     <AccordionContent>
-      <div class="p-datatable p-component p-datatable-striped">
-        <div class="p-datatable-table-container">
-          <table class="w-100 p-datatable-table">
-            <!-- Table header -->
-            <thead class="p-datatable-thead">
-              <tr>
-                <th class="p-datatable-header-cell">
-                  Source
-                </th>
-                <th class="p-datatable-header-cell text-right">
-                  Bonus
-                </th>
-              </tr>
-            </thead>
-            <tbody class="p-datatable-tbody">
-              <tr
-                v-for="(modifier, index) in props.proficiency.appliedModifiers" :key="index"
-                :class="index % 2 === 0 ? 'p-row-even' : 'p-row-odd'"
-              >
-                <td>
-                  {{ modifier.name }}
-                </td>
-                <td class="text-right">
-                  {{ modifier.value >= 0 ? '+' : '' }}{{ modifier.value }}
-                </td>
-              </tr>
-            </tbody>
-          </table>
-        </div>
-      </div>
+      <table class="w-100">
+        <thead>
+          <tr>
+            <th class="text-left">
+              Source
+            </th>
+            <th class="text-right">
+              Bonus
+            </th>
+          </tr>
+        </thead>
+        <tbody>
+          <tr
+            v-for="(modifier, index) in props.proficiency.appliedModifiers" :key="index"
+            :class="index % 2 === 0 ? 'p-row-even' : 'p-row-odd'"
+          >
+            <td>
+              {{ modifier.name }}
+            </td>
+            <td class="text-right">
+              {{ modifier.value >= 0 ? '+' : '' }}{{ modifier.value }}
+            </td>
+          </tr>
+        </tbody>
+      </table>
     </AccordionContent>
   </AccordionPanel>
 </template>
