4f5b6a8740e63bae536dd62690660706c8320973
noremacskich <noremacskich@gmail.com>
2026-05-30 02:29:19 -0500

    Added Prima Void and Wealth Level Modifiers to DB
    Also added new Enum to keep track of that

---
+++ api/ExpressedRealms.DB/Migrations/20260530072303_AddMoteAndWealtModifiersToStatModifiers.cs
@@ -0,0 +1,39 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+#pragma warning disable CA1814 // Prefer jagged arrays over multidimensional
+
+namespace ExpressedRealms.DB.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddMoteAndWealtModifiersToStatModifiers : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.InsertData(
+                table: "stat_modifiers",
+                columns: new[] { "id", "name" },
+                values: new object[,]
+                {
+                    { 24, "Prima / Void" },
+                    { 25, "Wealth Level" }
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DeleteData(
+                table: "stat_modifiers",
+                keyColumn: "id",
+                keyValue: 24);
+
+            migrationBuilder.DeleteData(
+                table: "stat_modifiers",
+                keyColumn: "id",
+                keyValue: 25);
+        }
+    }
+}
