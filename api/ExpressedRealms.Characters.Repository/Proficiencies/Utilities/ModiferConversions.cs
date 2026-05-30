9e276eabc65bf5677a8ad68a3bf116e0a699ad7c
noremacskich <noremacskich@gmail.com>
2026-05-30 02:34:30 -0500

    Removed DB Modifier Type Conversions
    This is no longer needed - was carry over from initial implementation, and was no longer needed with Modifier System

--- api/ExpressedRealms.Characters.Repository/Proficiencies/Utilities/ModiferConversions.cs
+++ api/ExpressedRealms.Characters.Repository/Proficiencies/Utilities/ModiferConversions.cs
@@ -40,35 +40,6 @@ internal static class ModiferConversions
         };
     }
 
-    public static ModifierType GetModifierType(DbModifierTypes statType)
-    {
-        return statType switch
-        {
-            DbModifierTypes.StrengthStonePull => ModifierType.Strength,
-            DbModifierTypes.StrikeProficiencyModifier => ModifierType.Strike,
-            DbModifierTypes.ConstitutionStonePull => ModifierType.Constitution,
-            //DbModifierTypes.WisdomStonePull => ,
-            DbModifierTypes.IntelligenceStonePull => ModifierType.Intelligence,
-            DbModifierTypes.WillpowerStonePull => ModifierType.Willpower,
-            DbModifierTypes.DexterityStonePull => ModifierType.Dexterity,
-            DbModifierTypes.ThrustProficiencyModifier => ModifierType.Thrust,
-            DbModifierTypes.ShootProficiencyModifier => ModifierType.Shoot,
-            DbModifierTypes.ThrowRange => ModifierType.Throw,
-            DbModifierTypes.CastProficiencyModifier => ModifierType.Cast,
-            DbModifierTypes.DodgeGeneralDamageProficiency => ModifierType.Dodge,
-            DbModifierTypes.ParryGeneralDamageProficiency => ModifierType.Parry,
-            DbModifierTypes.EvadeGeneralDamageProficiency => ModifierType.Evade,
-            DbModifierTypes.WardGeneralDamageProficiency => ModifierType.Ward,
-            DbModifierTypes.DeflectGeneralDamageProficiency => ModifierType.Deflection,
-            DbModifierTypes.ReserveWillPower => ModifierType.RWP,
-            DbModifierTypes.ThrowStat => ModifierType.Throw,
-            DbModifierTypes.ProjectStat => ModifierType.Project,
-            DbModifierTypes.AgilityStonePull => ModifierType.Agility,
-            DbModifierTypes.Project => ModifierType.Project,
-            _ => throw new ArgumentOutOfRangeException(nameof(statType), statType, null),
-        };
-    }
-
     public static ModifierType GetModifierType(ProficiencyModifierInfoDto mapping)
     {
         return mapping.ModifierTypeId switch
