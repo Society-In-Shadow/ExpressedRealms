ba7c017e1677589eff00c16853060021a68c7277
noremacskich <noremacskich@gmail.com>
2026-06-05 23:14:56 -0500

    Fixed Type Issue with Forms
    Typescript will now properly pick up and display the field type automatically based on yup values

--- client/src/utilities/formUtilities.ts
+++ client/src/utilities/formUtilities.ts
@@ -1,7 +1,11 @@
-import { useForm } from 'vee-validate'
+import { type Path, useForm } from 'vee-validate'
 import type { ObjectSchema } from 'yup'
-import { computed } from 'vue'
+import { computed, type Ref } from 'vue'
 import type { FormField } from '@/FormWrappers/Interfaces/FormField'
+
+type FormFields<T> = {
+  [K in keyof T]: FormField<T[K]>
+}
 
 export function useGenericForm<T extends Record<string, any>>(validationSchema: ObjectSchema<T>) {
   const { defineField, handleSubmit, errors, handleReset, setErrors } = useForm<T>({
@@ -10,17 +14,20 @@ export function useGenericForm<T extends Record<string, any>>(validationSchema: 
     keepValuesOnUnmount: false,
   })
 
-  function createFormField<K extends keyof T>(fieldName: K): FormField {
+  function createFormField<K extends keyof T>(
+    fieldName: K,
+  ): FormField<T[K]> {
+    const path = fieldName as unknown as Path<T>
     return {
-      field: defineField(fieldName)[0],
-      error: computed(() => errors.value[fieldName]),
-      label: (validationSchema.fields[fieldName as string] as any)?.spec?.label ?? fieldName,
-      isRequired: (validationSchema.fields[fieldName as string] as any)?.spec?.optional === false,
+      field: defineField(path)[0] as Ref<T[K]>,
+      error: computed(() => errors.value[path] as string | undefined),
+      label: (validationSchema.fields[path] as any)?.spec?.label ?? fieldName,
+      isRequired: (validationSchema.fields[path] as any)?.spec?.optional === false,
     }
   }
 
   // Generate all form fields dynamically
-  const fields = {} as Record<keyof T, FormField>
+  const fields = {} as FormFields<T>
   Object.keys(validationSchema.fields).forEach((key) => {
     fields[key as keyof T] = createFormField(key as keyof T)
   })
