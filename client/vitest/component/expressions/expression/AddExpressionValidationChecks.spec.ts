fe5ea669740b4074d593218f515ff35851e2af29
noremacskich <noremacskich@gmail.com>
2026-08-04 21:14:59 -0500

    Updated ViTests

--- client/vitest/component/expressions/expression/AddExpressionValidationChecks.spec.ts
+++ client/vitest/component/expressions/expression/AddExpressionValidationChecks.spec.ts
@@ -1,70 +1,83 @@
-import { describe, expect, it } from 'vitest'
+import { beforeEach, describe, expect, it } from 'vitest'
 import {
-  handleSubmit,
-  nameField,
-  navMenuImageField,
-  shortDescriptionField,
-} from '../../../../src/components/expressions/expression/AddExpressionValidation'
+  getValidationInstance,
+} from '../../../../src/components/expressions/expression/validators/addExpressionValidation'
 
 describe('Add Expression Schema - Field Validations', () => {
+  let form: ReturnType<typeof getValidationInstance>
+
+  beforeEach(() => {
+    form = getValidationInstance()
+    form.setValues()
+  })
+
   describe('Name', () => {
     it('Fails when there are more then 50 characters', async () => {
-      nameField.field.value = 'a'.repeat(51)
-      await handleSubmit(() => {})()
-      expect(nameField.error.value).toEqual('Name must be at most 50 characters')
+      form.fields.name.field.value = 'a'.repeat(51)
+      await form.handleSubmit(() => {})()
+      expect(form.fields.name.error.value).toEqual('Name must be at most 50 characters')
     })
+
     it('Says it\'s required when not filled in', async () => {
-      nameField.field.value = ''
-      await handleSubmit(() => {})()
-      expect(nameField.error.value).toEqual('Name is a required field')
+      form.fields.name.field.value = ''
+      await form.handleSubmit(() => {})()
+      expect(form.fields.name.error.value).toEqual('Name is a required field')
     })
+
     it('No Errors when it\'s a valid value', async () => {
-      nameField.field.value = 'asdf'
-      await handleSubmit(() => {})()
-      expect([undefined, '']).toContain(nameField.error.value)
+      form.fields.name.field.value = 'asdf'
+      await form.handleSubmit(() => {})()
+      expect(form.fields.name.error.value).toBeUndefined()
     })
+
     it('Label is correct', async () => {
-      expect(nameField.label).toEqual('Name')
+      expect(form.fields.name.label).toEqual('Name')
     })
   })
 
   describe('Short Description', () => {
     it('Fails when there are more then 125 characters', async () => {
-      shortDescriptionField.field.value = 'a'.repeat(126)
-      await handleSubmit(() => {})()
-      expect(shortDescriptionField.error.value).toEqual('Short Description must be at most 125 characters')
+      form.fields.shortDescription.field.value = 'a'.repeat(126)
+      await form.handleSubmit(() => {})()
+      expect(form.fields.shortDescription.error.value).toEqual('Short Description must be at most 125 characters')
     })
+
     it('Says it\'s required when not filled in', async () => {
-      shortDescriptionField.field.value = ''
-      await handleSubmit(() => {})()
-      await expect(shortDescriptionField.error.value).toEqual('Short Description is a required field')
+      form.fields.shortDescription.field.value = ''
+      await form.handleSubmit(() => {})()
+      expect(form.fields.shortDescription.error.value).toEqual('Short Description is a required field')
     })
+
     it('Label is correct', async () => {
-      expect(shortDescriptionField.label).toEqual('Short Description')
+      expect(form.fields.shortDescription.label).toEqual('Short Description')
     })
+
     it('No Errors when it\'s a valid value', async () => {
-      shortDescriptionField.field.value = 'asdf'
-      await handleSubmit(() => {})()
-      expect([undefined, '']).toContain(shortDescriptionField.error.value)
+      form.fields.shortDescription.field.value = 'asdf'
+      await form.handleSubmit(() => {})()
+      expect(form.fields.shortDescription.error.value).toBeUndefined()
     })
   })
 
   describe('Nav Menu Icon', () => {
     it('Default Value is \'emergency_home\'', async () => {
-      expect(navMenuImageField.field.value).toEqual('emergency_home')
+      expect(form.fields.navMenuImage.field.value).toEqual('emergency_home')
     })
+
     it('Says it\'s required when not filled in', async () => {
-      navMenuImageField.field.value = ''
-      await handleSubmit(() => {})()
-      await expect(navMenuImageField.error.value).toEqual('Nav Menu Icon is a required field')
+      form.fields.navMenuImage.field.value = ''
+      await form.handleSubmit(() => {})()
+      expect(form.fields.navMenuImage.error.value).toEqual('Nav Menu Icon is a required field')
     })
+
     it('No Errors when it\'s a valid value', async () => {
-      navMenuImageField.field.value = 'asdf'
-      await handleSubmit(() => {})()
-      expect([undefined, '']).toContain(navMenuImageField.error.value)
+      form.fields.navMenuImage.field.value = 'asdf'
+      await form.handleSubmit(() => {})()
+      expect(form.fields.navMenuImage.error.value).toBeUndefined()
     })
+
     it('Label is correct', async () => {
-      expect(navMenuImageField.label).toEqual('Nav Menu Icon')
+      expect(form.fields.navMenuImage.label).toEqual('Nav Menu Icon')
     })
   })
 })
