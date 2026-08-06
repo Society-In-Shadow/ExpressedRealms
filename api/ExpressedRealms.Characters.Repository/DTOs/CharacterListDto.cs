8cc9e4fcbe8355c8d57a498ce23afa1237459199
noremacskich <noremacskich@gmail.com>
2026-08-06 02:43:28 -0500

    Character List now shows copied expression emblems
    Plus added new Expression Type to frontend

--- api/ExpressedRealms.Characters.Repository/DTOs/CharacterListDto.cs
+++ api/ExpressedRealms.Characters.Repository/DTOs/CharacterListDto.cs
@@ -2,15 +2,16 @@ namespace ExpressedRealms.Characters.Repository.DTOs;
 
 public sealed record CharacterListDto
 {
-    public string Id { get; set; } = null!;
+    public int Id { get; set; }
 
     /// <example>John Doe</example>
     public string Name { get; set; } = null!;
 
     /// <example>Adept</example>
-    public string Expression { get; set; } = null!;
+    public string ExpressionName { get; set; } = null!;
 
     public bool IsPrimaryCharacter { get; set; }
     public bool IsInCharacterCreate { get; set; }
     public bool IsRetired { get; set; }
+    public int? ExpressionSubTypeId { get; set; }
 }
--- client/src/components/common/ExpressionLogo.vue
+++ client/src/components/common/ExpressionLogo.vue
@@ -1,11 +1,14 @@
 <script setup lang="ts">
 
 import Skeleton from 'primevue/skeleton'
+import { computed, type PropType } from 'vue'
+import { ExpressionSubTypes } from '@/components/expressions/expressionSubTypes.ts'
 
 const props = defineProps({
-  expressionName: {
-    type: String,
+  expressionSubTypeId: {
+    type: Number as PropType<ExpressionSubTypes>,
     required: false,
+    default: 0,
   },
   isLoading: {
     type: Boolean,
@@ -14,9 +17,21 @@ const props = defineProps({
   },
 })
 
+const image = computed(() => {
+  switch (props.expressionSubTypeId) {
+    case ExpressionSubTypes.Adepts: return '/expressions/adepts_square.png'
+    case ExpressionSubTypes.Aeternari: return '/expressions/aeternari_square.png'
+    case ExpressionSubTypes.Shammas: return '/expressions/shammas_square.png'
+    case ExpressionSubTypes.Sidhe: return '/expressions/sidhe_square.png'
+    case ExpressionSubTypes.Sorcerers: return '/expressions/sorcerers_square.png'
+    case ExpressionSubTypes.Vampyre: return '/expressions/vampyres_square.png'
+    default: return ''
+  }
+})
+
 </script>
 
 <template>
   <Skeleton v-if="props.isLoading" width="150px" height="150px" />
-  <img v-else :src="'/expressions/' + props.expressionName.toLowerCase() + '_square.png'" class="w-100" style="max-width: 250px" alt="round circle">
+  <img v-else :src="image" class="w-100" style="max-width: 250px" alt="round circle">
 </template>
