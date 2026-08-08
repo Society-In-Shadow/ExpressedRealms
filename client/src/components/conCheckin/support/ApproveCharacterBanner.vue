40b39559e99d00eb906e9eccc0774ff8e328e862
noremacskich <noremacskich@gmail.com>
2026-08-08 03:23:18 -0500

    Added Dynamic Faction Promotion Request button
    On GO Checkin, if they have unanswered faction promotion requests, it will now add a checkbox that will "force them" to take a look

--- client/src/components/conCheckin/support/ApproveCharacterBanner.vue
+++ client/src/components/conCheckin/support/ApproveCharacterBanner.vue
@@ -9,12 +9,15 @@ import { EventCheckinStore } from '@/components/conCheckin/stores/eventCheckinSt
 import { userPermissionStore } from '@/stores/userPermissionStore.ts'
 import { characterStore } from '@/components/characters/character/stores/characterStore.ts'
 import { useRoute } from 'vue-router'
+import { pickedFactionQuery } from '@/components/characters/wizard/factions/stores/factionStore.ts'
+import { useQuery } from '@pinia/colada'
 
 const eventCheckinInfo = EventCheckinStore()
 const permissionInfo = userPermissionStore()
 const characterInfo = characterStore()
 const permissionCheck = permissionInfo.permissionCheck
 const reviewedContacts = ref(false)
+const reviewedFactionPromotionRequest = ref(false)
 const hasCheckinPermission = ref(false)
 const route = useRoute()
 
@@ -26,21 +29,40 @@ onBeforeMount(async () => {
 const showBanner = computed(() => eventCheckinInfo.hasActiveEvent && hasCheckinPermission.value
   && characterInfo.isPrimaryCharacter && route.query.src == 'approve_character')
 
+const { data: characterData, isLoading: characterDataLoading } = useQuery(() => ({
+  ...pickedFactionQuery(Number.parseInt(route.params.id)),
+  enabled: showBanner.value,
+}))
+
 const reviewedCharacter = async () => {
   await eventCheckinInfo.approveCharacterSheet()
 }
+
+const showFactionInfo = computed(() => {
+  return !characterDataLoading.value && characterData.value!.factionLevels.find(x => x.requestedPromotion && x.approvalDate == null)
+})
+
+const enableReviewButton = computed(() => {
+  return reviewedContacts.value
+    && (!showFactionInfo.value || reviewedFactionPromotionRequest.value)
+})
+
 </script>
 
 <template>
   <Message v-if="showBanner" severity="warn" class="mb-3">
     <div class="w-100">
       <p>You need to review this character sheet.</p>
-      <div>
+      <div class="pb-3">
         <Checkbox v-model="reviewedContacts" input-id="reviewed" class="mr-2" binary />
         <label for="reviewed">I have reviewed all contacts</label>
       </div>
+      <div v-if="showFactionInfo">
+        <Checkbox v-model="reviewedFactionPromotionRequest" input-id="reviewed" class="mr-2" binary />
+        <label for="reviewed">I have addressed their faction promotion request</label>
+      </div>
 
-      <Button label="Reviewed Character" class="mt-3" :disabled="!reviewedContacts" @click="reviewedCharacter" />
+      <Button label="Reviewed Character" class="mt-3" :disabled="!enableReviewButton" @click="reviewedCharacter" />
     </div>
   </Message>
 </template>
