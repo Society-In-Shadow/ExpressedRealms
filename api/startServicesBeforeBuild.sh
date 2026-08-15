ce5d1fad7015d4b629c26386b106138543fd3073
noremacskich <noremacskich@gmail.com>
2026-08-15 03:02:28 -0500

    Tweaked how podman compose works on startup

--- api/startServicesBeforeBuild.sh
+++ api/startServicesBeforeBuild.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
 # Reminder that this is relative to the ExpressedRealms.Server folder, as that is where the root of this is at
-podman compose -f ../../docker-compose.yaml -f ../../docker-compose.container-api.yaml stop "webapi"
 
 podman compose up -d
+podman compose -f ../../docker-compose.yaml -f ../../docker-compose.container-api.yaml stop "webapi"
