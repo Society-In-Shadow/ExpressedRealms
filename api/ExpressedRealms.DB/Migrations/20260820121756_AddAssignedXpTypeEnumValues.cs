34b4d2ad9887e6f18749de84b20ec8f930b4d29a
noremacskich <noremacskich@gmail.com>
2026-08-20 07:18:57 -0500

    Added Assigned XP Type Enum and tied to db generation

---
+++ api/ExpressedRealms.DB/Migrations/20260820121756_AddAssignedXpTypeEnumValues.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+#pragma warning disable CA1814 // Prefer jagged arrays over multidimensional
+
+namespace ExpressedRealms.DB.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddAssignedXpTypeEnumValues : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.InsertData(
+                table: "assigned_xp_types",
+                columns: new[] { "id", "deleted_at", "description", "is_deleted", "name" },
+                values: new object[,]
+                {
+                    { 7, null, "When a user pays for character storage, they get max of 5 XP", false, "Bought Character Storage" }
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DeleteData(
+                table: "assigned_xp_types",
+                keyColumn: "id",
+                keyValue: 7);
+        }
+    }
+}
