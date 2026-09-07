931705e56107cdcbeb8c83d4d08e4be1b6fecaed
noremacskich <noremacskich@gmail.com>
2026-09-07 04:45:40 -0500

    Can now keep track of Initiative Modifier

--- api/ExpressedRealms.DB/Models/ModifierSystem/StatModifiers/StatModifierEnum.cs
+++ api/ExpressedRealms.DB/Models/ModifierSystem/StatModifiers/StatModifierEnum.cs
@@ -80,4 +80,9 @@ public sealed class StatModifierEnum : SmartEnum<StatModifierEnum, int>
         "Sprinting Paces",
         34
     );
+    
+    public static readonly StatModifierEnum Initiative = new StatModifierEnum(
+        "Initiative",
+        35
+    );
 }
