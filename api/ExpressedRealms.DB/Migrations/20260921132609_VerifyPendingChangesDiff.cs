9599dcb696ffe5be7ab04b296318e2f37e0461a9
noremacskich <noremacskich@gmail.com>
2026-09-21 08:27:58 -0500

    Apparently was missing the new final check enum value

---
+++ api/ExpressedRealms.DB/Migrations/20260921132609_VerifyPendingChangesDiff.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace ExpressedRealms.DB.Migrations
+{
+    /// <inheritdoc />
+    public partial class VerifyPendingChangesDiff : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.InsertData(
+                table: "checkin_stages",
+                columns: new[] { "id", "description", "name" },
+                values: new object[] { 15, "The player has completed all steps, and is fully checked in for all the days of the event.", "Final Stage" });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DeleteData(
+                table: "checkin_stages",
+                keyColumn: "id",
+                keyValue: 15);
+        }
+    }
+}
